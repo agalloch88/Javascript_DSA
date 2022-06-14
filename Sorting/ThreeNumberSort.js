954aff45e7862e7faeb38b8546bb6687dcbef4ab
Ryan Kirsch <agalloch88@gmail.com>
2022-06-14 09:44:00 -0400

    ThreeNumberSort solution 3

--- Sorting/ThreeNumberSort.js
+++ Sorting/ThreeNumberSort.js
@@ -82,3 +82,39 @@ function swap(i, j, array) {
     array[j] = array[i];
     array[i] = temp;
 }
+
+// Solution 3:
+// This option does a single pass of the array, and uses three pointers to determine where
+// array items should be positioned
+
+function threeNumberSort(array, order) {
+    const firstValue = order[0];
+    const secondValue = order[1];
+
+    let firstIdx = 0;
+    let secondIdx = 0;
+    let thirdIdx = array.length - 1;
+
+    while (secondIdx <= thirdIdx) {
+        const value = array[secondIdx];
+        if (value === firstValue) {
+            swap(firstIdx, secondIdx, array);
+            firstIdx++;
+            secondIdx++;
+        } else if (value === secondValue) {
+            secondIdx++;
+        } else {
+            swap(secondIdx, thirdIdx, array);
+            thirdIdx--;
+        }
+    }
+
+    return array;
+
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
