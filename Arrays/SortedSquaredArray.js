24db8923c0480191cae3b3b68fa22f775c27f886
Ryan Kirsch <agalloch88@gmail.com>
2022-06-16 07:08:51 -0400

    SortedSquaredArray solution 2

--- Arrays/SortedSquaredArray.js
+++ Arrays/SortedSquaredArray.js
@@ -26,3 +26,36 @@ function sortedSquaredArray(array) {
     sortedSquares.sort((a, b) => a - b);
     return sortedSquares;
 }
+
+// Solution 2:
+
+// O(n) time due to single for loop pass over input array
+// O(n) space due to creating new holder array for n values
+
+function sortedSquaredArray(array) {
+    // create holder array filled with zeroes for length of input array
+    const sortedSquares = new Array(array.length).fill(0);
+    // initialize two pointers at beginning and end of input array
+    let smallerValueIdx = 0;
+    let largerValueIdx = array.length - 1;
+    // will fill the squared values from the end of the array forward, such that it
+    // is sorted without needing to call .sort()
+    for (let idx = array.length - 1; idx >= 0; idx--) {
+        // initialize variables to compare theoretically smaller & larger variables at
+        // either end of array. since input array is sorted, extreme values will be
+        // at opposite ends, so we work our way inward.
+        const smallerValue = array[smallerValueIdx];
+        const largerValue = array[largerValueIdx];
+        // compare the absolute values of these integers to determine which will be
+        // the larger squared value, then move that index inward toward center
+        if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+            sortedSquares[idx] = smallerValue * smallerValue;
+            smallerValueIdx++;
+        } else {
+            sortedSquares[idx] = largerValue * largerValue;
+            largerValueIdx--;
+        }
+    }
+
+    return sortedSquares;
+}
