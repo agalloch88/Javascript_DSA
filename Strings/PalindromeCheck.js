bf9f9d7a667310d068e555658b995096140268b6
Ryan Kirsch <agalloch88@gmail.com>
2022-06-17 16:36:10 -0400

    PalindromeCheck solution 2

--- Strings/PalindromeCheck.js
+++ Strings/PalindromeCheck.js
@@ -19,10 +19,31 @@
 function isPalindrome(string) {
     // create empty holder string
     let reversedString = '';
-    // start at the end of input string, iterate forward and pop values at each index to reversedString
+    // start at the end of input string, iterate forward and add values at each index to reversedString
     for (let i = string.length - 1; i >= 0; i--) {
         reversedString += string[i];
     }
     // check whether input and reversedString are equal
     return string === reversedString;
+}
+
+// Solution 2:
+
+// O(n) time due to for loop, more efficient since using an array
+// O(n) space since building new data structure containing n values from input
+
+// This approach is similar to Solution 1, but opts to use an array to hold the values rather than a string.
+// Arrays are a more efficient data structure overall than strings, but the equality check must be done between
+// string and string, not string and array, so we join all of the values pushed into reversedChars to make the
+// comparison.
+
+function isPalindrome(string) {
+    // create holder array for each letter
+    const reversedChars = [];
+    // start at end of input string and push each new value into holder array
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedChars.push(string[i]);
+    }
+    // check whether input and joined values from array are equal
+    return string === reversedChars.join('');
 }
