88dca51d8ccef5c001fb48bb8724a9355a06cac0
Ryan Kirsch <agalloch88@gmail.com>
2022-06-25 09:16:36 -0400

    CaesarCypherEncryptor solution 2

--- Strings/CaesarCypherEncryptor.js
+++ Strings/CaesarCypherEncryptor.js
@@ -12,6 +12,8 @@
 
 // Solution 1:
 
+// This solution deals with unicode values, and calculating those values, so relies on knowledge of how to manipulate unicode.
+
 // O(n) time due to single for loop pass over n items in string
 // O(n) space due to storing n items from string in new array
 
@@ -31,6 +33,29 @@ function caesarCypherEncryptor(string, key) {
 function getNewLetter(letter, key) {
     // figure out what the new character code should be
     const newLetterCode = letter.charCodeAt() + key;
-    // if code is <= 122, can convert to string right aray, otherwise start from right before 'a' and add remainder from newLetterCode % 122
+    // if code is <= 122, can convert to string right away, otherwise start from right before 'a' and add remainder from newLetterCode % 122
     return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + (newLetterCode % 122));
+}
+
+// Solution 2:
+
+// Similar to solution 1, but we create an array of all alphabet letters rather than dealing with unicode values.
+
+// O(n) time due to looping over n values in input string
+// O(n) space due to storing values in new array
+
+function caesarCypherEncryptor(string, key) {
+    const newLetters = [];
+    const newKey = key % 26;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    for (const letter of string) {
+        newLetters.push(getNewLetter(letter, newKey, alphabet));
+    }
+    return newLetters.join('');
+}
+
+function getNewLetter(letter, key, alphabet) {
+    const newLetterCode = alphabet.indexOf(letter) + key;
+    return alphabet[newLetterCode] % 26;
 }
