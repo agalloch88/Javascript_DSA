ef55b6ec7c35e4b9b423dc364d90f41916326762
Ryan Kirsch <agalloch88@gmail.com>
2022-06-28 17:32:58 -0400

    GetNthFibonacci solution 1

---
+++ Recursion/NthFibonacci.js
@@ -0,0 +1,27 @@
+// The Fibonacci sequence is defined as follows: the first number of the sequence is 0, the seconf number is 1, and the nth number is the sum of the (n - 1)th
+// and (n - 2)th numbers. Write a function which takes in an integer n, and returns the nth Fibonacci number.
+
+// Important note: the Fibonacci sequence is often defined with its first two numbers as F0 = - and F1 = 1. For the purpose of this question,
+// the first Fibonacci nuymber is F0; therefore, getNthFib(1) is equal to F0, getNthFib(2) is equal to F1, etc.
+
+// Sample Input:
+// n = 2
+
+// Sample Output:
+// 1
+// found by adding 0 + 1
+
+// Solution 1:
+
+// O(2^n) time due to 2 operations per n value
+// O(n) space due to storing n values on the call stack
+
+function getNthFib(n) {
+    if (n === 2) {
+        return 1;
+    } else if (n === 1) {
+        return 0;
+    } else {
+        return getNthFib(n - 1) + getNthFib(n - 2);
+    }
+}
