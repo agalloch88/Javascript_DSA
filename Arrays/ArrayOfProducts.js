76d570893eeacf6c63daa70ab7342d3d75d24787
Ryan Kirsch <agalloch88@gmail.com>
2022-07-19 16:25:01 -0400

    ArrayOfProducts description and example

--- Arrays/ArrayOfProducts.js
+++ Arrays/ArrayOfProducts.js
@@ -1,3 +1,20 @@
+// Write a function which takes in a non-empty array of integers and returns and array of the same length, where each element in the output array is equal to 
+// the product of all the numbers in the input array except for the element at the current index.
+
+// In other words, the value at output[i] is the product of all the numbers in the input array except for the number at input[i].
+
+// Note that the solution cannot use division for the purposes of this problem.
+
+// Sample Input:
+// array = [5, 1, 4, 2]
+
+// Sample Output:
+// [8, 40, 10, 20]
+// 8 is equal to 1 x 4 x 2
+// 40 is equal 50 5 x 4 x 2
+// 10 is equal to 5 x 1 x 2
+// 20 is equal to 5 x 1 x 4
+
 // Solution 1:
 
 // O(n^2) time due ot nested for loops
