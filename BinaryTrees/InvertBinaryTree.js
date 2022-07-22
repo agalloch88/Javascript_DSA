a0fb667d5951248f15df7f9c0e28f775cd5c6598
Ryan Kirsch <agalloch88@gmail.com>
2022-07-22 17:11:09 -0400

    InvertBinaryTree start

---
+++ BinaryTrees/InvertBinaryTree.js
@@ -0,0 +1,25 @@
+// Write a function which takes in a Binary Tree and inverts the tree. In other words, the function 
+// should swap every left node in the tree for its corresponding right node.
+
+// Each Binary Tree node has an integer value, a left child node, and a right child node. Children nodes can either be Binary Tree nodes themselves, or None/null.
+
+// Sample Input:
+// tree =       1
+//             /  \
+//            2    3
+//           / \  / \
+//          4   5 6  7
+//         / \
+//        8   9
+
+// Sample Output:
+// tree =       1
+//             /  \
+//            3    2
+//           / \  / \
+//          7   6 5  4
+//                  / \
+//                 9   8
+
+// Solution 1:
+
