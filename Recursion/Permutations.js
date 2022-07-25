0e6b851de503cf28c9783089a8a274d96bdbda51
Ryan Kirsch <agalloch88@gmail.com>
2022-07-25 08:28:42 -0400

    Permutatins start

---
+++ Recursion/Permutations.js
@@ -0,0 +1,11 @@
+// Write a function which takes in an array of unique integers, and returns an array of all permutations of those integers in no specified order.
+
+// If the input array is empty, the function should return an empty array.
+
+// Sample Input:
+// array = [1, 2, 3]
+
+// Sample Output:
+// [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+// Solution 1:
