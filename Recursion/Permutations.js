1eccb0d9f7b644fac3d1cefd5b1ee3582a955c3b
Ryan Kirsch <agalloch88@gmail.com>
2022-07-25 08:39:30 -0400

    Permutations solution 1

--- Recursion/Permutations.js
+++ Recursion/Permutations.js
@@ -9,3 +9,31 @@
 // [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
 
 // Solution 1:
+
+// solution using slicing to grab new permutations
+
+// O(n*n!) time on average due to n! possible permutations and n time to get each, at worst O(n^2*n!)
+// O(n*n!) space due to storing n! permutations in new array
+
+function getPermutations(array) {
+    // initialize empty holder array for permutations
+    let permutations = []
+    // recursively populate permutations array
+    getPermutationsHelper(array, [], permutations);
+    // return permutations; if input empty, this will be base case of returning empty array
+    return permutations;
+}
+
+function getPermutationsHelper(array, currentPermutation, permutations) {
+    // once reaching end of input numbers and we have a permutation, push it to holder array
+    if (!array.length && currentPermutation.length) {
+        permutations.push(currentPermutation);
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            let newArray = array.slice(0, i).concat(array.slice(i + 1));
+            let newPermutation = currentPermutation.concat([array[i]]);
+            getPermutationsHelper(newArray, newPermutation, permutations);
+        }
+    }
+
+}
