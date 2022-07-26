9c0d3768ff0b914590e85c4e2c6d211058f7ba8e
Ryan Kirsch <agalloch88@gmail.com>
2022-07-26 07:40:16 -0400

    FindSuccessor start

---
+++ BinaryTrees/FindSuccessor.js
@@ -0,0 +1,39 @@
+// Write a function which takes in a Binary Tree (where nodes have an additional pointer to their parent node), as well as a node contained within that tree,
+// and returns the given node's successor.
+
+// A node's successor is the next node to be visited (immediately after the given node) when traversing the tree using the in-order tree-traversal
+// technique. A node has no successor if it is the last node to be visited in the in-order traversal.
+
+// If a node has no successor, the function should return None/null.
+
+// Each Binary Tree has an integer value, a parent node, a left child node, and a right child node. Children nodes can either be Binary Tree nodes themselves,
+// or None/null.
+
+// Sample Input:
+// tree =           1
+//                 / \
+//                2   3
+//               / \
+//              4   5
+//             /
+//            6
+
+// node = 5
+
+// Sample Output:
+// 1, as the in-order traversal is [6, 4, 2, 5, 1, 3], and 1 comes immediately after 5
+
+// Solution 1:
+
+class BinaryTree{
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+    }
+}
+
+function findSuccessor(tree, node) {
+    
+}
