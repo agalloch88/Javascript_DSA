c61d9d0077b13d13718dc8383779feccd072358d
Ryan Kirsch <agalloch88@gmail.com>
2022-07-27 12:52:13 -0400

    HeightBalancedBinaryTree solution 1

--- BinaryTrees/HeightBalancedBinaryTree.js
+++ BinaryTrees/HeightBalancedBinaryTree.js
@@ -18,3 +18,46 @@
 
 // Solution 1:
 
+// recursive solution which passes variables tracking whether a subtree is balanced, and height, all the way back up to root for final return of boolean
+
+// O(n) time due to iterating over n nodes in tree
+// O(h) space where h is the height of the longest branch, as there will be at most h frames on call stack at any given time
+
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  class TreeInfo {
+    constructor(isBalanced, height) {
+      this.isBalanced = isBalanced;
+      this.height = height;
+    }
+  }
+  
+  function heightBalancedBinaryTree(tree) {
+    let treeInfo = getTreeInfo(tree);
+    return treeInfo.isBalanced;
+  }
+  
+  function getTreeInfo(node) {
+    if (node === null) {
+      return new TreeInfo(true, -1);
+    }
+  
+    let leftSubTreeInfo = getTreeInfo(node.left);
+    let rightSubTreeInfo = getTreeInfo(node.right);
+  
+    let isBalanced =
+      leftSubTreeInfo.isBalanced &&
+      rightSubTreeInfo.isBalanced &&
+      Math.abs(leftSubTreeInfo.height - rightSubTreeInfo.height) <= 1;
+  
+    let height = Math.max(leftSubTreeInfo.height, rightSubTreeInfo.height) + 1;
+  
+    return new TreeInfo(isBalanced, height);
+  }
+  
