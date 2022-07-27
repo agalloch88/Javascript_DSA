61b4ac2e078bb0cd9606eb4322d93f3e0782f5e0
Ryan Kirsch <agalloch88@gmail.com>
2022-07-27 12:51:49 -0400

    HeightBalancedBinaryTree start

---
+++ BinaryTrees/HeightBalancedBinaryTree.js
@@ -0,0 +1,20 @@
+// The problem presents the root node of a Binary Tree. Write a function which returns true if this Binary Tree is height-balanced, and false if it is not.
+
+// A Binary Tree is height-balanced if, for each node in the tree, the difference between the height of its left subtree and its right subtree is, at most, 1.
+
+// Each Binary Tree node has an integer value, a left child node, and a right child node. Children nodes can either be Binary Tree nodes themselves, or None/null.
+
+// Sample Input:
+// tree =       1
+//             / \
+//            2   3
+//           / \   \
+//          4   5   6
+//             / \
+//            7   8
+
+// Sample Output:
+// true
+
+// Solution 1:
+
