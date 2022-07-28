35c5a881203c888079ec7daf2f5830bde9d8cd44
Ryan Kirsch <agalloch88@gmail.com>
2022-07-28 17:19:40 -0400

    FirstDuplicateValue solution 1

--- Arrays/FirstDuplicateValue.js
+++ Arrays/FirstDuplicateValue.js
@@ -21,6 +21,28 @@
 
 // Solution 1:
 
+// interative solution using two for loops to compare values
+
+// O(n^2) time due to nested for loops and iterating over every value in array
+// O(1) space due to only storing a couple variables
+
 function firstDuplicateValue(array) {
-    
+    let minimumSecondIndex = array.length;
+
+    for (let i = 0; i < array.length; i++) {
+        let value = array[i];
+        for (let j = i + 1; j < array.length; j++) {
+            let valueToCompare = array[j];
+
+            if (value === valueToCompare) {
+                minimumSecondIndex = Math.min(minimumSecondIndex, j);
+            }
+        }
+    }
+
+    if (minimumSecondIndex === array.length) {
+        return -1;
+    }
+
+    return minimumSecondIndex;
 }
