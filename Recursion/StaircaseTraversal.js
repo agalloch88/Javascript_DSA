ad4cb1cef3e4a95974d0eb590e18901bf6704629
Ryan Kirsch <agalloch88@gmail.com>
2022-08-04 09:23:56 -0400

    StaircaseTraversal start

---
+++ Recursion/StaircaseTraversal.js
@@ -0,0 +1,23 @@
+// The problem provides two positive integers, representing the height of a staircase, and the maximum number of steps someone may advance up the staircase at a time, respectively.
+// Write a function which returns the number of ways in which someone may climb the staircase.
+
+// For example, given a staircase of height = 3, and maxSteps = 2, someone could climb the staircase in 3 ways. They could take 1 step -> 1 step -> 1 step,
+// or 1 step -> 2 steps, or 2 steps -> 1 step.
+
+// Note that, for this problem, maxSteps <= height will always be true.
+
+// Sample Input:
+// height = 4
+// maxSteps = 2
+
+// Sample Output:
+// 5
+// The staircase could be climbed in the following ways:
+// 1 -> 1 -> 1 -> 1
+// 1 -> 1 -> 2
+// 1 -> 2 -> 1
+// 2 -> 1 -> 1
+// 2 -> 2
+
+// Solution 1:
+
