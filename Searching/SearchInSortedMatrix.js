7ce3f780040a4a354f5e8ca5a90fb52d1c772bf2
Ryan Kirsch <agalloch88@gmail.com>
2022-08-05 07:23:33 -0400

    SearchInSortedMatrix start

---
+++ Searching/SearchInSortedMatrix.js
@@ -0,0 +1,21 @@
+// The problem presents a two-dimensional array (also called a matrix), which contains distinct integers, and a target integer. Each row in the matrix is sorted, and
+// each column is also sorted. The matrix does not necessarily have the same height and width.
+
+// Write a function which returns an array of the row and column indi9ces of the target integer, if said integer is actually found within the matrix.
+// If it is not found, return [-1, -1].
+
+// Sample Input:
+// matrix = [
+//      [1, 4, 7, 12, 15, 1000],
+//      [2, 5, 19, 31, 32, 10001],
+//      [3, 8, 24, 33, 35, 1002],
+//      [40,, 41, 42, 44, 45, 1003],
+//      [99, 100, 103, 106, 128, 1004],         
+//     ]
+// target = 44
+
+// Sample Output:
+// [3, 3]
+
+// Solution 1:
+
