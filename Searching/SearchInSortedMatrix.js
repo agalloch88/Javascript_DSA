630875ec9bfdb5229257b65544e1a89cf24bc86e
Ryan Kirsch <agalloch88@gmail.com>
2022-08-05 11:19:10 -0400

    SearchInSortedMatrix solution 1 comments

--- Searching/SearchInSortedMatrix.js
+++ Searching/SearchInSortedMatrix.js
@@ -19,23 +19,28 @@
 
 // Solution 1:
 
-// solution employing binary search-esque technique to discard values higher or lower than target
+// solution employing binary search-esque technique to discard values higher or lower than target, starting at top right-most value
 
 // O(n + m) time due to at most working through n columns and m rows of values to get target or return [-1, -1]
 // O(1) space due to only storing a few variables, no auxiliary data structures
 
 function searchInSortedMatrix(matrix, target) {
+    // set coordinates of where at in the matrix for x(row) and y(col)
     let row = 0;
     let col = matrix[0].length - 1;
-
+    // while within the bounds of the matrix, keep searching
     while (row < matrix.length && col >= 0) {
+        // if current value at coordinates is bigger than target, any other values in this col will also be larger, so discard rest of col
         if (matrix[row][col] > target) {
             col--;
+        // if current value at coordinates is smaller than target, rest of values to the left in this row will also be smaller, so discard rest of row
         } else if (matrix[row][col] < target) {
             row++;
+        // if not greater or smaller, must have found value, so return coordinates
         } else {
             return [row, col];
         }
     }
+    // if break out of confines of matrix without returning coordinates of value, value must not be in matrix, so return base case
     return [-1, -1];
 }
