f9b8d63a8702d6c7bcbd3891fc73123de2d9d9f4
Ryan Kirsch <agalloch88@gmail.com>
2022-08-07 10:24:27 -0400

    ValidateBST start

---
+++ BinarySearchTrees/ValidateBST.js
@@ -0,0 +1,24 @@
+// Write a function which takes in a potentially invalid Binary Search Tree (BST), and returns
+// a boolean representing whether the BST is valid.
+
+// Each BST node has an integer value, a left child node, and a right child node. A node is
+// said to be a valid BST node if and only if it satisfies the BST property: its value is
+// strictly greater than the values of every node to its left, its value is less than or equal
+// to the values of every node to its right, and its children nodes are either valid BST nodes
+// themselves, or None/null.
+
+// A BST is valid if and only if all of its nodes are valid BST nodes.
+
+// Sample Input:
+// tree =   10
+//         /  \
+//        5    15
+//       / \   / \
+//      2   5 13  22
+//     /        \
+//    1          14
+
+// Sample Output:
+// true
+
+// Solution 1:
