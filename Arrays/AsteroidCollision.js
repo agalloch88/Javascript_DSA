4d4f013acd9765c5d84f9ed48fd30e608b0a6c20
Ryan Kirsch <agalloch88@gmail.com>
2022-08-08 15:41:17 -0400

    AsteroidCollision solution 1

--- Arrays/AsteroidCollision.js
+++ Arrays/AsteroidCollision.js
@@ -15,3 +15,34 @@
 // -3 collides and is detroyed by 10, making 7, -2 collides and is destroyed by 4, making 2
 
 // Solution 1:
+
+// iterative solution which uses stack to keep track of asteroids and check conditions about them
+
+// O(n) time due to single pass over n asteroids
+// O(n) space due to storing at max n asteroids in stack
+
+function asteroidCollision(asteroids) {
+    let stack = [];
+
+    for (let i = 0; i < asteroids.length; i++) {
+        let current = asteroids[i];
+
+        if (current > 0) {
+            stack.push(current);
+        } else {
+            while (
+                stack.length &&
+                stack[stack.length - 1] > 0 &&
+                stack[stack.length - 1] < Math.abs(current)
+            ) {
+                stack.pop();
+            }
+
+            if (stack.length && stack[stack.length - 1] === Math.abs(current)) {
+                stack.pop();
+            } else if (!stack.length || stack[stack.length - 1] < 0) {
+                stack.push(current);
+            }
+        }
+    }
+}
