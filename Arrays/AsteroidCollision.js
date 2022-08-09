57b9937d13b593256ed968847a86fada88be8dc9
Ryan Kirsch <agalloch88@gmail.com>
2022-08-09 08:12:38 -0400

    AsteroidCollision solution 1 comments

--- Arrays/AsteroidCollision.js
+++ Arrays/AsteroidCollision.js
@@ -17,19 +17,23 @@
 // Solution 1:
 
 // iterative solution which uses stack to keep track of asteroids and check conditions about them
+// solves version of problem where larger asteroids cancel out/replace smaller ones, but not where computation occurs between collisions
 
 // O(n) time due to single pass over n asteroids
 // O(n) space due to storing at max n asteroids in stack
 
 function asteroidCollision(asteroids) {
+    // set up stack to keep track of asteroids
     let stack = [];
-
+    // start iterating over the asteroids array
     for (let i = 0; i < asteroids.length; i++) {
+        // grab current asteroid value
         let current = asteroids[i];
-
+        // if positive, moving to right, push number to stack
         if (current > 0) {
             stack.push(current);
         } else {
+            // if negative, while there are items in stack, and last item greated than 0, and last item in stack is less than absolute value of current, pop it out
             while (
                 stack.length &&
                 stack[stack.length - 1] > 0 &&
@@ -37,12 +41,15 @@ function asteroidCollision(asteroids) {
             ) {
                 stack.pop();
             }
-
+            // if items in stack and last item is equal to absolute value of current, pop it out, these asteroids cancel each other
             if (stack.length && stack[stack.length - 1] === Math.abs(current)) {
                 stack.pop();
+            // if no items in stack or last item is negative, push current item to stack
             } else if (!stack.length || stack[stack.length - 1] < 0) {
                 stack.push(current);
             }
         }
     }
+    // return stack as list of asteroid state
+    return stack;
 }
