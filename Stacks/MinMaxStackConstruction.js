91eb7daecdec8dc6bd003b3db45612d38df19094
Ryan Kirsch <agalloch88@gmail.com>
2022-08-11 08:56:35 -0400

    MinMaxStack solution 1 comments and push() fix

--- Stacks/MinMaxStackConstruction.js
+++ Stacks/MinMaxStackConstruction.js
@@ -35,38 +35,45 @@
 // O(1) space for all operations within the class as the operations themselves do not utilize any
 // additional space
 
+// set up class
 class MinMaxStack {
+    // create data structures to hold stack and specific min/max values for ease of reference
     constructor() {
         this.minMaxStack = [];
         this.stack = [];
     }
-
+    // looks at top value on the stack
     peek() {
         return this.stack[this.stack.length - 1];
     }
-
+    // removes top value from stack, and also the min/max
     pop() {
         this.minMaxStack.pop();
         return this.stack.pop();
     }
-
+    // adds new value on top of stack
     push(number) {
+        // set up potential new min/max values
         let newMinMax = {min: number, max: number};
-
+        // if there are already items in the stack, must compare min/max with new addition
         if (this.minMaxStack.length) {
+            // existing min/max grabbed here
             let lastMinMax = this.minMaxStack[this.minMaxStack.length - 1];
+            // take the minimum value between existing and new min
             newMinMax.min = Math.min(lastMinMax.min, number);
+            // take the maximum value between existing and new max
             newMinMax.max = Math.max(lastMinMax.max, number);
         }
-
-        this.minMaxStack.push(number);
-        this.stackk.push(number);
+        // update min/max in minMaxStack
+        this.minMaxStack.push(newMinMax);
+        // add new number to top of stack
+        this.stack.push(number);
     }
-
+    // retrieve the min value in stack
     getMin() {
         return this.minMaxStack[this.minMaxStack.length - 1].min;
     }
-
+    // retrieve the max value in stack
     getMax() {
         return this.minMaxStack[this.minMaxStack.length - 1].max;
     }
