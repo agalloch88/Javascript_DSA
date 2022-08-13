d46585c2fc3a34b6b8c75e7cbb88bc1e39af77a7
Ryan Kirsch <agalloch88@gmail.com>
2022-08-13 18:02:03 -0400

    MinHeapConstruction methods

--- Heaps/MinHeapConstruction.js
+++ Heaps/MinHeapConstruction.js
@@ -21,3 +21,47 @@
 // remove(): 2 ([6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76])
 // peek(): 6
 // insert(87): - ([6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76, 87])
+
+// Solution 1:
+
+class MinHeap {
+    constructor(array) {
+        this.heap = this.buildHeap(array);
+    }
+
+    buildHeap(array) {
+        let firstParentIdx = Math.floor((array.length - 2) / 2);
+    }
+
+    siftUp(currentIdx, heap) {
+        let parentIdx = Math.floor((currentIdx - 1) / 2);
+        while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+            this.swap(currentIdx, parentIdx, heap);
+            currentIdx = parentIdx;
+            parentIdx = Math.floor((currentIdx - 1) / 2);
+        }
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    remove () {
+        this.swap(0, this.heap.length - 1, this.heap);
+        let valueToRemove = this.heap.pop();
+        this.siftDown(0, this.heap.length - 1, this.heap);
+        return valueToRemove;
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.siftUp(this.heap.length - 1, this.heap);
+    }
+
+    swap(i, j, temp) {
+        let temp = heap[j];
+        heap[j] = heap[i];
+        heap[i] = temp;
+    }
+}
+
