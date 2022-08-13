632991901fc58eed9b51fb8dac7d10bbb030b12d
Ryan Kirsch <agalloch88@gmail.com>
2022-08-13 17:35:22 -0400

    add MinHeapConstruction, SuffixTrieConstruction files

---
+++ Heaps/MinHeapConstruction.js
@@ -0,0 +1,11 @@
+// Implement a MinHeap class which supports:
+
+// - Building a Min Heap from an input array of integers
+// - Inserting integers in the heap
+// - Removing the heap's minimum/root value
+// - Peeking at the heap's minimum/root value
+// - Sifting integers up and down the heap, which is to be used when inserting and removing values
+
+// Note that the heap should be represented in the form of an array.
+
+// Sample Usage:
