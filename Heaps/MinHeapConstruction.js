a602877310d30d813ec8b1c6378d7d7db0923776
Ryan Kirsch <agalloch88@gmail.com>
2022-08-13 17:43:09 -0400

    MinHeapConstruction sample usage

--- Heaps/MinHeapConstruction.js
+++ Heaps/MinHeapConstruction.js
@@ -9,3 +9,15 @@
 // Note that the heap should be represented in the form of an array.
 
 // Sample Usage:
+// array = [48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41]
+
+// All operations below performed sequentially
+// MinHeap(array): - (instantiates a MinHeap, calling the buildHeap method and populating the heap)
+// buildHeap(array) : - ([-5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41])
+// insert(76): - ([-5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76])
+// peek(): -5
+// remove(): -5 ([2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76])
+// peek(): 2
+// remove(): 2 ([6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76])
+// peek(): 6
+// insert(87): - ([6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76, 87])
