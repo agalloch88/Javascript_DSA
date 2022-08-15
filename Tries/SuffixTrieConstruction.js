b825fe839bb7c60d64b97458bce38b10c99621eb
Ryan Kirsch <agalloch88@gmail.com>
2022-08-15 08:09:14 -0400

    SuffixTrieConstruction solution 1

--- Tries/SuffixTrieConstruction.js
+++ Tries/SuffixTrieConstruction.js
@@ -28,3 +28,48 @@
 // Sample Output: true
 
 // Solution 1:
+
+class SuffixTrie {
+    constructor(string) {
+        this.root = {};
+        this.endSymbol = '*';
+        this.populateSuffixTreeFrom(string);
+    }
+
+    populateSuffixTreeFrom(string) {
+        for (let i = 0; i < string.length; i++) {
+            this.insertSubstringStartingAt(i, string);
+        }
+    }
+
+    insertSuffixTreeStartingAt(i, string) {
+        let node = this.root;
+
+        for (let j = i; j < string.length; j++) {
+            let letter = string[j];
+
+            if (!(letter in node)) {
+                node[letter] = {};
+            }
+
+            node = node[letter];
+        }
+
+        node[this.endSymbol] = true;
+    }
+
+    contains(string) {
+        let node = this.node;
+        
+        for (let letter of string) {
+
+            if (!(letter in node)) {
+                return false;
+            }
+
+            node = node[letter];
+        }
+
+        return this.endSymbol in node;
+    }
+}
