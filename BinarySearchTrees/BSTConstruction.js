7793ffb02d24bbc0b2f0192f01f2efd0a65d947d
Ryan Kirsch <agalloch88@gmail.com>
2022-08-17 07:28:08 -0400

    BSTConstruction solution 2 start

--- BinarySearchTrees/BSTConstruction.js
+++ BinarySearchTrees/BSTConstruction.js
@@ -131,3 +131,53 @@ class BST {
         }
     }
 }
+
+// Solution 2:
+
+// iterative solution with better space complexity as no frames on call stack
+
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(value) {
+        let currentNode = this;
+        while (true) {
+            if (value < currentNode.value) {
+                if (currentNode.left === null) {
+                    currentNode.left = new BST(value);
+                    break;
+                } else {
+                    currentNode = currentNode.left;
+                }
+            } else {
+                 if (currentNode.right === null) {
+                    currentNode.right = new BST(value);
+                    break;
+                 } else {
+                    currentNode = currentNode.right;
+                 }
+            }
+        }
+        return this;
+    }
+
+    contains(value) {
+
+    }
+
+    remove(value) {
+
+    }
+
+    getMinValue() {
+        let currentNode = this;
+        while (currentNode.left !== null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+}
