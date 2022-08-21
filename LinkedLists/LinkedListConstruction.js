8e73fa65cf6344d66c123866b8f4c39fa84f0d53
Ryan Kirsch <agalloch88@gmail.com>
2022-08-21 14:01:09 -0400

    LinkedListConstruction solution 1 setup

--- LinkedLists/LinkedListConstruction.js
+++ LinkedLists/LinkedListConstruction.js
@@ -47,3 +47,54 @@
 // return boolean for whether existing nodes contain specific value
 
 // Solution 1:
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    setHead(node) {
+
+    }
+
+    setTail(node) {
+
+    }
+
+    insertBefore(node, nodeToInsert) {
+
+    }
+
+    insertAfter(node, nodeToInsert) {
+
+    }
+
+    insertAtPosition(position, nodeToInsert) {
+
+    }
+
+    removeNodesWithValue(value) {
+
+    }
+
+    remove(node) {
+
+    }
+
+    containsNodeWithValue(value) {
+
+    }
+
+    removeNodeBindings(node) {
+        
+    }
+}
