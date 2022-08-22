c34e858b4d1d20dad7634f250e549695a416b12e
Ryan Kirsch <agalloch88@gmail.com>
2022-08-22 08:29:21 -0400

    BalancedBrackets start

---
+++ Stacks/BalancedBrackets.js
@@ -0,0 +1,14 @@
+// Write a function whcih takes in a string made up of brackets ( (, ), [, ], {, } ) and other optional characters. The function should return a boolean
+// representing whether the string is balanced with regard to the brackets.
+
+// A string is said to be balanced if it has as many opening brackets of a certain type as it has closing brackets of that type, and if no bracket is unmatched.
+//  Note that an opening bracket cannot match a corresponding closing bracket which comes before it, and similarly, a closing bracket cannot match a 
+// corresponding opening bracket which comes after it. Also, brackets cannot overlap each other, as in the examplke [(]).
+
+// Sample Input:
+// string = "([])(){}(())()()"
+
+// Sample Output:
+// true, as it is balanced
+
+// Solution 1:
