42002a8fbfcfa5b327d36b732d5253501e53f770
Ryan Kirsch <agalloch88@gmail.com>
2022-08-22 08:43:38 -0400

    BalancedBrackets solution 1

--- Stacks/BalancedBrackets.js
+++ Stacks/BalancedBrackets.js
@@ -12,3 +12,32 @@
 // true, as it is balanced
 
 // Solution 1:
+
+// iterative solution utilizing stack to add starting brackets, then pop them off when encountering closing bracket
+
+// O(n) time due to iterating over n items in input string
+// O(n) space due to storing at most n items in stack; balanced solutions will not reach full O(n), but n/2 still converges to O(n)
+
+function balancedBrackets(string) {
+    let startingBrackets = "([{";
+    let endingBrackets = ")]}";
+    let matchingBrackets = { ')': '(', ']': '[', '}': '{'};
+    let stack = [];
+
+    for (let char of string) {
+        if (startingBrackets.includes(char)) {
+            stack.push(char);
+        } else if (endingBrackets.includes(char)) {
+            if (stack.length === 0) {
+                return false;
+            }
+
+            if (stack[stack.length - 1] === matchingBrackets[char]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
