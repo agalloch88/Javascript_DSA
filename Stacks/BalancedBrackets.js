4afb94fc0e837ab94ef39aa1d1b67874d33b93e0
Ryan Kirsch <agalloch88@gmail.com>
2022-08-22 08:52:27 -0400

    BalancedBrackets solution 1 comments

--- Stacks/BalancedBrackets.js
+++ Stacks/BalancedBrackets.js
@@ -19,25 +19,33 @@
 // O(n) space due to storing at most n items in stack; balanced solutions will not reach full O(n), but n/2 still converges to O(n)
 
 function balancedBrackets(string) {
+    // set up strings for start and end chars to match against
     let startingBrackets = "([{";
     let endingBrackets = ")]}";
+    // set up hashmap/JS object of start values and their corresponding end key
     let matchingBrackets = { ')': '(', ']': '[', '}': '{'};
+    // initialize empty holder stack
     let stack = [];
-
+    // start iterating over every character in input string
     for (let char of string) {
+        // if current character is a starting bracket, add it to the stack via push
         if (startingBrackets.includes(char)) {
             stack.push(char);
+        // if not starting bracket, it may be an ending bracket, so if current character is an ending bracket, have to do a few checks
         } else if (endingBrackets.includes(char)) {
+            // if there's nothing in the stack currently and there's an ending bracket, no previously-encounter starting bracket to cancel it out, so return false
             if (stack.length === 0) {
                 return false;
             }
-
+            // if the last/top item in the stack, which in this case would be a starting bracket, is an exact match for this closing bracket, pop the opening bracket off the stack
             if (stack[stack.length - 1] === matchingBrackets[char]) {
                 stack.pop();
+            // if not a match, then return false
             } else {
                 return false;
             }
         }
     }
+    // last boolean check, and if any remaining items in the stack which were not handled above, will return false, otherwise true
     return stack.length === 0;
 }
