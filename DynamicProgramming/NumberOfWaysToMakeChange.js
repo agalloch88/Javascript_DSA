ff51d53b0a2ebbcc0dbe0d47c4b63815d94451af
Ryan Kirsch <agalloch88@gmail.com>
2022-08-28 08:50:28 -0400

    NumberOfWaysToMakeChange start

---
+++ DynamicProgramming/NumberOfWaysToMakeChange.js
@@ -0,0 +1,14 @@
+// Given an array of distinct positive integers representing coin denominations, and a single,
+// non-negative integer n representing a target amount of money, write a function which returns
+// the number of ways to make change for that target amount using the given coin denominations.
+
+// Note that an unlimited amount of coins is at disposal for this question.
+
+// Sample Input:
+// n = 6
+// denoms = [1, 5]
+
+// Sample Output:
+// 2, being 1 x 1 and 1 x 5, and 6 x 1
+
+// Solution 1:
