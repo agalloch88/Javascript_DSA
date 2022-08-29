a0ace4e32a3b4ccce2b9f356bb2705171e419f06
Ryan Kirsch <agalloch88@gmail.com>
2022-08-29 07:18:30 -0400

    NumberOfWaysToMakeChange solution 1 comments

--- DynamicProgramming/NumberOfWaysToMakeChange.js
+++ DynamicProgramming/NumberOfWaysToMakeChange.js
@@ -13,16 +13,25 @@
 
 // Solution 1:
 
-function numberOfWaysToMakeChange(n, denoms) {
-    let ways = new Array(n + 1).fill(0);
-    ways[0] = 1;
+// O(n * d) time, where is the number of denoms given as have to iterate over n potential values using d denoms
+// O(n) space due to creating new placeholder array to trackt he number of ways
 
+function numberOfWaysToMakeChange(n, denoms) {
+    // initialize empty array to hold all values from 0 to n
+    let ways = new Array(n + 1).fill(0);
+    // only way to get 0 is to do nothing, so initialize it first
+    ways[0] = 1;
+    // for each denom given, keep going
     for (let denom of denoms) {
+        // start the amounts at 1 and keep going
         for (let amount = 1; amount < n + 1; amount++) {
+            // if the denom is less than or equal to current amount, possible to make change
             if (denom <= amount) {
+                // update the value in the ways array to be the difference between amount and denom, add it to exisitng ways found
                 ways[amount] += ways[amount - denom];
             }
         }
     }
+    // after looping, should have final number of ways at value for n in ways array, so return it
     return ways[n];
 }
