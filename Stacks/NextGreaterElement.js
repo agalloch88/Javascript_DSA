7cfd2a76b132039ef1f1b6bc8c3da3a12b60a6bd
Ryan Kirsch <agalloch88@gmail.com>
2022-08-31 16:08:58 -0400

    NextGreaterElement solution 1 comments

--- Stacks/NextGreaterElement.js
+++ Stacks/NextGreaterElement.js
@@ -17,23 +17,30 @@
 
 // Solution 1:
 
-// iterative solution using a stack and looping through the input on two passes
+// iterative solution using a stack and looping through the input on two passes, starting from left/beginning of array in this solution
 
 // O(n) time due to looping over n items twice with additional n checks, so 3n converges to n
 // O(n) space due to storing n additional items in new result array and new stack, so 2n converges to n
 
 function nextGreaterElement(array) {
+    // initialize new array of input length, fill with -1's to start
     let result = new Array(array.length).fill(-1);
+    // will keep track of largest value and values which need to be replaced with said value via a stack
     let stack = [];
-
+    // iterate through the array twice to ensure everything is correct after second pass, account for largest element
     for (let idx = 0; idx < 2 * array.length; idx++) {
+        // to allow circular traversal, use modulo operator to provide a circularIdx value
         let circularIdx = idx % array.length;
-
+        // while there are items in the stack and the value on top of the stack is less than the current value at the circularIdx, do stuff
         while (stack.length > 0 && array[stack[stack.length - 1]] < array[circularIdx]) {
+            // keep track of top value and pop it off
             let top = stack.pop();
+            // every item in result with the value of top will not be set to current item in array at the circularIdx
             result[top] = array[circularIdx];
         }
+        // push item on to the stack and keep doing this until passing through the array twice
         stack.push(circularIdx);
     }
+    // return the final result array
     return result;
 }
