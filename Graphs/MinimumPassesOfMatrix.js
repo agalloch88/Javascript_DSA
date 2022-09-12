eba305d2c333beea7ccc3d0febf89ee8539d3703
Ryan Kirsch <agalloch88@gmail.com>
2022-09-12 09:15:37 -0400

    MinimumPassesOfMatrix solution 2 comments

--- Graphs/MinimumPassesOfMatrix.js
+++ Graphs/MinimumPassesOfMatrix.js
@@ -181,79 +181,106 @@ function containsNegative(matrix) {
 
 // Solution 2:
 
+// recursive solution using queue rather than stack, tracking passes and number of items in the queue to determine when done
+
+// O(w * h) time complexity where w is width and h is height of matrix, plus O(n) shift operations, where 2n converges to O(n)
+// O(w * h) space due to using queue, etc
+
 function minimumPassesOfMatrix(matrix) {
+    // set variable to result of passes of helper function, which converts negatives at each pass
     let passes = convertNegatives(matrix);
+    // return result of check to see if any negatives remain
     return !containsNegative(matrix) ? passes - 1 : -1;
 }
 
 function convertNegatives(matrix) {
-    let stack = getAllPositivePositions(matrix);
-
+    // create queue for results of all positive positions
+    let queue = getAllPositivePositions(matrix);
+    // create variable for how many passes/iterations through matrix occur
     let passes = 0;
-
-    while (stack.length > 0) {
+    // while there are items in the queue, do stuff
+    while (queue.length > 0) {
+        // create currentSize variable to track how many items left to process in the queue
         let currentSize = stack.length;
-
+        // when no more items to process, while loop will break
         while (currentSize > 0) {
-            let [currentRow, currentCol] = stack.pop();
-
+            // shift front item out of queue and deconstruct to grab row/col position
+            let [currentRow, currentCol] = queue.shift();
+            // store adjacent positions in variable coming back from helper
             let adjacentPositions = getAdjacentPositions(currentRow, currentCol, matrix);
             for (let position of adjacentPositions) {
+                // for current position, deconstruct the row/col
                 let [row, col] = position;
-
+                // set variable value equal to current number at row/col coordinates in matrix
                 let value = matrix[row][col];
+                // if value is less than zero, dealing with a negative, so turn it positive
                 if (value < 0) {
                     matrix[row][col] *= -1;
-                    stack.push([row, col]);
+                    // push this new value into the queue to then look at its neighbors
+                    queue.push([row, col]);
                 }
             }
+            // dealt with one item in queue, so decrement currentSize variable
             currentSize--;
         }
+        // completed one pass, so increment passes variable
         passes++;
     }
+    // when done with entire queue, while loop exits so return passes variable
     return passes;
 }
 
 function getAllPositivePositions(matrix) {
+    // set up holder array to find all positive integer positions
     let positivePositions = [];
-
+    // for every item in the matrix, will check to see if positive
     for (let row = 0; row < matrix.length; row++) {
         for (let col = 0; col < matrix[row].length; col++) {
+            // store the numeric value contained at coordinates in a variable
             let value = matrix[row][col];
+            // if this item is greater than 0, positive integer found, so push to array
             if (value > 0) {
                 positivePositions.push([row, col]);
             }
         }
     }
+    // return array of all positive positions found
     return positivePositions;
 }
 
 function getAdjacentPositions(row, col, matrix) {
+    // create holder array for valid adjacent items
     let adjacentPositions = [];
-
+    // check for valid adjacent position above current item, and if there, push to array
     if (row > 0) {
         adjacentPositions.push([row - 1, col]); // UP
     }
+    // check for valid adjacent position below current item, and if there, push to array
     if (row < matrix.length - 1) {
         adjacentPositions.push([row + 1, col]); // DOWN
     }
+    // check for valid adjacent position left of current item, and if there, push to array
     if (col > 0) {
         adjacentPositions.push([row, col - 1]); // LEFT
     }
+    // check for valid adjacent position right of current item, and if there, push to array
     if (col < matrix[row].length - 1) {
         adjacentPositions.push([row, col + 1]); // RIGHT
     }
-
+    // return results of these check
     return adjacentPositions;
 }
 
 function containsNegative(matrix) {
+    // in every row of matrix, check every value
     for (let row of matrix) {
         for (let value of row) {
+            // if value of an item less than 0, still have a negative, so return true
             if (value < 0) {
                 return true;
             }
         }
     }
+    // if no hits, all negatives were handled, so return false as no negatives
     return false;
 }
