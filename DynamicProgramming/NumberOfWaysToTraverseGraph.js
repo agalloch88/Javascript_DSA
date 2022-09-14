2935ff05da4cdc52868f6e50431a8eaeb0153b4a
Ryan Kirsch <agalloch88@gmail.com>
2022-09-14 07:01:30 -0400

    NumberOfWaysToTraverseGraph solution 1

--- DynamicProgramming/NumberOfWaysToTraverseGraph.js
+++ DynamicProgramming/NumberOfWaysToTraverseGraph.js
@@ -23,3 +23,11 @@
 // 10
 
 // Solution 1:
+
+function numberOfWaysToTraverseGraph(width, height) {
+    if (width === 1 || height === 1) {
+        return 1;
+    }
+
+    return numberOfWaysToTraverseGraph(width - 1, height) + numberOfWaysToTraverseGraph(width, height - 1);
+}
