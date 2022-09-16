cf65332e8b1c1007a4b2ba1fb53f34b84f6787ee
Ryan Kirsch <agalloch88@gmail.com>
2022-09-16 09:36:41 -0400

    LinkedListConstruction solution 1

--- LinkedLists/LinkedListConstruction.js
+++ LinkedLists/LinkedListConstruction.js
@@ -63,38 +63,109 @@ class DoublyLinkedList {
     }
 
     setHead(node) {
-
+        if (this.head === null) {
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+        this.insertBefore(this.head, node);
     }
 
     setTail(node) {
-
+        if (this.tail === null) {
+            this.setHead(node);
+            return;
+        }
+        this.insertAfter(this.tail, node);
     }
 
     insertBefore(node, nodeToInsert) {
+        if (nodeToInsert === this.head && nodeToInsert === this.tail) {
+            return;
+        }
+        this.remove(nodeToInsert);
+        nodeToInsert.prev = node.prev;
+        nodeToInsert.next = node;
 
+        if (node.prev === null) {
+            this.head = nodeToInsert;
+        } else {
+            node.prev.next = nodeToInsert;
+        }
+        node.prev = nodeToInsert;
     }
 
     insertAfter(node, nodeToInsert) {
+        if (nodeToInsert === this.head && nodeToInsert === this.tail) {
+            return;
+        }
+        this.remove(nodeToInsert);
+        nodeToInsert.prev = node;
+        nodeToInsert.next = node.next;
 
+        if (node.next === null) {
+            this.tail = nodeToInsert;
+        } else {
+            node.next.prev = nodeToInsert;
+        }
+        node.next = nodeToInsert;
     }
 
     insertAtPosition(position, nodeToInsert) {
+        if (position === 1) {
+            this.setHead(nodeToInsert);
+            return;
+        }
+        let node = this.head;
+        let currentPosition = 1;
 
+        while (node !== null && currentPosition++ !== position) {
+            node = node.next;
+        }
+        if (node !== null) {
+            this.insertBefore(node, nodeToInsert);
+        } else {
+            this.setTail(nodeToInsert);
+        }
     }
 
     removeNodesWithValue(value) {
-
+        let node = this.head;
+        while (node !== null) {
+            let nodeToRemove = node;
+            node = node.next;
+            if (nodeToRemove.value === value) {
+                this.remove(nodeToRemove);
+            }
+        }
     }
 
     remove(node) {
-
+        if (node === this.head) {
+            this.head = this.head.next;
+        }
+        if (node === this.tail) {
+            this.tail = this.tail.prev;
+        }
+        this.removeNodeBindings(node);
     }
 
     containsNodeWithValue(value) {
-
+        let node = this.head;
+        while (node !== null && node.value !== value) {
+            node = node.next;
+        }
+        return node !== null;
     }
 
     removeNodeBindings(node) {
-        
+        if (node.prev !== null) {
+            node.prev.net = node.next;
+        }
+        if (node.next !== null) {
+            node.next.prev = node.prev;
+        }
+        node.prev = null;
+        node.next = null;
     }
 }
