96354e8530bf11893bf0d7f249117224b51b8224
Ryan Kirsch <agalloch88@gmail.com>
2022-09-21 07:18:45 -0400

    SumOfLinkedLists solution 1

--- LinkedLists/SumOfLinkedLists.js
+++ LinkedLists/SumOfLinkedLists.js
@@ -18,3 +18,35 @@
 // 1742 + 549 = 2291
 
 // Solution 1:
+
+class LinkedList {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+function sumOfLinkedLists(linkedListOne, linkedListTwo) {
+    let newLinkedListHeadPointer = new LinkedList(0);
+    let currentNode = newLinkedListHeadPointer;
+    let carry = 0;
+
+    let nodeOne = linkedListOne;
+    let nodeTwo = linkedListTwo;
+
+    while (nodeOne !== null || nodeTwo !== null || carry !== 0) {
+        let valueOne = nodeOne !== null ? nodeOne.value : 0;
+        let valueTwo = nodeTwo !== null ? nodeTwo.value : 0;
+        let sumOfValues = valueOne + valueTwo + carry;
+
+        let newValue = sumOfValues % 10;
+        let newNode = new LinkedList(value);
+        currentNode.next = newNode;
+        currentNode = newNode;
+
+        carry = Math.floor(sumOfValues / 10);
+        nodeOne = nodeOne !== null ? nodeOne.next : null;
+        nodeTwo = nodeTwo !== null ? nodeTwo.next : null;
+    }
+    return newLinkedListHeadPointer.next;
+}
