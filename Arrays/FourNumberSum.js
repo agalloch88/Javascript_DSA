9e3517e3420ea5a6beda8aa155f0640fc00befd2
Ryan Kirsch <agalloch88@gmail.com>
2022-09-26 17:45:44 -0400

    FourNumberSum start

---
+++ Arrays/FourNumberSum.js
@@ -0,0 +1,13 @@
+// Write a function which takes in a non-empty array of distinct integers, and an integer which represents a target sum. The function should find all quadruplets in the array
+// which add up to the target sum, and should then return a two-dimensional array of all the found quadruplets in no particular order.
+
+// If no four numbers in the input array sum up to the target sum, the function should return an empty array.
+
+// Sample Input:
+// array = [7, 6, 4, -1, 1, 2]
+// targetSum = 16
+
+// Sample Output:
+//[[7, 6, 4, -1], [7, 6, 1, 2]]
+
+// Solution 1:
