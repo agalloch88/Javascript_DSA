173284364d8e69a36cad202db5e2ce496419384e
Ryan Kirsch <agalloch88@gmail.com>
2022-09-27 06:38:22 -0400

    FourNumberSum solution 1

--- Arrays/FourNumberSum.js
+++ Arrays/FourNumberSum.js
@@ -11,3 +11,33 @@
 //[[7, 6, 4, -1], [7, 6, 1, 2]]
 
 // Solution 1:
+
+function fourNumberSum(array, targetSum) {
+    let allPairSums = {};
+    let quadruplets = [];
+
+    for (let i = 1; i < array.length - 1; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            let currentSum = array[j] + array[i];
+            let difference = targetSum - currentSum;
+
+            if (difference in allPairSums) {
+                for (let pair of allPairSums[difference]) {
+                    quadruplets.push(pair.concat([array[i], array[j]]));
+                }
+            }
+        }
+
+        for (let k = 0; k < i; k++) {
+            let currentSum = array[i] + array[k];
+
+            if (!(currentSum in allPairSums)) {
+                allPairSums[currentSum] = [[array[k], array[i]]];
+            } else {
+                allPairSums[currentSum].push([array[k], array[i]]);
+            }
+        }
+    }
+
+    return quadruplets;
+}
