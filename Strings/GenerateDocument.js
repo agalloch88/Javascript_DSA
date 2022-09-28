f189829cd8b1d373cb90bbb2662253c7a22b0a91
Ryan Kirsch <agalloch88@gmail.com>
2022-09-28 09:13:22 -0400

    GenerateDocument solution 1 comments

--- Strings/GenerateDocument.js
+++ Strings/GenerateDocument.js
@@ -17,25 +17,38 @@
 
 // Solution 1:
 
-function generateDocument(characters, document) {
-    for (let character of document) {
-        let documentFrequency = countCharacterFrequency(character, document);
-        let charactersFrequency = countCharacterFrequency(character, characters);
+// iterative solution counting frequencies of characters in characters and document inputs, then comparing them
 
+// O(m * (n + m)) time due to comparing the two strings for each item in document repeatedly
+// O(1) space due to only storing a few variables and frequency for each value
+
+// main function taking in the provided characters and document strings
+function generateDocument(characters, document) {
+    // iterating over each single character in the provided document, need to compare frequency of the specific character between input characters and input document
+    for (let character of document) {
+        // set variable documentFrequency equal to result of helper function counting frequency of specific character in input document
+        let documentFrequency = countCharacterFrequency(character, document);
+        // set variable charactersFrequency equal to result of helper function counting frequency of specific character in input characters
+        let charactersFrequency = countCharacterFrequency(character, characters);
+        // if, for any character in the input document, there is a higher count in the document than what is provided in the input characters, return false, since the document cannot be generated
         if (documentFrequency > charactersFrequency) {
             return false;
         }
     }
+    // if both inputs were iterated successfully and the if condition above never triggered, document can be generated from input characters, so return true
     return true;
 }
-
+// helper function solely responsible for targeting a specific character and counting the frequency of the specific character
 function countCharacterFrequency(character, target) {
+    // initialize frequency variable to 0
     let frequency = 0;
-
+    // for every character in the target, do stuff
     for (let char of target) {
-        if (char === target) {
+        // if the current char is equal to the character currently being looked at, increment the frequency of that char by 1
+        if (char === character) {
             frequency++;
         }
     }
+    // return the frequency of this char so it can be compared across the characters and document in above function
     return frequency;
 }
