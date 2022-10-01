c1ea16cf3e4b3521392d4361c87daee18e2b5efb
Ryan Kirsch <agalloch88@gmail.com>
2022-10-01 10:29:28 -0400

    SubarraySort solution 1

--- Arrays/SubarraySort.js
+++ Arrays/SubarraySort.js
@@ -10,3 +10,45 @@
 // [3, 9]
 
 // Solution 1:
+
+function subarraySort(array) {
+    let minOutOfOrder = Infinity;
+    let maxOutOfOrder = -Infinity;
+
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+
+        if (isOutOfOrder(i, num, array)) {
+            minOutOfOrder = Math.min(minOutOfOrder, num);
+            maxOutOfOrder = Math.max(maxOutOfOrder, num);
+        }
+    }
+
+    if (minOutOfOrder === Infinity) {
+        return [-1. -1];
+    }
+
+    let subarrayLeftIdx = 0;
+    while (minOutOfOrder >= array[subarrayLeftIdx]) {
+        subarrayLeftIdx++;
+    }
+
+    let subarrayRightIdx = array.length - 1;
+    while (maxOutOfOrder <= array[subarrayRightIdx]) {
+        subarrayRightIdx--;
+    }
+
+    return [subarrayLeftIdx, subarrayRightIdx];
+}
+
+function isOutOfOrder(i, num, array) {
+    if (i === 0) {
+        return num > array[i + 1];
+    }
+
+    if (i === array.length - 1) {
+        return num < array[i - 1];
+    }
+
+    return num > array[i + 1] || num < array[i - 1];
+}
