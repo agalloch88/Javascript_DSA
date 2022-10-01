5bfa53d1cc8bae52312ddbbdb33163d8b210cbad
Ryan Kirsch <agalloch88@gmail.com>
2022-10-01 10:22:50 -0400

    SubarraySort start

---
+++ Arrays/SubarraySort.js
@@ -0,0 +1,12 @@
+// Write a function which takes in an array of at least two integers, and which returns an array containing the starting and ending indices of the smallest subarray contained
+// inside the input array which needs to be sorted in place in order for the entire input array to be sorted (in ascending order).
+
+// If the input array is already sorted, the function should return [-1, -1].
+
+// Sample Input:
+// array = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+
+// Sample Output:
+// [3, 9]
+
+// Solution 1:
