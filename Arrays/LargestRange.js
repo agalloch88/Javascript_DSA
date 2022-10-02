4e1af0e20bc12ccea3f92ce601d8b42cf41464dd
Ryan Kirsch <agalloch88@gmail.com>
2022-10-02 09:59:03 -0400

    LargestRange solution 1

--- Arrays/LargestRange.js
+++ Arrays/LargestRange.js
@@ -16,3 +16,43 @@
 // [0, 7]
 
 // Solution 1:
+
+function largestRange(array) {
+    let bestRange = [];
+    let longestLength = 0;
+    let nums = {};
+    
+    for (let num of array) {
+        nums[num] = true;
+    }
+
+    for (let num of array) {
+        if (!nums[num]) {
+            continue;
+        }
+        
+        nums[num] = false;
+        let currentLength = 1;
+        let left = num - 1;
+        let right = num + 1;
+
+        while (left in nums) {
+            nums[left] = false;
+            currentLength++;
+            left--;
+        }
+
+        while (right in nums) {
+            nums[right] = false;
+            currentLength++;
+            right++;
+        }
+
+        if (currentLength > longestLength) {
+            longestLength = currentLength;
+            bestRange = [left + 1, right - 1];
+        }
+    }
+
+    return bestRange;
+}
