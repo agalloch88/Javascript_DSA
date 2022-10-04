9d22c16b660fbebfe5aaa7e1e1109561abaf6d1d
Ryan Kirsch <agalloch88@gmail.com>
2022-10-04 09:07:19 -0400

    LongestPalindromicSubstring solution 1

--- Strings/LongestPalindromicSubstring.js
+++ Strings/LongestPalindromicSubstring.js
@@ -12,3 +12,29 @@
 // "xyzzyx"
 
 // Solution 1:
+
+function longestPalindromicSubstring(string) {
+    let longest = '';
+    for (let i = 0; i < string.length; i++) {
+        for (let j = i; j < string.length; j++) {
+            let substring = string.slice(i, j + 1);
+            if (substring.length > longest.length && isPalindrome(substring)) {
+                longest = substring;
+            }
+        }
+    }
+    return longest;
+}
+
+function isPalindrome(string) {
+    let leftIdx = 0;
+    let rightIdx = string.length - 1;
+    while (leftIdx < rightIdx) {
+        if (string[leftIdx] !== string[rightIdx]) {
+            return false;
+        }
+        leftIdx++;
+        rightIdx--;
+    }
+    return true;
+}
