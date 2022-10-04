9ea27a5eb035770afa90e8434353ab4b4f1e3fd4
Ryan Kirsch <agalloch88@gmail.com>
2022-10-04 09:00:43 -0400

    LongestPalindromicSubstring start

---
+++ Strings/LongestPalindromicSubstring.js
@@ -0,0 +1,14 @@
+// Write a function which, given an input string, returns its longest palindromic substring.
+
+// A palidrome is defined as a string of characters which is written the same forward and backward. Note that, in the context of the problem, single-character strings
+// are palindromes.
+
+// For the context of the problem, assume there will only be one longest palindromic substring.
+
+// Sample Input:
+// string = "abaxyzzyxf"
+
+// Sample Output:
+// "xyzzyx"
+
+// Solution 1:
