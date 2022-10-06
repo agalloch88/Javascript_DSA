4342aed7caff52b599d61e8a127bc71bb584062a
Ryan Kirsch <agalloch88@gmail.com>
2022-10-06 10:26:55 -0400

    LongestPalindromicSubstring solution 2

--- Strings/LongestPalindromicSubstring.js
+++ Strings/LongestPalindromicSubstring.js
@@ -58,3 +58,30 @@ function isPalindrome(string) {
     // for use in above function checks
     return true;
 }
+
+// Solution 2 
+
+function longestPalindromicSubstring(string) {
+    let currentLongest = [0, 1];
+
+    for (let i = 1; i < string.length; i++) {
+        let odd = getLongestPalindromeFrom(string, i - 1, i + 1);
+        let even = getLongestPalindromeFrom(string, i - 1, i);
+        let longest = odd[1] - odd[0] > even[1] - even [0] ? odd : even;
+
+        currentLongest = currentLongest[1] - currentLongest[0] > longest[1] - longest[0] ? currentLongest : longest;
+    }
+
+    return string.slice(currentLongest[0], currentLongest[1]);
+}
+
+function getLongestPalindromeFrom(string, leftIdx, rightIdx) {
+    while (leftIdx >= 0 && rightIdx < string.length) {
+        if (string[leftIdx] !== string[rightIdx]) {
+            break;
+        }
+        leftIdx++;
+        rightIdx--;
+    }
+    return [leftIdx + 1, rightIdx];
+}
