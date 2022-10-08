3bdb876f5834313829adfb24ebc0fbbb2ce320c5
Ryan Kirsch <agalloch88@gmail.com>
2022-10-08 15:28:48 -0400

    GroupAnagrams solution 1

--- Strings/GroupAnagrams.js
+++ Strings/GroupAnagrams.js
@@ -12,3 +12,45 @@
 // [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
 
 // Solution 1:
+
+function groupAnagrams(words) {
+    if (words.length === 0) {
+        return [];
+    }
+
+    let sortedWords = words.map(word => word.split('').sort().join(''));
+    let indices = [...Array(words.length).keys()];
+
+    indices.sort((a, b) => {
+        if (sortedWords[a] < sortedWords[b]) {
+            return -1;
+        }
+        if (sortedWords[a] > sortedWords[b]) {
+            return 1;
+        }
+
+        return 0;
+    });
+
+    let result = [];
+    let currentAnagramGroup = [];
+    let currentAnagram = sortedWords[indices[0]];
+
+    for (let index of indices) {
+        let word = words[index];
+        let sortedWord = sortedWords[index];
+
+        if (sortedWord === currentAnagram) {
+            currentAnagramGroup.push(word);
+            continue;
+        }
+
+        result.push(currentAnagramGroup);
+        currentAnagramGroup = [word];
+        currentAnagram = sortedWord;
+    }
+
+    result.push(currentAnagramGroup);
+
+    return result;
+}
