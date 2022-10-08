155d8839a4411a1d8b3818c4e6a7b4f881696066
Ryan Kirsch <agalloch88@gmail.com>
2022-10-08 15:20:50 -0400

    GroupAnagrams start

---
+++ Strings/GroupAnagrams.js
@@ -0,0 +1,14 @@
+// Write a function which takes in an array of strings and groups anagrams together.
+
+// Anagrams are strings made up of the same letters, where order does not matter. For example,
+// "cinema" and "iceman" are anagrams. Similarly, "foo" and "ofo" are anagrams.
+
+// The function should return a list of anagram groups in no particular order.
+
+// Sample Input:
+// words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+
+// Sample Output:
+// [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
+
+// Solution 1:
