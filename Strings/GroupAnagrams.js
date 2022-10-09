495ff14052fedfca2653ba3a268a9ade99b521fc
Ryan Kirsch <agalloch88@gmail.com>
2022-10-09 11:26:18 -0400

    GroupAnagrams solution 1 comments

--- Strings/GroupAnagrams.js
+++ Strings/GroupAnagrams.js
@@ -13,14 +13,21 @@
 
 // Solution 1:
 
+// iterative solution storing sorted words and matching them to anagrams
+
+// O(w * n * log(n) + n * w * log(w)) time due to sorting and looking at w words at potential max length of n
+// O(wn) space due to storing w words of potentially n length each in new arrays
+
 function groupAnagrams(words) {
+    // check to see if the words is empty, which is an edge case, and return empty array if so
     if (words.length === 0) {
         return [];
     }
-
+    // set up variable to store words, map over the words to sort then join them again
     let sortedWords = words.map(word => word.split('').sort().join(''));
+    // generate the indexes by spreading an array of words.length length and grabbing the keys
     let indices = [...Array(words.length).keys()];
-
+    // sort the indices to determine the order they should go in
     indices.sort((a, b) => {
         if (sortedWords[a] < sortedWords[b]) {
             return -1;
@@ -31,26 +38,32 @@ function groupAnagrams(words) {
 
         return 0;
     });
-
+    // set up holder arrays for result, currentAnagram group, and the currentAnagram starting at
+    // index 0 of the sortedWords
     let result = [];
     let currentAnagramGroup = [];
     let currentAnagram = sortedWords[indices[0]];
-
+    // iterate over the items in indices
     for (let index of indices) {
+        // store item in words at current index in variable word
         let word = words[index];
+        // grab item in sortedWords at current index and store in variable sortedWord
         let sortedWord = sortedWords[index];
-
+        // if sortedWord and currentAnagram are equal, then push this word into the currentAnagramGroup
+        // then continue
         if (sortedWord === currentAnagram) {
             currentAnagramGroup.push(word);
             continue;
         }
-
+        // once done, push the currentAnagramGroup into result
         result.push(currentAnagramGroup);
+        // set currentAnagramGroup equal to word
         currentAnagramGroup = [word];
+        // set currentAnagram equal to the sortedWord
         currentAnagram = sortedWord;
     }
-
+    // after finished looping, push the currentAnagramGroup to the result
     result.push(currentAnagramGroup);
-
+    // once done, return the full result array
     return result;
 }
