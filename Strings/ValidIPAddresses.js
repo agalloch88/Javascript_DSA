96b8da314900744883f3a2bc04a00f8aaf3e86c8
Ryan Kirsch <agalloch88@gmail.com>
2022-10-11 08:17:25 -0400

    ValidIPAddresses solution 1

--- Strings/ValidIPAddresses.js
+++ Strings/ValidIPAddresses.js
@@ -31,3 +31,50 @@
 // ]
 
 // Solution 1:
+
+// iterative solution checking for lengths and valid IP address values
+
+// O(1) time due to simple value checks
+// O(1) space due to storing parts then full strings in array
+
+function validIPAddresses(string) {
+    let ipAddressesFound = [];
+
+    for (let i = 0; i < Math.min(string.length, 4); i ++) {
+        let currentIPAddressParts = ['', '', '', ''];
+
+        currentIPAddressParts[0] = string.slice(0, i);
+
+        if (!isValidPart(currentIPAddressParts[0])) {
+            continue;
+        }
+
+        for (let j = i + 1; j < i + Math.min(string.length - i, 4); j ++) {
+            currentIPAddressParts[1] = string.slice(i, j);
+
+            if (!isValidPart(currentIPAddressParts[1])) {
+                continue;
+            }
+
+            for (let k = j + 1; k < j + Math.min(string.length - j, 4); k++) {
+                currentIPAddressParts[2] = string.slice(j, k);
+                currentIPAddressParts[3] = string.slice(k);
+
+                if (isValidPart(currentIPAddressParts[2]) && isValidPart(currentIPAddressParts[3])) {
+                    ipAddressesFound.push(currentIPAddressParts.join('.'));
+                }
+            }
+        }
+    }
+    return ipAddressesFound;
+}
+
+function isValidPart(string) {
+    const stringAsInt = parseInt(string);
+    
+    if (stringAsInt > 255) {
+        return false;
+    }
+
+    return string.length === stringAsInt.toString().length;
+}
