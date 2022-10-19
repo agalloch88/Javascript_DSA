e1286cbfe9573713a4b362934027e46036a6d50b
Ryan Kirsch <agalloch88@gmail.com>
2022-10-19 07:14:55 -0400

    FindLoop solution 1

--- LinkedLists/FindLoop.js
+++ LinkedLists/FindLoop.js
@@ -14,3 +14,29 @@
 // The node with the value 4
 
 // Solution 1:
+
+class LinkedList {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+function findLoop(head) {
+    let first = head.next;
+    let second = head.next.next;
+
+    while (first !== second) {
+        first = first.next;
+        second = second.next.next;
+    }
+
+    first = head;
+
+    while (first !== second) {
+        first = first.next;
+        second = second.next;
+    }
+
+    return first;
+}
