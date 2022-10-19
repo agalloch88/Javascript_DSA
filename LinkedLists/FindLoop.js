55b51472f80f9a3187bcffd3cc94764b3b67993c
Ryan Kirsch <agalloch88@gmail.com>
2022-10-19 07:31:47 -0400

    FindLoop solution 1 comments

--- LinkedLists/FindLoop.js
+++ LinkedLists/FindLoop.js
@@ -15,28 +15,40 @@
 
 // Solution 1:
 
+// iterative solution using two pointers and algebraic principles to determine where pointers will overlap as start of the loop
+
+// O(n) time due to iterating over all values in Linked List, 2n converges to n
+// O(1) space due to only storing two variables, no auxillary data structures per requirement of constant space
+
+// main LinkedList class, showing properties of value and next on each node
 class LinkedList {
     constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
-
+// main function, which takes in a parameter labeled head which is the start of the Linked list
 function findLoop(head) {
+    // set up first pointer, and set equal to head.next
     let first = head.next;
+    // set up second pointer, and set it next to first pointer so while condition below will not immediately exit
     let second = head.next.next;
-
+    // start iterating so long as the pointers do not overlap
     while (first !== second) {
+        // keep incrementing the first pointer by one value
         first = first.next;
+        // increment the second pointer by 2x that of the first, such that the second pointer will eventually lap the first and the while loop will break
         second = second.next.next;
     }
-
+    // when the while loop above breaks, set the first pointer back to the head, now will increment to find where they overlap again and should be the loop start point
     first = head;
-
+    // start another while loop which will exit when the pointers once again overlap. the second pointer is within the loop at this point
     while (first !== second) {
+        // keep incrementing the first pointer by one
         first = first.next;
+        // this time, increment the second pointer by one, too, so they are moving at the same place the overlap/converge at the loop start
         second = second.next;
     }
-
+    // when the loop above exits, return wherever the first pointer is
     return first;
 }
