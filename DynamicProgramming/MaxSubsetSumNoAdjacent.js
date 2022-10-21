397f71effa4e720aa4fda1c16c25f497e25f1dea
Ryan Kirsch <agalloch88@gmail.com>
2022-10-21 15:09:34 -0400

    MaxSubsetSumNoAdjacent solution 1

--- DynamicProgramming/MaxSubsetSumNoAdjacent.js
+++ DynamicProgramming/MaxSubsetSumNoAdjacent.js
@@ -1,4 +1,4 @@
-// Write a function which takes in an array of positive integers and returns the maximum sum od non-adjacent elements in the array.
+// Write a function which takes in an array of positive integers and returns the maximum sum of non-adjacent elements in the array.
 
 // If the input array is empty, the function should return 0.
 
@@ -10,3 +10,21 @@
 // 75 + 120 + 135
 
 // Solution 1:
+
+function maxSubsetSumNoAdjacent(array) {
+    if (!array.length) {
+        return 0;
+    }
+
+    if (array.length === 1) {
+        return array[0];
+    }
+
+    let maxSums = array.slice();
+    maxSums[1] = Math.max(array[0], array[1]);
+
+    for (let i = 2; i < array.length; i++) {
+        maxSums[i] = Math.max(maxSums[i - 1], maxSums[i - 2] + array[i]);
+    }
+    return maxSums[maxSums.length - 1];
+}
