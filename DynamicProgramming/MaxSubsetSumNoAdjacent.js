281f1b7d7d51840044c68fc9ea19ed437d42fc50
Ryan Kirsch <agalloch88@gmail.com>
2022-10-21 08:54:46 -0400

    MaxSubsetSumNoAdjacent start

---
+++ DynamicProgramming/MaxSubsetSumNoAdjacent.js
@@ -0,0 +1,12 @@
+// Write a function which takes in an array of positive integers and returns the maximum sum od non-adjacent elements in the array.
+
+// If the input array is empty, the function should return 0.
+
+// Sample Input:
+// array = [75, 105, 120, 75, 90, 135]
+
+// Sample Output:
+// 330
+// 75 + 120 + 135
+
+// Solution 1:
