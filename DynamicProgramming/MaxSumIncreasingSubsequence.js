85bc26995ff93a209a7858d8e1d2890c80c8a61e
Ryan Kirsch <agalloch88@gmail.com>
2022-10-21 08:52:13 -0400

    MaxSumIncreasingSubsequence solution 1 comments

--- DynamicProgramming/MaxSumIncreasingSubsequence.js
+++ DynamicProgramming/MaxSumIncreasingSubsequence.js
@@ -17,37 +17,53 @@
 
 // Solution 1:
 
+// iterative solution using a couple extra arrays to track total sums, and the index trail of increasing subsequences to build that subsequence easily for return
+
+// O(n^2) time due to nested for loops, looking over the values twice
+// O(n) space due to extra arrays, 3n converges to n
+
 function maxSumIncreasingSubsequence(array) {
+    // set up holder array for the sequences to track of index values for increasing subsequence, all values initially undefined
     let sequences = new Array(array.length);
+    // set up sums array to keep track of the current sum for increasing subsequences, minimum value is the value at that index
     let sums = array.map(num => num);
+    // keep track of where the last value is for the maximum sum subsequence, so it's known where to start from and work backwards in sums array
     let maxSumIdx = 0;
-
+    // set up first for loop to look over entire input array
     for (let i = 0; i < array.length; i++) {
+        // initialize variable currentNum to grab value in array at position i
         let currentNum = array[i];
-
+        // set up second for loop to look over input array up to position i
         for (let j = 0; j < i; j++) {
+            // initialize variable otherNum to grab value in array at position j
             let otherNum = array[j];
-
-            if (otherNum <currentNum && sums[j] + currentNum >= sum[i]) {
+            // if the otherNum value is smaller than currentNum, and value of position j in sums plus the currentNum is greater or equal to sums value at position i,
+            // this is an increasin subsequence, so update the toal sum in sums array at position i to equal this value
+            if (otherNum < currentNum && sums[j] + currentNum >= sums[i]) {
                 sums[i] = sums[j] + currentNum;
+                // also update sequences array to reflect a new addition to the sequence, and set position i equal to j and link them together
                 sequences[i] = j;
             }
         }
-
+        // if the current subsequence sum at position i is greater than the subsequence sum stored in the value at maxSumIdx in sums, update maxSumIdx to equal position i
         if (sums[i] >= sums[maxSumIdx]) {
             maxSumIdx = i;
         }
     }
+    // return the value at maxSumIdx, plus the return result of helper function which builds the sequence
     return [sums[maxSumIdx], buildSequence(array, sequences, maxSumIdx)];
 }
-
+// helper function which takes in the input array, sequences array built in function above, and maxSumIdx for last position of the subsequence
 function buildSequence(array, sequences, currentIdx) {
+    // set up empty holder array for the sequence to build and return from this function
     let sequence = [];
-
+    // while loop which looks at the items in the sequence, and once it reaches an undefined value, will break out as the subsequence is finished
     while (currentIdx !== undefined) {
+        // unshift the value at currentIdx into sequence holder array, which allows to build the sequence in reverse without having to do an additional reversing step
         sequence.unshift(array[currentIdx]);
+        // after unshifting currentIdx, move to the next index value
         currentIdx = sequences[currentIdx];
     }
-
+    // return completed sequence at the end
     return sequence;
 }
