674edca51e7126ab7bc657adb35313be46350a7a
Ryan Kirsch <agalloch88@gmail.com>
2022-10-22 14:56:44 -0400

    ZigzagTraverse start

---
+++ Arrays/ZigzagTraverse.js
@@ -0,0 +1,17 @@
+// Write a function which takes in a two-dimensional array of dimensions N x M (which make be square-shaped if N === M), and which returns a one-dimensional array of all
+// the input, two-dimensional array's elements in zagzag order.
+
+// Zigzag order starts at the top left corner of the two-dimensional array, goes down by one element, and proceeds in a zigzag pattern all the way tot he bottom right corner.
+
+// Sample Input:
+// array = [
+//  [1, 3, 4, 10],
+//  [2, 5, 9, 11],
+//  [6, 8, 12, 15],
+//  [7, 13, 14, 16],   
+// ]
+
+// Sample Output:
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+// Solution 1:
