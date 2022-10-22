1e55769101db57d5978217146f7315c0244445be
Ryan Kirsch <agalloch88@gmail.com>
2022-10-22 14:51:05 -0400

    MaxSubsetSumNoAdjacent solution 2 comments

--- DynamicProgramming/MaxSubsetSumNoAdjacent.js
+++ DynamicProgramming/MaxSubsetSumNoAdjacent.js
@@ -40,22 +40,33 @@ function maxSubsetSumNoAdjacent(array) {
 
 // Solution 2:
 
+// iterative solution which improves space complexity since only required to know position in array, and two values at a time
+
+// O(n) time due to iterating over all values in input array once
+// O(1) space due to only storing a few variables
+
 function maxSubsetSumNoAdjacent(array) {
+    // handle edge case of an empty input array, which instructions say should result in return of 0
     if (!array.length) {
         return 0;
     }
-
+    // handle edge case of a one-value input array, in which case can simply return the value stored at index 0
     if (array.length === 1) {
         return array[0];
     }
-
+    // store index zero in input array inside variable second
     let second = array[0];
+    // store the greater of index zero or 1 in input array inside variable first
     let first = Math.max(array[0], array[1]);
-    
+    // start iterating over other values in input array, starting from index 2
     for (let i = 2; i < array.length; i++) {
+        // take the maximum value between value in variable first or variable second plus current position in input array, store in variable current
         let current = Math.max(first, second + array[i]);
+        // set second equal to first to move it up
         second = first;
+        // set first to the value in current
         first = current;
     }
+    // once at the end, return the value inside first
     return first;
 }
