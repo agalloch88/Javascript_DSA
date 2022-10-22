44eb69ad4f364ee03b3056cdc7f9d683236b2df2
Ryan Kirsch <agalloch88@gmail.com>
2022-10-22 15:53:34 -0400

    ZigzagTraverse solution 1

--- Arrays/ZigzagTraverse.js
+++ Arrays/ZigzagTraverse.js
@@ -15,3 +15,48 @@
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 // Solution 1:
+
+function zigzagTraverse(array) {
+    let height = array.length - 1;
+    let width = array[0].length - 1;
+    let result = [];
+
+    let row = 0;
+    let col = 0;
+    let goingDown = true;
+
+    while (!isOutOfBounds(row, col, height, width)) {
+        result.push(array[row][col]);
+        if (goingDown) {
+            if (col === 0 || row === height) {
+                goingDown = false;
+
+                if (row === height) {
+                    col++;
+                } else {
+                    row++;
+                }
+            } else {
+                row++;
+                col--;
+            }
+        } else {
+            if (row === 0 || col === width) {
+                goingDown = true;
+                if (col === width) {
+                    row++;
+                } else {
+                    col++;
+                }
+            } else {
+                row--;
+                col++;
+            }
+        }
+    }
+    return result;
+}
+
+function isOutOfBounds(row, col, height, width) {
+    return row < 0 || row > height || col < 0 || col > width;
+}
