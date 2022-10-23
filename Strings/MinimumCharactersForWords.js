ce90c34ec2385e3a24c1576a3165e1839b27820c
Ryan Kirsch <agalloch88@gmail.com>
2022-10-23 13:20:31 -0400

    MinimumCharactersForWords start

---
+++ Strings/MinimumCharactersForWords.js
@@ -0,0 +1,14 @@
+// Write a function which takes in an array of words, and which returns the smallest array of characters needs to form all of the words. The characters do not
+// need to be in any particular order.
+
+// For example, the characters ["y", "r", "o", "u"] are needed to form the words ["your", "you", "or", "yo"].
+
+// Note: the input words will not contain any spaces. However, they may contain punctuation and/or special characters.
+
+// Sample Input:
+// words = ["this", "that", "did", "deed", "them!", "a"]
+
+// Sample Output:
+// ["t", "t", "h", "i", "s", "a", "d", "d", "e", "e", "m", "!"]
+
+// Solution 1:
