cc38e8e49a21ccd26949d385c87db8d731a66dbd
Ryan Kirsch <agalloch88@gmail.com>
2022-10-26 08:20:33 -0400

    MinimumNumberOfJumps solution 1

--- DynamicProgramming/MinimumNumberOfJumps.js
+++ DynamicProgramming/MinimumNumberOfJumps.js
@@ -13,3 +13,17 @@
 // 3 -> (4 or 2) -> (2 or 3) -> 7 -> 3
 
 // Solution 1:
+
+function minNumberOfJumps(array) {
+    let jumps = new Array(array.length).fill(Infinity);
+    jumps[0] = 0;
+
+    for (let i = 1; i < array.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (array[j] < i - j) {
+                jumps[i] = Math.min(jumps[j] + 1, jumps[i]);
+            }
+        }
+    }
+    return jumps[jumps.length - 1];
+}
