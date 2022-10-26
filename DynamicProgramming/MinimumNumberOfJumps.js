4bf23936c07c878ab721e068377056dd7c86402c
Ryan Kirsch <agalloch88@gmail.com>
2022-10-26 18:12:28 -0400

    MinimumNumberOfJumps solution 2 comments

--- DynamicProgramming/MinimumNumberOfJumps.js
+++ DynamicProgramming/MinimumNumberOfJumps.js
@@ -40,23 +40,36 @@ function minNumberOfJumps(array) {
 
 // Solution 2:
 
+// iterative solution improving time and space complexities by not storing values in additional array and reducing to one for loop
+
+// O(n) time due to iterating over n values once
+// O(1) space due to only storing a few values
+
 function minNumberOfJumps(array) {
+    // handle edge case of a single-value input array, which should return 0 min jumps
     if (array.length === 1) {
         return 0;
     }
-
+    // initialize variable jumps to 0
     let jumps = 0;
+    // initialize maxReach to initially be first element in input
     let maxReach = array[0];
+    // initialize variable steps to initially be first element in input
     let steps = array[0];
-
+    // iterate over the inputs, starting for second element, and stopping before last element
     for (let i = 1; i < array.length - 1; i++) {
+        // update maxReach to be the maximum of either the maxReach value, or value of i plus the value of position i in array
         maxReach = Math.max(maxReach, i + array[i]);
+        // decrement steps by 1
         steps--;
-
+        // check for whether steps is 0
         if (steps === 0) {
+            // if so, increment jumps by 1
             jumps++;
+            // reset steps to equal maxReach - value of i
             steps = maxReach - i;
         }
     }
+    // since stopping before last element in array, return the total number of jumps + 1 to account for last element
     return jumps + 1;
 }
