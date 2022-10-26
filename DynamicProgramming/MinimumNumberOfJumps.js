73de3e13fdf4ab5ec3ea06e07ef8ffb31550df30
Ryan Kirsch <agalloch88@gmail.com>
2022-10-26 08:42:35 -0400

    MinimumNumberOfJumps solution 2

--- DynamicProgramming/MinimumNumberOfJumps.js
+++ DynamicProgramming/MinimumNumberOfJumps.js
@@ -37,3 +37,26 @@ function minNumberOfJumps(array) {
     // as jumps was tracking the minimum number of jumps to all indexes, return the last value in the jumps array
     return jumps[jumps.length - 1];
 }
+
+// Solution 2:
+
+function minNumberOfJumps(array) {
+    if (array.length === 1) {
+        return 0;
+    }
+
+    let jumps = 0;
+    let maxReach = array[0];
+    let steps = array[0];
+
+    for (let i = 1; i < array.length - 1; i++) {
+        maxReach = Math.max(maxReach, i + array[i]);
+        steps--;
+
+        if (steps === 0) {
+            jumps++;
+            steps = maxReach - i;
+        }
+    }
+    return jumps + 1;
+}
