1845e72e60c7d7557bb04ef5dfc73048833a013d
Ryan Kirsch <agalloch88@gmail.com>
2022-10-27 13:07:02 -0400

    WaterAreas start

---
+++ DynamicProgramming/WaterArea.js
@@ -0,0 +1,22 @@
+// The problem presents an array of non-negative integers, where each non-zero integer represents the height of a pillar with a width of 1. In the problem, imagine water
+// pours over all of the pillars. Write a function which returns the surface area of the water trapped between the pillars viewed from the front. Note that
+// spilled water should be ignored.
+
+// Sample Input:
+// heights = [0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3]
+
+// Sample Output:
+// 48
+// visual representation including 48 dots:
+//        |
+//        |
+//  |.....|
+//  |.....|
+//  |.....|
+//  |..|..|
+//  |..|..|
+//  |..|..|.....|
+//  |..|..|.....|
+// _|..|..|..||.|
+
+// Solution 1:
