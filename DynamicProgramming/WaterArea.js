e00e6aee4bd2b83c7b65c394c88b30a03cb6420d
Ryan Kirsch <agalloch88@gmail.com>
2022-10-27 13:25:55 -0400

    WaterAreas solution 1

--- DynamicProgramming/WaterArea.js
+++ DynamicProgramming/WaterArea.js
@@ -20,3 +20,28 @@
 // _|..|..|..||.|
 
 // Solution 1:
+
+function waterArea(heights) {
+    let maxes = new Array(heights.length).fill(0);
+    let leftMax = 0;
+
+    for (let i = 0; i < heights.length; i++) {
+        let height = heights[i];
+        maxes[i] = leftMax;
+        leftMax = Math.max(leftMax, height);
+    }
+    let rightMax = 0;
+
+    for (let i = heights.length - 1; i >= 0; i--) {
+        let height = heights[i];
+        let minHeight = Math.min(rightMax, maxes[i]);
+
+        if (height < minHeight) {
+            maxes[i] = minHeight - height;
+        } else {
+            maxes[i] = 0;
+        }
+        rightMax = Math.max(rightMax, height);
+    }
+    return maxes.reduce((a, b) => a + b, 0);
+}
