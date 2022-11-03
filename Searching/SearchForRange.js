2b1689ead10daa35d4c97c0436b909502d74cc27
Ryan Kirsch <agalloch88@gmail.com>
2022-11-03 09:17:37 -0400

    SearchForRange start

---
+++ Searching/SearchForRange.js
@@ -0,0 +1,14 @@
+// Write a function which takes in a sorted array of integers, as well as a target integer. The function should use a variation of the Binary Search algorithm to find a range of 
+// indices in between which the target number is contained in the array, and should this range in the form of an array.
+
+// The first number in the output array should represent the first index at which the target number is located, while the second number should represent the last index at
+// which the target number is located. The function should return [-1, -1] if the integer is not contained at all in the array.
+
+// Sample Input:
+// array = [0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73]
+// target = 45
+
+// Sample Output:
+// [4, 9]
+
+// Solution 1
