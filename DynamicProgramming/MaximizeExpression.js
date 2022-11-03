43a1793b6ac70671e622af5d9eaf9889134069e0
Ryan Kirsch <agalloch88@gmail.com>
2022-11-03 07:43:55 -0400

    MaximizeExpression solution 3

--- DynamicProgramming/MaximizeExpression.js
+++ DynamicProgramming/MaximizeExpression.js
@@ -110,3 +110,24 @@ function maximizeExpression(array) {
     // return the last value of final array as the answer
     return maxOfAMinusBPlusCMinusD[maxOfAMinusBPlusCMinusD.length - 1];
 }
+
+// Solution 3:
+
+function maximizeExpression(array) {
+    if (array.length < 4) {
+        return 0;
+    }
+
+    let maxOfA = -Infinity;
+    let maxOfAB = -Infinity;
+    let maxOfABC = -Infinity;
+    let maxOfABCD = -Infinity;
+
+    for (let i = 0; i < array.length - 3; i++) {
+        maxOfA = Math.max(maxOfA, array[i]);
+        maxOfAB = Math.max(maxOfAB, maxOfA - array[i + 1]);
+        maxOfABC = Math.max(maxOfABC, maxOfAB + array[i + 2]);
+        maxOfABCD = Math.max(maxOfABCD, maxOfABC - array[i + 3]);
+    }
+    return maxOfABCD;
+}
