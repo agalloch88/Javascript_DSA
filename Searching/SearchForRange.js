04a8c8f7df3054d71c7e0d7b2486e2966a1334d9
Ryan Kirsch <agalloch88@gmail.com>
2022-11-04 13:34:50 -0400

    SearchForRange solution 1 comments

--- Searching/SearchForRange.js
+++ Searching/SearchForRange.js
@@ -13,34 +13,57 @@
 
 // Solution 1:
 
+// recursive solution implementing modified binary search on sorted input to find leftmost and rightmost instances of target value
+
+// O(log(n)) time due to modified binary search efficiency
+// O(log(n)) space due to recursive calls
+
 function searchForRange(array, target) {
+    // set up holder array for final values, initialize to contain -1's right now in case no instances of target exist in input array
     let finalRange = [-1, -1];
+    // recursive call for helper function with goLeft set to true to indicate looking left in input array
     alteredBinarySearch(array, target, 0, array.length - 1, finalRange, true);
+    // recursive call for helper function with goLeft set to false to indicate looking right in input array
     alteredBinarySearch(array,target, 0, array.length - 1, finalRange, false);
+    // once both recursive calls return, should have the finalRange to return, if target exists in input array, otherwise same initialized values satisfy requirements
     return finalRange;
 }
-
+// helper function taking in input array, target value, values for left and right points to calculate mid from, the finalRange holder array, and value for goLeft to determine direction
 function alteredBinarySearch(array, target, left, right, finalRange, goLeft) {
+    // base case
+    // if left pointer greater than right, looked at all values so return
     if (left > right) {
         return;
     }
-
+    // floor the result of left plus right divided by 2 to find middle point of range, set equal to variable mid
     let mid = Math.floor((left + right) / 2);
-
+    // if the mid value in input array is smaller than the target value, can discard the left half of the values as would be even smaller
     if (array[mid] < target) {
+        // call helper again with left pointer shifted one to right of mid value
         alteredBinarySearch(array, target, mid + 1, right, finalRange, goLeft);
+    // if the mid value in the input array is greater than the target value, can discard the right half of the values as would be even larger
     } else if (array[mid] > target) {
+        // call helper again with right pointer shifted one to left of mid value
         alteredBinarySearch(array, target, left, mid - 1, finalRange, goLeft);
+    // if mid is not larger or smaller than target, must be equal, so have found at least one instance of the target, so execute block below
     } else {
+        // if looking to the left for additional target values, execute block below
         if (goLeft) {
+            // if all the way at start of the input array, of if current mid's neighbor to left is not equal to the target value, set the first value in finalRange equal to current mid index
             if (mid === 0 || array[mid - 1] !== target) {
+                // this index represents the first occurence in the range of target values within the input array
                 finalRange[0] = mid;
+            // if neither at the start of array or off the target value, call helper function again to continue looking for leftmost index, updating right pointer to one left of current mid
             } else {
                 alteredBinarySearch(array, target, left, mid - 1, finalRange, goLeft);
             }
+        // if not looking left, must be looking for additional target values to the right, so execute block below
         } else {
+            // if all the way at the end of the input array, or if current mid's neighbor to the right is not equal to the target value, set the second value in finalRange equal to current mid index
             if (mid === array.length - 1 || array[mid + 1] !== target) {
+                // this index represents the first occurence in the range of target values within the input array
                 finalRange[1] = mid;
+            // if neight at the end of array or off the target value, call helper function again to continue looking for rightmost index, updating left pointer to one right of current mid
             } else {
                 alteredBinarySearch(array, target, mid + 1, right, finalRange, goLeft);
             }
