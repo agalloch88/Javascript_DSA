c2dff914bc4a4be18289ab0526ee967d76735de2
Ryan Kirsch <agalloch88@gmail.com>
2022-11-05 18:17:29 -0400

    SearchForRange solution 2

--- Searching/SearchForRange.js
+++ Searching/SearchForRange.js
@@ -70,3 +70,41 @@ function alteredBinarySearch(array, target, left, right, finalRange, goLeft) {
         }
     }
 }
+
+// Solution 2:
+
+function searchForRange(array, target) {
+    let finalRange = [-1, -1];
+    alteredBinarySearch(array, target, 0, array.length - 1, finalRange, true);
+    alteredBinarySearch(array, target, 0, array.length - 1, finalRange, false);
+    return finalRange;
+}
+
+function alteredBinarySearch(array, target, left, right, finalRange, goLeft) {
+    while (left <= right) {
+        let mid = Math.max((left + right) / 2);
+
+        if (array[mid] < target) {
+            left = mid + 1;
+        } else if (array[mid] > target) {
+            right = mid - 1;
+        } else {
+            if (goLeft) {
+                if (mid === 0 || array[mid - 1] !== target) {
+                    finalRange[0] = mid;
+                    return;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (mid === array.length - 1 || array[mid + 1] !== target) {
+                    finalRange[1] = mid;
+                    return;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+        }
+    }
+}
