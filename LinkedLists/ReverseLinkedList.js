5c43f5e43e3d2f05fda8f2be999359a72e04ccb3
Ryan Kirsch <agalloch88@gmail.com>
2022-11-06 15:49:26 -0500

    ReverseLinkedList start

---
+++ LinkedLists/ReverseLinkedList.js
@@ -0,0 +1,14 @@
+// Write a function which takes in the head of a Singly Linked List, reverses the list in place (meaning, the solution does not create a brand new Linked List)
+// and returns the new head of the Linked List.
+
+// Each Linked List node has an integer value, as well as a next node pointing to the next node in the list, or to None/null if it is the tail of the Linked List.
+
+// In the context of the problem, assume that the input Linked List will always have at least one node. In other words, the head will never be None/null.
+
+// Sample Input:
+// head = 0 -> 1 -> 2 -> 3 -> 4 -> 5
+
+// Sample Output:
+// 5 -> 4 -> 3 -> 2 -> 1 -> 0
+
+// Solution 1:
