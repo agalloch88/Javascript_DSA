c310144004680c44872912b4ce7fcc5e86164bc9
Ryan Kirsch <agalloch88@gmail.com>
2022-11-11 13:45:09 -0500

    FindLoop solution 2 comments

--- LinkedLists/FindLoop.js
+++ LinkedLists/FindLoop.js
@@ -55,22 +55,35 @@ function findLoop(head) {
 
 // Solution 2:
 
+// iterative approach which adds another value to the LinkedList class to track whether the node is seen or not
+
+// O(n) time due to iterating over all inputs of the linked list
+// O(1) space due to not storing any extra information
+
+// main LinkedList class, showing properties of value and next on each node
+
 class LinkedList {
     constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
-
+// main function, which takes in a parameter labeled head which is the start of the Linked list
 function findLoop(head) {
+    // create variable node and set equal to the head value passed in to the function
     let node = head;
-
+    // while loop to iterate over the input so long as there is a node
     while (node) {
+        // if node was previously seen, this is the start of the loop
         if (node.seen) {
+            // since found where the loop begins/where tail converges back into the loop, return this node
             return node;
+        // if not seen, execute code below
         } else {
+            // create new property on each node called seen, and set it to true
             node.seen = true;
         }
+        // move on to the next node in the Linked List
         node = node.next;
     }
 }
