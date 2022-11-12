b453b6c5db052dfc6156ab2a7793b622ee94fcbd
Ryan Kirsch <agalloch88@gmail.com>
2022-11-12 08:43:20 -0500

    MinRewards solution 1

--- Arrays/MinRewards.js
+++ Arrays/MinRewards.js
@@ -23,3 +23,21 @@
 // [4, 3, 2, 1, 2, 3, 4, 5, 1]
 
 // Solution 1:
+
+function minRewards(scores) {
+    let rewards = scores.map(_ => 1);
+
+    for (let i = 1; i < scores.length; i++) {
+        let j = i - 1;
+
+        if (scores[i] > scores[j]) {
+            rewards[i] = rewards[j] + 1;
+        } else {
+            while (j >= 0 && scores[j] > scores[j + 1]) {
+                rewards[j] = Math.max(rewards[j], rewards[j + 1] + 1);
+                j--;
+            }
+        }
+    }
+    return rewards.reduce((a, b) => a + b);
+}
