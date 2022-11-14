34cb9be143e21802b63b925cc1cefd612773fd5a
Ryan Kirsch <agalloch88@gmail.com>
2022-11-14 11:13:18 -0500

    MinRewards solution 2

--- Arrays/MinRewards.js
+++ Arrays/MinRewards.js
@@ -57,3 +57,54 @@ function minRewards(scores) {
     // return the reduced rewards array value which is the total number of rewards needed
     return rewards.reduce((a, b) => a + b);
 }
+
+// Solution 2:
+
+function minRewards(rewards) {
+    let rewards = scores.map(_ => 1);
+    let localMinIdxs = getLocalMinIdxs(scores);
+    for (let localMinIdx of localMinIdxs) {
+        expandFromLocalMinIdx(localMinIdx, scores, rewards);
+    }
+    return rewards.reduce((a, b) => a + b);
+}
+
+function getLocalMinIdxs(array) {
+    if (array.length === 1) {
+        return [0];
+    }
+
+    let localMinIdxs = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i === 0 && array[i] < array[i + 1]) {
+            localMinIdxs.push(i);
+        }
+
+        if (i === array.length - 1 && array[i] < array[i - 1]) {
+            localMinIdxs.push(i);
+        }
+
+        if (i === 0 || i === array.length - 1) {
+            continue;
+        }
+
+        if (array[i] < array[i + 1] && array[i] < array[i - 1]) {
+            localMinIdxs.push(i);
+        }
+    }
+    return localMinIdxs;
+}
+
+function expandFromLocalMinIdx(localMinIdx, scores, rewards) {
+    let leftIdx = localMinIdx - 1;
+    while (leftIdx >= 0 && scores[leftIdx] > scores[leftIdx + 1]) {
+        rewards[leftIdx] = Math.max(rewards[leftIdx], rewards[leftIdx + 1] + 1);
+        leftIdx--;
+    }
+
+    let rightIdx = localMinIdx + 1;
+    while (rightIdx < scores.length && scores[rightIdx] > scores[rightIdx - 1]) {
+        rewards[rightIdx] = rewards[rightIdx - 1] + 1;
+        rightIdx++;
+    }
+}
