4e41a6585829f4a00ac231b89d00c77ab58a7193
Ryan Kirsch <agalloch88@gmail.com>
2022-11-15 09:30:03 -0500

    MinRewards solution 2 comments

--- Arrays/MinRewards.js
+++ Arrays/MinRewards.js
@@ -60,51 +60,72 @@ function minRewards(scores) {
 
 // Solution 2:
 
-function minRewards(rewards) {
+// iterative solution making use of local min/max peak/valley concept to figure out the greatest and smallest values, then expanding from those min values to update surroundings
+
+// O(n) time due to iterating over all values in scores input
+// O(n) space due to storing new rewards array
+
+function minRewards(scores) {
+    // create copy of scores array and map over, inserting 1 for each value as this is the minimum reward any student receives, store in variable rewards
     let rewards = scores.map(_ => 1);
+    // call helper function and pass in scores array, store return value in variable localMinIdxs
     let localMinIdxs = getLocalMinIdxs(scores);
+    // iterate over every value in the localMinIdxs
     for (let localMinIdx of localMinIdxs) {
+        // for every localMinIdx in the input, call the helper function and pass in the current localMinIdx, the scores input, and the rewards holder array
         expandFromLocalMinIdx(localMinIdx, scores, rewards);
     }
+    // return the sum of the reduced rewards array, which should be the total number of rewards the students earn
     return rewards.reduce((a, b) => a + b);
 }
-
+// helper function to find the min indexes/valleys in the input array
 function getLocalMinIdxs(array) {
+    // handle edge case if the input is a single value, return value at index 0
     if (array.length === 1) {
         return [0];
     }
-
+    // set up empty array and store in variable localMinIdxs
     let localMinIdxs = [];
+    // iterate over the input values
     for (let i = 0; i < array.length; i++) {
+        // if at the beginning of the array and the value at i is smaller than the value to the right of i, push i into the localMinIdxs array
         if (i === 0 && array[i] < array[i + 1]) {
             localMinIdxs.push(i);
         }
-
+        // if at the end of the array and the value of i is smaller than the value to the left of i, push i into the localMinIdxs array
         if (i === array.length - 1 && array[i] < array[i - 1]) {
             localMinIdxs.push(i);
         }
-
+        // if at the beginning or end of the array, continue along
         if (i === 0 || i === array.length - 1) {
             continue;
         }
-
+        // if value in array at position i is smaller than the value to the right of i and the value in array at position i is smaller than the value to the left of i, push i into localMinIdxs
         if (array[i] < array[i + 1] && array[i] < array[i - 1]) {
             localMinIdxs.push(i);
         }
     }
+    // once iterated over all inputs, return the localMinIdxs array
     return localMinIdxs;
 }
-
+// helper function to expand from the min indexes/valleys
 function expandFromLocalMinIdx(localMinIdx, scores, rewards) {
+    // take the passed in localMinIdx - 1 and store in variable leftIdx
     let leftIdx = localMinIdx - 1;
+    // while the leftIdx value is greater than 0 and value at leftIdx in scores input array is greater than value to its right, execute below
     while (leftIdx >= 0 && scores[leftIdx] > scores[leftIdx + 1]) {
+        // update the value at leftIdx in rewards array to equal the max between current value at leftIdx in rewards, or the value to i's right in rewards plus 1
         rewards[leftIdx] = Math.max(rewards[leftIdx], rewards[leftIdx + 1] + 1);
+        // decrement leftIdx and keep going
         leftIdx--;
     }
-
+    // take the passed in localMinIdx + 1 and store in variable rightIdx
     let rightIdx = localMinIdx + 1;
+    // while the rightIdx value is less than length of the scores input and value at rightIdx in scores input array is greater than value to the left of rightIdx in scores input array, execute below
     while (rightIdx < scores.length && scores[rightIdx] > scores[rightIdx - 1]) {
+        // since moving to the right, no need to do the max check, so set rightIdx in rewards equal to value of rightIdx -1 plus 1
         rewards[rightIdx] = rewards[rightIdx - 1] + 1;
+        // increment rightIdx and keep going
         rightIdx++;
     }
 }
