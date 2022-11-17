145de065625cab4de2e2748735f5a6fd73aee652
Ryan Kirsch <agalloch88@gmail.com>
2022-11-17 07:59:55 -0500

    QuickSort solution 1

--- Sorting/QuickSort.js
+++ Sorting/QuickSort.js
@@ -8,3 +8,48 @@
 
 // Solution 1:
 
+function quickSort(array) {
+    quickSortHelper(array, 0, array.length - 1);
+    return array;
+}
+
+function quickSortHelper(array, startIdx, endIdx) {
+    if (startIdx >= endIdx) {
+        return;
+    }
+
+    let pivotIdx = startIdx;
+    let leftIdx = startIdx + 1;
+    let rightIdx = endIdx;
+
+    while (rightIdx >= leftIdx) {
+        if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]) {
+            swap(leftIdx, rightIdx, array);
+        }
+
+        if (array[leftIdx] <= array[pivotIdx]) {
+            leftIdx++;
+        }
+
+        if (array[rightIdx] >= array[pivotIdx]) {
+            rightIdx--;
+        }
+    }
+    swap(pivotIdx, rightIdx, array);
+
+    let leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1);
+
+    if (leftSubarrayIsSmaller) {
+        quickSortHelper(array, startIdx, rightIdx - 1);
+        quickSortHelper(array, rightIdx + 1, endIdx);
+    } else {
+        quickSortHelper(array, rightIdx + 1, endIdx);
+        quickSortHelper(array, startIdx, rightIdx - 1);
+    }
+} 
+
+function swap(i, j, array) {
+    let temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
