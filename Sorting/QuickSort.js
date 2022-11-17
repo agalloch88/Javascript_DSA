8d237a337323f75df6cd56c6b962188d56041441
Ryan Kirsch <agalloch88@gmail.com>
2022-11-17 07:59:23 -0500

    QuickSort start

---
+++ Sorting/QuickSort.js
@@ -0,0 +1,10 @@
+// Write a function which takes in an array of integers, and which returns a sorted version of that array. Use the Quick Sort algorithm to sort the array.
+
+// Sample Input:
+// array = [8, 5, 2, 9, 5, 6, 3]
+
+// Sample Output:
+// [2, 3, 5, 5, 6, 8, 9]
+
+// Solution 1:
+
