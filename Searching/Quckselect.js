53ebfc2b3dfcd8d72e3a99e112394cb368267ffb
Ryan Kirsch <agalloch88@gmail.com>
2022-11-18 08:59:50 -0500

    Quickselect start

---
+++ Searching/Quckselect.js
@@ -0,0 +1,12 @@
+// Write a function which takes in an array of distinct integers, as well as an integer k, and which returns the kth smallest integer in the input array.
+
+// The function should complete the search in linear time, on average.
+
+// Sample Input:
+// array = [8, 5, 2, 9, 7, 6, 3]
+// k = 3
+
+// Sample Output:
+// 5
+
+// Solution 1:
