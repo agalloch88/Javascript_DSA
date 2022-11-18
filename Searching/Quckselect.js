0b782ee81e83aa4d0ddca5f5451b47f09cee035f
Ryan Kirsch <agalloch88@gmail.com>
2022-11-18 09:41:13 -0500

    Quickselect solution 1

--- Searching/Quckselect.js
+++ Searching/Quckselect.js
@@ -10,3 +10,49 @@
 // 5
 
 // Solution 1:
+
+function quickselect(array, k) {
+    let position = k - 1;
+    return quickselectHelper(array, 0, array.length - 1, position);
+}
+
+function quickselectHelper(array, startIdx, endIdx, position) {
+    while (true) {
+        if (startIdx > endIdx) {
+            throw new Error("The algorithm should never arrive here!");
+        }
+
+        let pivotIdx = startIdx;
+        let leftIdx = startIdx + 1;
+        let rightIdx = array.length - 1;
+
+        while (leftIdx <= rightIdx) {
+            if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]) {
+                swap(leftIdx, rightIdx, array);
+            }
+
+            if (array[leftIdx] <= array[pivotIdx]) {
+                leftIdx++;
+            }
+
+            if (array[rightIdx] >= array[pivotIdx]) {
+                rightIdx--;
+            }
+        }
+        swap(pivotIdx, rightIdx, array);
+
+        if (rightIdx === position) {
+            return array[rightIdx];
+        } else if (rightIdx < position) {
+            startIdx = rightIdx + 1;
+        } else {
+            endIdx = rightIdx - 1;
+        }
+    }
+}
+
+function swap(i, j, array) {
+    let temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
