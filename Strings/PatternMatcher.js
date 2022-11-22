bf18b37cee564fbee34b42e2f5c99fddbf750583
Ryan Kirsch <agalloch88@gmail.com>
2022-11-22 09:08:06 -0500

    PatternMatcher solution 1

--- Strings/PatternMatcher.js
+++ Strings/PatternMatcher.js
@@ -17,3 +17,67 @@
 // ["go", "powerranger"]
 
 // Solution 1:
+
+function patternMatcher(pattern, string) {
+    if (pattern.length > string.length) {
+        return [];
+    }
+
+    let newPattern = getNewPattern(pattern);
+    let didSwitch = newPattern[0] !== pattern[0];
+    let counts = {x: 0, y: 0};
+    let firstYPos = getCountsAndFirstYPos(newPattern, counts);
+
+    if (counts['y'] !== 0) {
+        for (let lenOfX = 1; lenOfX < string.length; lenOfX++) {
+            let lenOfY = (string.length - lenOfX * counts['x']) / counts['y'];
+
+            if (lenOfY <= 0 || lenOfY % 1 !== 0) {
+                continue;
+            }
+            let yIdx = firstYPos * lenOfX;
+            let x = string.slice(o, lenOfX);
+            let y = string.slice(yIdx, yIdx + lenOfY);
+            let potentialMatch = newPattern.map(char => (char === 'x' ? x : y));
+
+            if (string === potentialMatch.join('')) {
+                return !didSwitch ? [x, y] : [y, x];
+            }
+        }
+    } else {
+        let lenOfX = string.length / counts['x'];
+
+        if (lenOfX % 1 === 0) {
+            let x = string.slice(0, lenOfX);
+            let potentialMatch = newPattern.map(char => (char === 'x' ? x : ''));
+
+            if (string === potentialMatch.join('')) {
+                return !didSwitch ? [x, ''] : ['', x];
+            }
+        }
+    }
+    return [];
+}
+
+function getNewPattern(pattern) {
+    let patternLetters = pattern.split('');
+    if (pattern[0] === 'x') {
+        return patternLetters;
+    } else {
+        return patternLetters.map(char => (char === 'y' ? 'x' : 'y'));
+    }
+}
+
+function getCountsAndFirstYPos(pattern, counts) {
+    let firstYPos = null;
+
+    for (let i = 0; i < pattern.length; i++) {
+        let char = pattern[i];
+        counts[char]++;
+
+        if (char === 'y' && firstYPos === null) {
+            firstYPos = i;
+        }
+    }
+    return firstYPos;
+}
