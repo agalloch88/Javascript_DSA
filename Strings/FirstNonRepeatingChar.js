f634b0b1ad1675b58e91b835b08999c3708f9cbb
Ryan Kirsch <agalloch88@gmail.com>
2022-11-25 08:16:46 -0500

    FirstNonRepeatingChar solution 2 comments

--- Strings/FirstNonRepeatingChar.js
+++ Strings/FirstNonRepeatingChar.js
@@ -1,4 +1,4 @@
-// Write a function that takes in a string of lowercase, English-alphabet, non-symbol non-numeric characters
+// Write a function that takes in a string of lowercase, English-alphabet, non-symbol, non-numeric characters
 // and returns the index of the string's first non-repeating chacter.
 // The first non-repeating character is the first character in the string
 // which occurs only once.
@@ -44,26 +44,34 @@ function firstNonRepeatingCharacter(string) {
 
 // Solution 2:
 
+// iterative solution reducing time complexity by using JS object to store frequencies of all alphabet characters
+
 // O(n) time due to single pass over string
 // O(1) space, even though we are using JS object, it will hold max of 26 values
 // since this is constant, regardless of string length, this equates to O(1)
 
 function firstNonRepeatingCharacter(string) {
+    // set up empty JS object and store in variable characterFrequencies
     const characterFrequencies = {};
-
+    // iterate over every character in the string
     for (const character of string) {
+        // if the character is not present in characterFrequencies JS object yet, add it and set count to 0
         if (!(character in characterFrequencies)) {
             characterFrequencies[character] = 0;
         }
+        // increment the frequency count of the current character by 1
         characterFrequencies[character]++;
     }
-
+    // iterate over every item in the string again
     for (let idx = 0; idx < string.length; idx++) {
+        // grab the value at idx in the string, and store in the variable character
         const character = string[idx];
+        // if the value of the specific character being looked at is equal to 1, this character does not repeat and is the answer
         if (characterFrequencies[character] === 1) {
+            // return this idx, as it is the answer
             return idx;
         }
     }
-
+    // should the if block above never trigger, there are no non-repeating characters, so return -1
     return -1;
 }
