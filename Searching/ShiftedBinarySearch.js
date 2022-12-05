6a3e8c40e5f98ff850b72b4fe93d3dc8478f0094
Ryan Kirsch <agalloch88@gmail.com>
2022-12-05 08:07:23 -0500

    ShiftedBinarySearch start

---
+++ Searching/ShiftedBinarySearch.js
@@ -0,0 +1,14 @@
+// Write a function which takes in a sorted array of distinct integers, as well as a target integer. The caveat for the problem at hand is that the integers in the array
+// are shifted by some amount. In other words, the integers are moved to the left or to the right by one or more positions. For example, [1, 2, 3, 4] might turn into [3, 4, 1, 2].
+
+// The function should use a variation of the Binary Search algorithm to determine if the target interger exists in the array, and should return its index if so. Otherwise,
+// the function should return -1.
+
+// Sample Input:
+// array = [45, 61, 71, 72, 73, 0, 1, 21, 33, 37]
+// target = 33
+
+// Sample Output:
+// 8
+
+// Solution 1:
