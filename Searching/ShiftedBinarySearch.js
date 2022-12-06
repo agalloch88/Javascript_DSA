d24da2aa5e8ea6c3e72d2ffcd35bf00207495aa3
Ryan Kirsch <agalloch88@gmail.com>
2022-12-06 08:10:56 -0500

    ShiftedBinarySearch solution 2

--- Searching/ShiftedBinarySearch.js
+++ Searching/ShiftedBinarySearch.js
@@ -67,3 +67,35 @@ function shiftedBinarySearchHelper(array, target, left, right) {
         }
     }
 }
+
+// Solution 2:
+
+function shiftedBinarySearch(array, target) {
+    return shiftedBinarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function shiftedBinarySearchHelper(array, target, left, right) {
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        let potentialMatch = array[middle];
+        let leftNum = array[left];
+        let rightNum = array[right];
+
+        if (target === potentialMatch) {
+            return middle;
+        } else if (leftNum <= potentialMatch) {
+            if (target < potentialMatch && target >= leftNum) {
+                right = middle + 1;
+            } else {
+                left = middle - 1;
+            }
+        } else {
+            if (target > potentialMatch && target <= rightNum) {
+                left = middle - 1;
+            } else {
+                right = middle + 1;
+            }
+        }
+    }
+    return -1;
+}
