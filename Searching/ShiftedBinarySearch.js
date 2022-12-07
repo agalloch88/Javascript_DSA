146562a1e7ad7f60ba0016e5e177496014d9a132
Ryan Kirsch <agalloch88@gmail.com>
2022-12-07 09:28:18 -0500

    ShiftedBinarySearch solution 2 comments

--- Searching/ShiftedBinarySearch.js
+++ Searching/ShiftedBinarySearch.js
@@ -70,32 +70,56 @@ function shiftedBinarySearchHelper(array, target, left, right) {
 
 // Solution 2:
 
+// iterative solution, utilizing helper function to check whether target is in left or right half accounting for shifted values
+
+// O(log(n)) time due to modified Binary Search implementation
+// O(1) space due to iteratively searching for target and no call stack buildup
+
+// main function taking in input array and target value to look for
 function shiftedBinarySearch(array, target) {
+    // return a call to the helper function, which takes in array, target, 0 for left value and last element of input array for right value
     return shiftedBinarySearchHelper(array, target, 0, array.length - 1);
 }
 
+// helper function which takes in input array, target to look for, and values for left and right pointers
 function shiftedBinarySearchHelper(array, target, left, right) {
+    // set up while loop to run so long as left pointer does not pass the right pointer
     while (left <= right) {
+        // calculate the exact middle of the input array and store in variable middle
         let middle = Math.floor((left + right) / 2);
+        // grab the middle value from the input array and store in variable potential match
         let potentialMatch = array[middle];
+        // grab the left pointer value in the input array and store in variable leftNum
         let leftNum = array[left];
+        // grab the right pointer value in the input array and store in variable rightNum
         let rightNum = array[right];
-
+        // if the target value is equal to the potentialMatch value, found the answer, so return the middle value
         if (target === potentialMatch) {
             return middle;
+        // otherwise, if the leftNum is less than or equal to the potentialMatch value, execute block below
         } else if (leftNum <= potentialMatch) {
+            // if target value is less than the potentialMatch value AND the target is greater than or equal to the leftNum, need to explore the left
             if (target < potentialMatch && target >= leftNum) {
-                right = middle + 1;
+                // update value of the right pointer to be value to the left of current middle
+                right = middle - 1;
+            // if target value is greater to potentialMatch OR target is less than leftNum, need to explore the right
             } else {
-                left = middle - 1;
+                // update value of the left pointer to be value to the right of current middle
+                left = middle + 1;
             }
+        // otherwise, if the leftNum value is greater than the potentialMatch, execute the block below
         } else {
+            // if target is greater than potentialMatch AND target is less than or equal to the rightNum, need to explore the right
             if (target > potentialMatch && target <= rightNum) {
-                left = middle - 1;
+                // update value of the left pointer to be value to the right of current middle
+                left = middle + 1;
+            // if target value is less than potentialMatch OR target is greater than rightNum, need to explore the left
             } else {
-                right = middle + 1;
+                // update value of the right pointer to the value to the left of current middle
+                right = middle - 1;
             }
         }
     }
+    // if while loop breaks and never returns, value is not in the input array, so return -1
     return -1;
 }
