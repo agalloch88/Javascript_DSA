d23d86d0e47edef34ba9330446921fce1c253bf1
Ryan Kirsch <agalloch88@gmail.com>
2022-12-13 10:59:19 -0500

    IndexEqualsValue start

---
+++ Searching/IndexEqualsValue.js
@@ -0,0 +1,14 @@
+// Write a function which takes in a sorted array of distinct, ascending integers, and which
+// returns the first index in the array which is equal to the value at that index. In other words,
+// the function should return the minimum index where index === array[index].
+
+// If there is no such index, the function should return -1.
+
+// Sample Input:
+// array = [-5, -3, 0, 3, 4, 5, 9]
+
+// Sample Output:
+// 3
+// 3 === array[3]
+
+// Solution 1:
