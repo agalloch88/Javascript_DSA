3c1758b5f1269192bd212feae9807a5e6f007543
Ryan Kirsch <agalloch88@gmail.com>
2022-12-15 19:19:26 -0500

    IndexEqualsValue solution 2 comments

--- Searching/IndexEqualsValue.js
+++ Searching/IndexEqualsValue.js
@@ -34,25 +34,45 @@ function indexEqualsValue(array) {
 
 // Solution 2:
 
+// recursive solution using helper function to check for middleIndex and middleValue equality
+
+// O(log(n)) time due to Binary Search-style approach to checking, eliminating half of remaining possibilities at each check
+// O(log(n)) space due to recursive calls on the call stack
+
+// main function which takes in array of distinct integers
 function indexEqualsValue(array) {
+    // return a call to the helper function, passing in the input array, a value for leftIndex, and a value for rightIndex
     return indexEqualsValueHelper(array, 0, array.length - 1)
 }
 
+// helper function which takes in the input array, and values for leftIndex and rightIndex
 function indexEqualsValueHelper(array, leftIndex, rightIndex) {
+    // base case
+    // when leftIndex is greater than rightIndex, all possible options were checked with no correct solution, so return -1
     if (leftIndex > rightIndex) {
         return -1;
     }
-
+    // add current value of leftIndex to the floored middle index of the inputs, and store in variable middleIndex
     let middleIndex = leftIndex + Math.floor((leftIndex + rightIndex) / 2);
+    // grab the value at the middleIndex in the input array and store in variable middleValue
     let middleValue = array[middleIndex];
-
+    // start checking here, first looking to see if the middleValue is smaller than the middleIndex
     if (middleValue < middleIndex) {
+        // if yes, return a recursive call of the helper, updating the leftIndex to the one index to the right of current middleIndex
         return indexEqualsValueHelper(array, middleIndex + 1, rightIndex);
+    // if the condition above is false, check whether middleValue is equal to the middleIndex AND middleIndex is 0
     } else if (middleValue === middleIndex && middleIndex === 0) {
+        // if both conditions above are true, then return the middleIndex
         return middleIndex;
+    // if the two coniditions above are false, check whether middleValue equals middleIndex AND the
+    // element directly left of middleIndex in the input array is smaller than the value of middleIndex
+    // minus 1
     } else if (middleValue === middleIndex && array[middleIndex - 1] < middleIndex - 1) {
+        // if both conditions above are true, then return the middleIndex
         return middleIndex;
+    // if none of the three above checks are true, then execute the final else block below
     } else {
+        // need to look left, so return a recursive call of the helper, updating the rightIndex to be one index to the left of current middleIndex
         return indexEqualsValueHelper(array, leftIndex, middleIndex - 1);
     }
 }
