0e8fc1fb381a35580c5bdf9281776231aba7e403
Ryan Kirsch <agalloch88@gmail.com>
2022-12-17 08:35:29 -0500

    FindThreeLargestNumbers solution 1

--- Searching/FindThreeLargestNumbers.js
+++ Searching/FindThreeLargestNumbers.js
@@ -11,3 +11,31 @@
 // [18, 141, 541]
 
 // Solution 1:
+
+function findThreeLargestNumbers(array) {
+    let threeLargest = [null, null, null];
+    for (let num of array) {
+        updateLargest(threeLargest, num);
+    }
+    return threeLargest;
+}
+
+function updateLargest(threeLargest, num) {
+    if (threeLargest[2] === null || num > threeLargest[2]) {
+        shiftAndUpdate(threeLargest, num, 2);
+    } else if (threeLargest[1] === null || num > threeLargest[1]) {
+        shiftAndUpdate(threeLargest, num, 1);
+    } else if (threeLargest[0] === null || num > threeLargest[0]) {
+        shiftAndUpdate(threeLargest, num, 0);
+    }
+}
+
+function shiftAndUpdate(array, num, idx) {
+    for (let i = 0; i <= idx; i++) {
+        if (i === idx) {
+            array[i] = num;
+        } else {
+            array[i] = array[i + 1];
+        }
+    }
+}
