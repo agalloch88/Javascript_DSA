d105b26629b0be6edf255bffba1c678e01f16fbf
Ryan Kirsch <agalloch88@gmail.com>
2022-12-17 08:24:59 -0500

    FindThreeLargestNumbers start

---
+++ Searching/FindThreeLargestNumbers.js
@@ -0,0 +1,13 @@
+// Write a function which takes in an array of at least three integers and, without sorting
+// the input array, returns a sorted array of the three larest integers in the input array.
+
+// The function should return duplicate integers, if necessary; for example, it should return
+// [10, 10, 12] for an input array of [10, 5, 9, 10, 12].
+
+// Sample Input:
+// array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+
+// Sample Output:
+// [18, 141, 541]
+
+// Solution 1:
