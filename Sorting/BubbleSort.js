eb1047f50c055616cda76929fa235668c1e11a62
Ryan Kirsch <agalloch88@gmail.com>
2022-12-18 13:41:17 -0500

    BubbleSort solution 1

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -8,3 +8,27 @@
 // [2, 3, 5, 5, 6, 8, 9]
 
 // Solution 1:
+
+function bubbleSort(array) {
+    let isSorted = false;
+    let counter = 0;
+
+    while (!isSorted) {
+        isSorted = true;
+
+        for (let i = 0; i < array.length - 1 - counter; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(i, i + 1, array);
+                isSorted = false;
+            }
+        }
+        counter++;
+    }
+    return array;
+}
+
+function swap(i, j, array) {
+    let temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
