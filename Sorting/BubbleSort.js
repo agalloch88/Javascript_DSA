645363380d8a9af75fd7c75b7640bb8fdc5a4f14
Ryan Kirsch <agalloch88@gmail.com>
2022-12-18 13:37:45 -0500

    BubbleSort start

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,10 @@
+// Write a function which takes in an array of integers and returns a sorted version of the
+// input array. To accomplish the sort, use the Bubble Sort algorithm.
+
+// Sample Input:
+// array = [8, 5, 2, 9, 5, 6, 3]
+
+// Sample Output:
+// [2, 3, 5, 5, 6, 8, 9]
+
+// Solution 1:
