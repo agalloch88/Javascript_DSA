be8d1b3c6035a7911a3dbf6bfe237d72b020dfbb
Ryan Kirsch <agalloch88@gmail.com>
2022-12-19 17:27:37 -0500

    Semordnilap solution 1 comments

--- Strings/Semordnilap.js
+++ Strings/Semordnilap.js
@@ -13,18 +13,31 @@
 
 // Solution 1:
 
+// iterative solution using JS set for O(1) lookup time, reversing given word and checking for match
+
+// O(n * m) space due to storing wordSet of n items, with longest item of m length
+// O(n * m) time due to checking for n items with a max length of m characters
+
 function saemordlinap(words) {
+    // create a new JS Set from the words input, and store in variable wordSet
+    // Sets have O(1) lookup time, so this is the best data structure to use for these comparisons
     let wordSet = new Set(words);
+    // initialize empty array to hold any found pairs, and store in variable semordlinapPairs
     let semordlinapPairs = [];
-
+    // iterate over every individual word string in the words input array
     for (let word of words) {
+        // take the current word, split off the '', reverse the string, then join it back with '' for comparison, and store this reversed string in variable reverse
         let reverse = word.split('').reverse().join('');
-
+        // check whether the value stored in reverse exists in wordSet using the Set .has() method, AND that the reversed string is not a palindrome of the current word
         if (wordSet.has(reverse) && reverse !== word) {
+            // if both conditions above are true, push an array pair of the value for word and reverse into semordlinapPairs array
             semordlinapPairs.push([word, reverse]);
+            // remove the value stored in reverse from the wordSet using the Set .delete() method, so it is not duplicated by accident
             wordSet.delete(reverse);
+            // remove the current word from the wordSet using the Set .delete() method, so it is not duplicated by accident
             wordSet.delete(word);
         }
     }
+    // once every input word is checked, return all the found semordlinapPairs
     return semordlinapPairs;
 }
