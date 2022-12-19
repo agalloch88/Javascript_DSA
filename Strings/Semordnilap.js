2a13a973dfa8e4748492049ffda8de423d8084d3
Ryan Kirsch <agalloch88@gmail.com>
2022-12-19 17:12:34 -0500

    Semordnilap start

---
+++ Strings/Semordnilap.js
@@ -0,0 +1,14 @@
+// Write a function which takes in a list of unique strings, and returns a list of semordlinap pairs.
+
+// A semordnilap pair is defined as a set of different strings where the reverse of one word is the same as the forward version of the other. For example, the words 'diaper'
+// and 'repaid' are a semordnilap pair, as are the words 'palindromes' and 'semordnilap'.
+
+// The order of the returned pairs, and the order of the strings within each pair, does not matter in the context of the problem.
+
+// Sample Input:
+// words = ['diaper', 'abc', 'test', 'cba', 'repaid']
+
+// Sample Output:
+// [['diaper', 'repaid'], ['abc', 'cba']]
+
+// Solution 1:
