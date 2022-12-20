d85a9d2ab3417d3b5747635419d2c50434086ddf
Ryan Kirsch <agalloch88@gmail.com>
2022-12-20 09:15:39 -0500

    ZeroSumSubarray solution 1 comments

--- Arrays/ZeroSumSubarray.js
+++ Arrays/ZeroSumSubarray.js
@@ -12,16 +12,29 @@
 
 // Solution 1:
 
-function zeroSumSubarray(nums) {
-    let sums = new Set([0]);
-    let currentSum = 0;
+// iterative solution using JS Set to track all sums and mathematical principle that seeing currentSum more than once means set exists from previously-encountered-index + 1 to current index
 
+// O(n) space due to storing potentially n values from nums in new Set
+// O(n) time due to iterating over up to n values from nums looking for zero sum subarray
+
+function zeroSumSubarray(nums) {
+    // create new JS set and initialize with 0 in case it is found elsewhere in nums, and store in variable sums
+    let sums = new Set([0]);
+    // set up variable currentSum to track current subarray total and set to 0
+    let currentSum = 0;
+    // iterate over every number in nums
     for (let num of nums) {
+        // add the current num to the current value of currentSum
         currentSum += num;
+        // check whether the current value of currentSum is already in the sums set
+        // if current value of currentSum was encountered previously, this means there is mathematical certainty of a zero sum subarray within nums
         if (sums.has(currentSum)) {
+            // therefore, since this is established, return true
             return true;
         }
+        // add the currentSum value to sums so it's logged in case encountered again
         sums.add(currentSum);
     }
+    // if iterate over every number in nums and never return true, there is no zero sum subarray, so return false
     return false;
 }
