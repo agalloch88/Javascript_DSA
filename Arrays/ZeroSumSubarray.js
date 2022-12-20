39123ea33c30cd9004db766e5952ef4db7547372
Ryan Kirsch <agalloch88@gmail.com>
2022-12-20 07:44:09 -0500

    ZeroSumSubarray start

---
+++ Arrays/ZeroSumSubarray.js
@@ -0,0 +1,13 @@
+// The problem presents a list of integers, nums. Write a function which returns a boolean representing whether there exists a zero-sum subarray within nums.
+
+// A zero-sum subarray is any subarray where all of the values add up to zero. A subarray in the context of the problem is any contiguous section of the array. For the
+// purposes of the problem, a subarray can be as small as one element, and as long as the entire original array.
+
+// Sample Input:
+// nums = [-5, -5, 2, 3, -2]
+
+// Sample Output:
+// True
+// the subarray [-5, 2, 3] has a sum of 0
+
+// Solution 1:
