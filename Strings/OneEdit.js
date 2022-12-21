a26b2dadfe06f50a88332310a70e19edb2010a46
Ryan Kirsch <agalloch88@gmail.com>
2022-12-21 07:58:28 -0500

    OneEdit start

---
+++ Strings/OneEdit.js
@@ -0,0 +1,18 @@
+// The problem presents two strings, stringOne and stringTwo. Write a function which determines if these two strings can be made equal using only one edit.
+
+// There are three possible edits to make:
+// * REPLACE: One character in one string is swapped for a different character.
+// * ADD: One character is added at any index in one of the strings.
+// * REMOVE: One character is removed at any index in one of the strings.
+
+// Note that both strings will contain at least one character. If the strings are the same, the function should return true.
+
+// Sample Input:
+// stringOne = "hello"
+// stringTwo = "hollo"
+
+// SampleOutput:
+// True
+// A single replace at index one of either string will make the strings equal.
+
+// Solution 1:
