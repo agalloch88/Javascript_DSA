505d2501490b098e10c00e91419dfda5f4adbe8a
Ryan Kirsch <agalloch88@gmail.com>
2022-12-21 08:11:55 -0500

    OneEdit solution 1

--- Strings/OneEdit.js
+++ Strings/OneEdit.js
@@ -16,3 +16,25 @@
 // A single replace at index one of either string will make the strings equal.
 
 // Solution 1:
+
+function oneEdit(stringOne, stringTwo) {
+    let lengthOne = stringOne.length;
+    let lengthTwo = stringTwo.length;
+
+    if (Math.abs(lengthOne - lengthTwo) > 1) {
+        return false;
+    }
+
+    for (let i = 0; i < Math.min(lengthOne, lengthTwo); i++) {
+        if (stringOne[i] !== stringTwo[i]) {
+            if (lengthOne > lengthTwo) {
+                return stringOne.slice(i + 1) === stringTwo.slice(i);
+            } else if (lengthTwo > lengthOne) {
+                return stringOne.slice(i) === stringTwo.slice(i + 1);
+            } else {
+                return stringOne.slice(i + 1) === stringTwo.slice(i + 1);
+            }
+        }
+    }
+    return true;
+}
