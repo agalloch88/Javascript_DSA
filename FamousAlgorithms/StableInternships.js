ad843d8d1173765552bcf8bf51d01d04481ab4b9
Ryan Kirsch <agalloch88@gmail.com>
2023-01-04 08:59:29 -0500

    StableInternships solution 1 comments

--- FamousAlgorithms/StableInternships.js
+++ FamousAlgorithms/StableInternships.js
@@ -39,47 +39,77 @@
 
 // Solution 1:
 
-function stableInternships(interns, teams) {
-    let chosenInterns = {};
-    let freeInterns = interns.map((_, i) => i);
-    let currentInternChoices = new Array(interns.length).fill(0);
+// iterative solution using a stack to keep track of unassigned interns, and weighting intern selections
+// to provide optimal team assignment
 
+// O(n ^ 2) time due to nested for operations, and other n ^ 2 operations, converges to n ^ 2
+// O(n ^ 2) space due to chosen interns JS object, map of n interns, array of n interns and other data strutures
+
+function stableInternships(interns, teams) {
+    // set up empty JS object and store in variable chosenInterns
+    let chosenInterns = {};
+    // map over the interns input and grab the intern numbers, meaning ultimately how many interns there are to place
+    let freeInterns = interns.map((_, i) => i);
+    // create a new array the same length as interns input and fill with 0's
+    let currentInternChoices = new Array(interns.length).fill(0);
+    // set up empty array which will hold a map of the team preferences
     let teamMaps = [];
+    // iterate over every team in the teams input
     for (let team of teams) {
+        // set up empty JS object and store in variable rank
         let rank = {};
+        // call forEach on the current team and use parameters of internNum and the index (i)
         team.forEach((internNum, i) => {
+            // set the internNum in rank equal to i
             rank[internNum] = i;
         });
+        // push the ranking into the teamMaps array
         teamMaps.push(rank);
     }
-
+    // while there are still items in the freeInterns stack, meaning interns to assign, keep looping
     while (freeInterns.length > 0) {
+        // pop the top item off the freeInterns stack and store value in variable internNum
         let internNum = freeInterns.pop();
-
+        // grab the value at internNum in the interns input, and store in variable intern
         let intern = interns[internNum];
+        // grab value at internNum in currentInternChoices of intern, and store in variable teamPreference
         let teamPreference = intern[currentInternChoices[internNum]];
+        // incremnent the value at internNum in currentInternChoices by 1
         currentInternChoices[internNum] += 1;
-
+        // if the value of teamPreference is NOT in chosenInterns, execute below
         if (!(teamPreference in chosenInterns)) {
+            // set the internNum equal to the value at teamPreference in chosenInterns, then continue
             chosenInterns[teamPreference] = internNum;
             continue;
         }
-
+        // grab the value at teamPreference in chosenInterns, and store in variable previousIntern
         let previousIntern = chosenInterns[teamPreference];
+        // grab the value at previousIntern teamPreference in teamMaps, and store in variable previousInternRank
         let previousInternRank = teamMaps[teamPreference][previousIntern];
+        // grab the value at internNum teamPreference in teamMaps, and store in variable currentInternRank
         let currentInternRank = teamMaps[teamPreference][internNum];
-
+        // if the currentInternRank is smaller than the previousInternRank, this means the
+        // current intern is more desirable to the given team, so execute below
         if (currentInternRank < previousInternRank) {
+            // push previousInterns into the freeInterns array
             freeInterns.push(previousIntern);
+            // set the internNum equal to the value at teamPreference in chosenInterns
             chosenInterns[teamPreference] = internNum;
+        // if the previousInternRank is instead greater than the currentInternRank, this
+        // means the previous intern is more desirable to the team, so execute below
         } else {
+            // push the internNum, which is the current intern, back into freeInterns array
             freeInterns.push(internNum);
         }
     }
-
+    // need to now convert the chosenInterns JS object into a two-dimensional array
+    // grab the entries value of the chosenInterns JS object, and map over them using parameters
+    // teamNum and internNum, setting the internNum as the first value in pair and the parsed
+    // integer of teamNum as the second value in the pair
     let matches = Object.entries(chosenInterns).map(([teamNum, internNum]) => [
         internNum,
         parseInt(teamNum),
     ]);
+    // return the matches, which could be one of many possible, correct combinations
     return matches;
 }
