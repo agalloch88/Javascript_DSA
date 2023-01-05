41e7ab32ea6446c0294c00fa1fa7a26517276619
Ryan Kirsch <agalloch88@gmail.com>
2023-01-05 07:29:36 -0500

    LongestBalancedSubstring start

---
+++ Strings/LongestBalancedSubstring.js
@@ -0,0 +1,14 @@
+// Write a function which takes in a string made up of parentheses, so ( these two ). The function should return an integer representing the length of the longest balanced
+// substring with regards to parentheses.
+
+// A string is said to be balanced if it has as many opening parentheses as it has closing parentheses, and if no parenthesis is unmatched. Note that an opening parenthesis
+// cannot match a closing parenthesis which comes before it, and similarly, a closing parenthesis cannot match an opening parenthesis which comes afert it.
+
+// Sample Input:
+// string = "(()))("
+
+// Sample Output:
+// 4
+// The longest balanced substring is (())
+
+// Solution 1:
