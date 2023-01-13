9ea02e64ddc3b4741ebb6210a91948c0fd021540
Ryan Kirsch <agalloch88@gmail.com>
2023-01-13 15:24:45 -0500

    MergeLinkedLists start

---
+++ LinkedLists/MergeLinkedLists.js
@@ -0,0 +1,16 @@
+// The problem presents two Linked Lists of potentially unequal length. These Linked Lists potentially merge at a shared intersection node. Write a function which returns the intersection
+// node, or returns None / null if there is no intersection.
+
+// Each LinkedList node has an integer value, as well as a next node pointing to the next node in the list, or to None / null if it is the tail of the list.
+
+// Note: the function should return an existing node. It should not modify either Linked List, and it should not create any new Linked Lists.
+
+// Sample Input:
+// linkedListOne = 2 -> 3 -> 1 -> 4
+// linkedListTwo = 8 -> 7 -> 1 -> 4
+
+// Sample Output:
+// 1 -> 4
+// The lists intersect at the node with value 1
+
+// Solution 1:
