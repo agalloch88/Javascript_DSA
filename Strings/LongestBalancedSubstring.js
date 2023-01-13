355f5e0cb5997d8f76b703b308a231c86e0c289d
Ryan Kirsch <agalloch88@gmail.com>
2023-01-13 15:16:47 -0500

    LongestBalancedSubstring solution 4 comments

--- Strings/LongestBalancedSubstring.js
+++ Strings/LongestBalancedSubstring.js
@@ -169,36 +169,54 @@ function longestBalancedSubstring(string) {
 
 // Solution 4:
 
+// optimized version of solution 3, abstracting repeating code to ternaries, and using helper function
+
+// O(n) time due to two passes over input, first left to right then right to left, 2n converges to n
+// O(1) space due to only storing a few variables
+
+// main function which takes in the input string
 function longestBalancedSubstring(string) {
+    // return the maximum between the return values of helper function looking left to right or return values of helper function looking right to left
     return Math.max(getLongestBalancedInDirection(string, true), getLongestBalancedInDirection(string, false));
 }
-
+// helper function which takes in the input string and boolean value determining direction, either left to right (true) or right to left (false)
 function getLongestBalancedInDirection(string, leftToRight) {
+    // the function will make two passes through the input, one from left to right and one from right to left, and directionality is controlled by the
+    // leftToRight variable value, either true or false. the three below variables will be set depending on this variable using the ternary operator.
+    // check whether leftToRight is true, and if so set openingParens variable equal to (, otherwise set it to )
     let openingParens = leftToRight ? '(' : ')';
+    // check whether leftToRight is true, and if so, set the startIdx variable equal to 0, otherwise set it to the last index in the input string
     let startIdx = leftToRight ? 0 : string.length - 1;
+    // check whether leftTToRight is true, and if so, set the variable step equal to 1, otherwise set it to -1
     let step = leftToRight ? 1 : -1;
-
+    // set the maxLength, openingCount, and closingCount variables equal to 0 to start
     let maxLength = 0;
     let openingCount = 0;
     let closingCount = 0;
+    // set the current idx equal to the value in startIdx
     let idx = startIdx;
-
+    // keep iterating so long as idx is in bounds from the start and end of the input string
     while (idx >= 0 && idx < string.length) {
+        // grab the value at idx in string, and store in variable char
         let char = string[idx];
-
+        // if the current char is equal to the current definition of openingParens, and if so, increment openingCount by 1
         if (char === openingParens) {
             openingCount++;
+        // otherwise, increment closingCount by 1
         } else {
             closingCount++;
         }
-
+        // if openingCount and closingCount are equal, then set maxLength equal to the maximum between current value of maxLength and the closingCount squared
         if (openingCount === closingCount) {
             maxLength = Math.max(maxLength, closingCount * 2);
+        // otherwise, if closingCount is greater than openingCount, this is not a balanced substring, so reset the openingCount and closingCount variables to 0
         } else if (closingCount > openingCount) {
             openingCount = 0;
             closingCount = 0;
         }
+        // increment the idx variable by the current value of step variable
         idx += step;
     }
+    // return the current value of maxLength
     return maxLength;
 }
