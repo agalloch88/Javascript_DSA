71d38a233930e44786bdf925b58210acbb514b2d
Ryan Kirsch <agalloch88@gmail.com>
2023-02-05 15:43:25 -0500

    MergeLinkedLists solution 1 comments

--- LinkedLists/MergeLinkedLists.js
+++ LinkedLists/MergeLinkedLists.js
@@ -15,6 +15,12 @@
 
 // Solution 1:
 
+// iterative solution using a set to store first linked list nodes, then looking for the convergence node, if any, between the linked lists
+
+// O(n + m) time, where n is the length of list one and m is the length of list two
+// O(n) space due to storing list one nodes in set
+
+// base LinkedList class, with every node having a numeric value and next pointer
 class LinkedList {
     constructor(value) {
         this.value = value;
@@ -22,23 +28,31 @@ class LinkedList {
     }
 }
 
+// main function which takes in the two linked lists to compare
 function mergeLinkedLists(linkedListOne, linkedListTwo) {
+    // create a new Set data structure, and store in variable listOneNodes
     let listOneNodes = new Set();
+    // grab head value in linkedListOne and store in variable currentNodeOne
     let currentNodeOne = linkedListOne;
-
+    // continue iterating so long as currentNodeOne doest not equal null, meaning not at the tail
     while (currentNodeOne !== null) {
+        // use the add method to append value of currentNodeOne into the listOneNodes Set
         listOneNodes.add(currentNodeOne);
+        // set currentNodeOne equal to the value of currentNodeOne.next
         currentNodeOne = currentNodeOne.next;
     }
-
+    // grab head value in linkedListTwo and store in variable currentNodeTwo
     let currentNodeTwo = linkedListTwo;
-
+    // continue iterating so long as currentNodeTwo does not equal null, meaning not at the tail
     while (currentNodeTwo !== null) {
+        // use has method to check whether the value of currentNodeTwo exists in linkedListOne via the stored values
         if (listOneNodes.has(currentNodeTwo)) {
+            // if it does, return the value of currentNodeTwo as this is the convergence point
             return currentNodeTwo;
         }
+        // should the above if block not return, still searching for convergence point, so set currentNodeTwo equal to the next value of currentNodeTwo
         currentNodeTwo = currentNodeTwo.next;
     }
-
+    // if break out of while loop again, reached the tail of linkedListOne without returning an answer, so there must be no convergence point, and should return null
     return null;
 }
