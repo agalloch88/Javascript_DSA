1a0fef7ccc0cc0fc21d7b440bfb0431268ea5a91
Ryan Kirsch <agalloch88@gmail.com>
2023-02-26 14:40:13 -0500

    MergeBinaryTrees start

---
+++ BinaryTrees/MergeBinaryTrees.js
@@ -0,0 +1,25 @@
+// Given two binary trees, merge them and return the resulting tree. If two nodes overlap during the merger, then sum the values; otherwise, user the existing node.
+
+// Note that a solution can either mutate the existing trees, or return a new binary tree.
+
+// Sample Input:
+
+// tree1 =  1
+//         / \
+//        3   2
+//       / \
+//      7   4
+
+// tree2 =  1
+//         / \
+//        5   9
+//       /   / \
+//      2   7   6
+
+// Sample Output:
+
+// output =     2
+//             / \
+//            8   11
+//           / \  / \
+//          9  4 7   6
