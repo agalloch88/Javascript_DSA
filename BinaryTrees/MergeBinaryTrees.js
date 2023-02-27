c0fe06c5b520ebff095535152ce985e3a90d9fee
Ryan Kirsch <agalloch88@gmail.com>
2023-02-27 07:46:09 -0500

    MergeBinaryTrees solution 1

--- BinaryTrees/MergeBinaryTrees.js
+++ BinaryTrees/MergeBinaryTrees.js
@@ -23,3 +23,28 @@
 //            8   11
 //           / \  / \
 //          9  4 7   6
+
+// Solution 1:
+
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function mergeBinaryTrees(tree1, tree2) {
+    if (tree1 === null) {
+        return tree2;
+    }
+
+    if (tree2 === null) {
+        return tree1;
+    }
+
+    tree1.value += tree2.value;
+    tree1.left = mergeBinaryTrees(tree1.left, tree2.left);
+    tree1.right = mergeBinaryTrees(tree1.right, tree2.right);
+    return tree1;
+}
