445fa69cb6053a28f71d75902ae181fce5c58eb8
Ryan Kirsch <agalloch88@gmail.com>
2023-03-02 14:47:32 -0500

    SymmetricalTree start

---
+++ BinaryTrees/SymmetricalTree.js
@@ -0,0 +1,17 @@
+// Write a function which takes in a Binary Tree, and returns if the input tree is symmetrical. A tree is symmetrical if the left and right subtrees are mirror images of each other.
+
+// Each BinaryTree node has an integer value, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves, or None/null.
+
+// Sample Input:
+
+// tree =   1
+//         / \
+//        2   2
+//       / \ / \
+//      3  4 4  3
+//     / \     / \
+//    5   6   6   5
+
+// Sample Output:
+
+// true
