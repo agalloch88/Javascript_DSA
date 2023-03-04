bba25d909cd611f3e0a463228605571b46039129
Ryan Kirsch <agalloch88@gmail.com>
2023-03-04 08:44:53 -0800

    MaxPathSumInBinaryTree start

---
+++ BinaryTrees/MaxPathSumInBinaryTree.js
@@ -0,0 +1,21 @@
+// Write a function which takes in a Binary Tree and returns its max path sum.
+
+// A path is a collection of connected nodes in a tree, where no node is connected to more than
+// two other nodes. A path sum, therefore, is the sum of the values of the nodes in a particular
+// path.
+
+// Each BinaryTree node has an integer value, a left child node, and a right child node. Children
+// nodes can either be BinaryTree nodes themselves, or None/null.
+
+// Sample Input:
+
+// tree = 1
+//       / \
+//      2   3
+//     / \ / \
+//    4  5 6  7
+
+// Sample Output:
+
+// 18
+// 5 + 2 + 1 + 3 + 7
