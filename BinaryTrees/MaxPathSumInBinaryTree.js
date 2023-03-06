37cee9df12905b6f0c98529500665a3c19fd06f9
Ryan Kirsch <agalloch88@gmail.com>
2023-03-06 06:59:41 -0800

    MaxPathSumInBinaryTree solution 1 comments

--- BinaryTrees/MaxPathSumInBinaryTree.js
+++ BinaryTrees/MaxPathSumInBinaryTree.js
@@ -22,24 +22,44 @@
 
 // Solution 1:
 
+// recursive solution checking path and branch totals for highest count and returning optimal combo
+
+// O(n) time due to checking n nodes in tree
+// O(log (n)) space due to recursive calls on call stack
+
+// main function which takes in binary tree
 function maxPathSum(tree) {
+  // set blank value and maxSum equal to call to helper function, passing in the tree
   let [_, maxSum] = findMaxSum(tree);
+  // return the maxSum, which should be the final answer
   return maxSum;
 }
-
+// helper function to deduce answer
 function findMaxSum(tree) {
+  // base case
+  // check whether the tree is null, and if so, return 0 and -Infinity for the maxSumAsBranch and maxPathSum, respectively
   if (tree === null) {
     return [0, -Infinity];
   }
 
+  // recursive case
+  // set leftMaxSumAsBranch and leftMaxPathSum equal to call of findMaxSum, passing in the tree.left value to look left
   let [leftMaxSumAsBranch, leftMaxPathSum] = findMaxSum(tree.left);
+  // set rightMaxSumAsBranch and rightMaxPathSum equal to call of findMaxSum, passing in the tree.right value to look right
   let [rightMaxSumAsBranch, rightMaxPathSum] = findMaxSum(tree.right);
+  // set maxChildSumAsBranch equal to the maximum of the previous two branch sums
   let maxChildSumAsBranch = Math.max(leftMaxSumAsBranch, rightMaxSumAsBranch);
-
+  // destructure the value off of the tree, as this is the root and want to see whether this will be part of the path or not
   let { value } = tree;
+  // set variable maxSumAsBranch equal to the max of the maxChildSumAsBranch plus the value, or the value itself, whichever is greater, using this as a branch itself
   let maxSumAsBranch = Math.max(maxChildSumAsBranch + value, value);
-  let maxSumAsRootNode = Math.max(leftMaxSumAsBranch + value + rightMaxSumAsBranch, maxSumAsBranch);
+  // set variable maxSumAsRootNode equal to the maximum between the left and right MaxSumAsBranch totals, including the root node/value to make ^ shape, or just the maxSumAsBranch found above
+  let maxSumAsRootNode = Math.max(
+    leftMaxSumAsBranch + value + rightMaxSumAsBranch,
+    maxSumAsBranch
+  );
+  // set final variable maxPathSum equal to the maximum between leftMaxPathSum, rightMaxPathSum, and maxSumAsRootNode, to determine the optimal value to return
   let maxPathSum = Math.max(leftMaxPathSum, rightMaxPathSum, maxSumAsRootNode);
-
+  // return the maxSumAsBranch and maxPathSum values for use in next recursive call
   return [maxSumAsBranch, maxPathSum];
 }
