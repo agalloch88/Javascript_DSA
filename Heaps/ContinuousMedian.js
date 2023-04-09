427f467789e948ca82013b8e3f247809d8210f09
Ryan Kirsch <agalloch88@gmail.com>
2023-04-09 17:57:29 -0400

    ContinuousMedian solution 1 finish Heap class

--- Heaps/ContinuousMedian.js
+++ Heaps/ContinuousMedian.js
@@ -53,15 +53,49 @@ class Heap {
     }
 
     buildHeap(array) {
-
+        let firstParentIdx = Math.floor((array.length - 2) / 2);
+        for (let currentIdx = 0; currentIdx >= 0; currentIdx--) {
+            this.siftDown(currentIdx, array.length - 1, array);
+        }
+        return array;
     }
 
     siftDown(currentIdx, endIdx, heap) {
+        let childOneIdx = currentIdx * 2 + 1;
+        while (childOneIdx <= endIdx) {
+            let childTwoIdx = currentIdx * 2 + 2 <= endIdx? currentIdx * 2 + 2 : -1
+            let idxToSwap;
+            if (childTwoIdx !== -1) {
+                if (this.comparisonFunc(heap[childTwoIdx], heap[childOneIdx])) {
+                    idxToSwap = childTwoIdx;
+                } else {
+                    idxToSwap = childOneIdx;
+                }
+            } else {
+                idxToSwap = childOneIdx;
+            }
 
+            if (this.comparisonFunc(heap[idxToSwap], heap[currentIdx])) {
+                this.swap(currentIdx, idxToSwap, heap);
+                currentIdx = idxToSwap;
+                childOneIdx = currentIdx * 2 + 1;
+            } else {
+                return;
+            }
+        }
     }
 
     siftUp(currentIdx, heap) {
-
+        let parentIdx = Math.floor((currentIdx - 1) / 2);
+        while (currentIdx > 0) {
+            if (this.comparisonFunc(heap[currentIdx], heap[parentIdx])) {
+                this.swap(currentIdx, parentIdx, heap);
+                currentIdx = parentIdx;
+                parentIdx = Math.floor((currentIdx - 1) / 2);
+            } else {
+                return;
+            }
+        }
     }
 
     peek() {
@@ -73,7 +107,9 @@ class Heap {
     }
 
     insert(value) {
-
+        this.heap.push(value);
+        this.length++;
+        this.siftUp(this.length - 1, this.heap);
     }
 
     swap(i, j, heap) {
