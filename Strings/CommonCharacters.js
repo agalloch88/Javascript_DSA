432619e11a6ba5b086ac8e98e91418f18b7cf803
Ryan Kirsch <agalloch88@gmail.com>
2023-04-12 16:15:03 -0400

    CommonCharacters start

---
+++ Strings/CommonCharacters.js
@@ -0,0 +1,13 @@
+// Write a function which takes in a non-empty array of non-empty strings, and which returns an array of characters common to all strings in the array, ignoring multiplicity.
+
+// Note that the strings are not guaranteed to only contain alphanumeric characters. The array returned by the function can be in any order.
+
+// Sample Input:
+
+// strings = ["abc", "bcd", "cbaccd"]
+
+// Sample Output:
+
+// ["b", "c"]
+
+// Solution 1:
