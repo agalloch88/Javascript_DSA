0ff638dd0e2d2986c07768bf201e89aa25b85bee
Ryan Kirsch <agalloch88@gmail.com>
2023-04-13 07:50:58 -0400

    CommonCharacters solution 1

--- Strings/CommonCharacters.js
+++ Strings/CommonCharacters.js
@@ -11,3 +11,28 @@
 // ["b", "c"]
 
 // Solution 1:
+
+function commonCharacters(strings) {
+    let characterCounts = {};
+
+    for (let string of strings) {
+        let uniqueStringCharacters = new Set(string);
+
+        for (let character of uniqueStringCharacters) {
+            if (!(character in characterCounts)) {
+                characterCounts[character] = 0;
+            }
+            characterCounts[character]++;
+        }
+    }
+
+    let finalCharacters = [];
+
+    for (let [character, count] of Object.entries(characterCounts)) {
+        if (count === strings.length) {
+            finalCharacters.push(character);
+        }
+    }
+
+    return finalCharacters;
+}
