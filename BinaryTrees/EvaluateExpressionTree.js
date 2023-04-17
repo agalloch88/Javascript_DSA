4b4dccbc21e933b868db896b9308f66f222ae098
Ryan Kirsch <agalloch88@gmail.com>
2023-04-17 07:43:14 -0400

    EvaluateExpressionTree solution 1

--- BinaryTrees/EvaluateExpressionTree.js
+++ BinaryTrees/EvaluateExpressionTree.js
@@ -30,3 +30,32 @@
 // (((2 * 3) - 2) + (8 / 3))
 
 // Solution 1:
+
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function evaluateExpressionTree(tree) {
+    if (tree.value >= 0) {
+        return tree.value;
+    }
+
+    let leftValue = evaluateExpressionTree(tree.left);
+    let rightValue = evaluateExpressionTree(tree.right);
+
+    if (tree.value === -1) {
+        return leftValue + rightValue;
+    }
+    if (tree.value === -2) {
+        return leftValue - rightValue;
+    }
+    if (tree.value === -3) {
+        return Math.trunc(leftValue / rightValue);
+    }
+
+    return leftValue * rightValue;
+}
