016d1a91f4b91ef7b784ef680016689f2a5d11fe
Ryan Kirsch <agalloch88@gmail.com>
2023-04-19 07:18:53 -0400

    MiddleNode start

---
+++ LinkedLists/MiddleNode.js
@@ -0,0 +1,15 @@
+// The problem presents a Linked List with at least one node. Write a function which returns the middle node of the Linked List. If there are two middle nodes 
+// (as in the case with an even-length list) the function should return the second of the two nodes.
+
+// Each LinkedList node has an integer value, as well as a next node pointing to the next node in the list, or to None/null if it is the tail node of the list.
+
+// Sample Input:
+
+// linkedList = 2 -> 7 -> 3 -> 5
+
+// Sample Output:
+
+// 3 -> 5
+// The middle could be either node, but as problem requires, would return 5 as the second of the two nodes.
+
+// Solution 1:
