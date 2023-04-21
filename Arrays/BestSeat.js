282843c5296aa9200b80b639a316de514a61288c
Ryan Kirsch <agalloch88@gmail.com>
2023-04-21 07:48:01 -0400

    BestSeat solution 1

--- Arrays/BestSeat.js
+++ Arrays/BestSeat.js
@@ -17,3 +17,24 @@
 // 4
 
 // Solution 1:
+
+function bestSeat(seats) {
+    let bestSeat = -1;
+    let maxSpace = 0;
+    let left = 0;
+
+    while (left < seats.length) {
+        let right = left + 1;
+
+        while (right < seats.length && seats[right] === 0) {
+            right++;
+        }
+
+        let availableSpace = right - left - 1;
+        if (availableSpace > maxSpace) {
+            bestSeat = Math.floor((left + right) / 2);
+            maxSpace = availableSpace;
+        }
+    }
+    return bestSeat;
+}
