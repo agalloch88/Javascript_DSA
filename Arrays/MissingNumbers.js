794739af937e54d509cd9ad0ac4349dd6f1415e2
Ryan Kirsch <agalloch88@gmail.com>
2023-04-29 03:32:43 -0400

    MissingNumbers solution 3

--- Arrays/MissingNumbers.js
+++ Arrays/MissingNumbers.js
@@ -92,3 +92,36 @@ let arrayFromAToB = (a, b) => {
 }
 // helper function which sums values, using reduce method to add values a and b together
 let sum = array => array.reduce((a, b) => a + b);
+
+// Solution 3:
+
+function missingNumbers(nums) {
+    let solutionXOR = 0;
+    for (let i = 0; i < nums.length + 3; i++) {
+        solutionXOR ^= i;
+        if (i < nums.length) {
+            solutionXOR ^= nums[i];
+        }
+    }
+
+    let solution = [0, 0];
+    let setBit = solutionXOR & -solutionXOR;
+    for (let i = 0; i < nums.length + 3; i++) {
+        if ((i & setBit) === 0) {
+            solution[0] ^= i;
+        } else {
+            solution[1] ^= i;
+        }
+
+        if (i < nums.length) {
+            if ((nums[i] & setBit) === 0) {
+                solution[0] ^= nums[i];
+            } else {
+                solution[1] ^= nums[i];
+            }
+        }
+    }
+
+    solution.sort((a, b) => a - b);
+    return solution;
+}
