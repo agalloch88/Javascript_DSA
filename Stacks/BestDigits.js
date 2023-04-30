87538e52d2b4c08fee7a85ad7bfdefb2b1190ea2
Ryan Kirsch <agalloch88@gmail.com>
2023-04-30 09:41:08 -0400

    BestDigits start

---
+++ Stacks/BestDigits.js
@@ -0,0 +1,17 @@
+// Write a function which takes a positive integer, represented as a string called number, and an integer called numDigits. Remove numDigits from the string, so that the number
+// represented by the string is as large as possible afterwards.
+
+// Note that the order of the remaining digits cannot be changed. For the purposes of the problem, assume numDigits will always the less than the length of variable number, and
+// greater than or equal to 0.
+
+// Sample Input:
+
+// number = "462839"
+// numDigits = 2
+
+// Sample Output
+
+// "6839"
+// remove the digits 4 and 2
+
+// Solution 1:
