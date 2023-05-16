a4a879d5b97601c081585b747ea663a8b50af305
Ryan Kirsch <agalloch88@gmail.com>
2023-05-16 15:24:31 -0400

    LongestCommonSubsequence start

---
+++ DynamicProgramming/LongestCommonSubsequence.js
@@ -0,0 +1,18 @@
+// Write a function which takes in two strings, and returns the longest common subsequence shared between the two input strings.
+
+// A subsequence of a string is defined as a set of characters which are not necessarily adjacent in the string, but which are in the same order as they appear in the string.
+// For instance, the characters ["a", "c", "d"] form a subsequence of the string "abcd", and so do the characters ["b", "d"]. Note that a single character in a string, as well
+// as the string itself, are both valid subsequences of the string.
+
+// For the context of the problem, assume that there will be only one longest common subsequence.
+
+// Sample Input:
+
+// str1 = "ZXVVYZW"
+// str2 = "XKYKZPW"
+
+// Sample Output:
+
+// ["X", "Y", "Z", "W"]
+
+// Soultion 1:
