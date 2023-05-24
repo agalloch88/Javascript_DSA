3a467af5ba4a4a850a91e79a835a803b7cf24317
Ryan Kirsch <agalloch88@gmail.com>
2023-05-24 08:36:01 -0400

    LongestCommonSubsequence solution 4

--- DynamicProgramming/LongestCommonSubsequence.js
+++ DynamicProgramming/LongestCommonSubsequence.js
@@ -184,3 +184,41 @@ function buildSequence(lcs) {
 }
 
 // Solution 4:
+
+function longestCommonSubsequence(str1, str2) {
+    let lengths = [];
+
+    for (let i = 0; i < str2.length + 1; i++) {
+        lengths.push(new Array(str1.length + 1).fill(0));
+    }
+
+    for (let i = 1; i < str2.length + 1; i++) {
+        for (let j = 1; j < str1.length + 1; j++) {
+            if (str2[i - 1] === str1[j - 1]) {
+                lengths[i][j] = lengths[i - 1][j - 1] + 1;
+            } else {
+                lengths[i][j] = Math.max(lengths[i - 1][j], lengths[i][j - 1]);
+            }
+        }
+    }
+    return buildSequence(lengths, str1);
+}
+
+function buildSequence(lengths, string) {
+    let sequence = [];
+    let i = lengths.length - 1;
+    let j = lengths[0].length - 1;
+
+    while (i !== 0 && j !== 0) {
+        if (lengths[i][j] === lengths[i - 1][j]) {
+            i--;
+        } else if (lengths[i][j] === lengths[i][j - 1]) {
+            j--;
+        } else {
+            sequence.unshift(string[j - 1]);
+            i--;
+            j--;
+        }
+    }
+    return sequence;
+}
