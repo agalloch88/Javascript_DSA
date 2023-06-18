b676532ac48a9d9e1b75be22e17cdc91d8c35056
Ryan Kirsch <agalloch88@gmail.com>
2023-06-18 15:04:44 -0400

    KnapsackProblem solution 1

--- DynamicProgramming/KnapsackProblem.js
+++ DynamicProgramming/KnapsackProblem.js
@@ -23,3 +23,50 @@
 // [10, [1, 3]], which are items [4, 3] and [6, 7]
 
 // Solution 1:
+
+function knapsackProblem(items, capacity) {
+    let knapsackValues = [];
+
+    for (let i = 0; i < items.length + 1; i++) {
+        let row = new Array(capacity + 1).fill(0);
+        knapsackValues.push(row);
+    }
+
+    for (let i = 1; i < items.length + 1; i++) {
+        let currentWeight = items[i - 1][1];
+        let currentValue = items[i - 1][0];
+
+        for (let c = 0; c < capacity + 1; c++) {
+            if (currentWeight > c) {
+                knapsackValues[i][c] = knapsackValues[i - 1][c];
+            } else {
+                knapsackValues[i][c] = Math.max(
+                    knapsackValues[i - 1][c],
+                    knapsackValues[i - 1][c - currentWeight] + currentValue,
+                );
+            }
+        }
+    }
+    return [knapsackValues[items.length][capacity], getKnapsackItems(knapsackValues, items)];
+}
+
+function getKnapsackItems(knapsackValues, items) {
+    let sequence = [];
+    let i = knapsackValues.length - 1;
+    let c = knapsackValues[0].length - 1;
+
+    while (i > 0) {
+        if (knapsackValues[i][c] === knapsackValues[i - 1][c]) {
+            i -= 1;
+        } else {
+            sequence.unshift(i - 1);
+            c -= items[i - 1][1];
+            i -= 1;
+        }
+
+        if (c === 0) {
+            break;
+        }
+    }
+    return sequence;
+}
