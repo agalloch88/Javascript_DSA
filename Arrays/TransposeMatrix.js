6f47e37637a206d5ef761c63e784daec2554960e
Ryan Kirsch <agalloch88@gmail.com>
2023-07-04 19:57:00 -0400

    TransposedMatrix solution 1

--- Arrays/TransposeMatrix.js
+++ Arrays/TransposeMatrix.js
@@ -53,3 +53,17 @@
 // ]
 
 // Solution 1:
+
+function transposedMatrix(matrix) {
+    let transposedMatrix = [];
+
+    for (let col = 0; col < matrix[0].length; col++) {
+        let newRow = [];
+
+        for (let row = 0; row < matrix.length; row++) {
+            newRow.push(matrix[row][col]);
+        }
+        transposedMatrix.push(newRow);
+    }
+    return transposedMatrix;
+}
