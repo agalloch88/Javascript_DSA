06b7386c744dd46c11c5010de23c54364545b661
Ryan Kirsch <agalloch88@gmail.com>
2023-08-03 12:46:43 -0400

    LargestPark solution 1

--- Stacks/LargestPark.js
+++ Stacks/LargestPark.js
@@ -21,3 +21,39 @@
 // The park would go from row 1 to row 2, in columns 0 to 3, giving a total area of 6
 
 // Solution 1:
+
+function largestPark(land) {
+    let heights = new Array(land[0].length).fill(0);
+    let maxArea = 0;
+
+    for (let row of land) {
+        for (let columnIndex = 0; columnIndex < land[0].length; columnIndex++) {
+            heights[columnIndex] = row[columnIndex] === false ? heights[columnIndex] + 1 : 0;
+            
+        }
+        maxArea = Math.max(maxArea, largestRectangleHistogram(heights));
+    }
+    return maxArea;
+}
+
+function largestRectangleHistogram(heights) {
+    let stack = [];
+    let maxArea = 0;
+
+    for (let columnIndex = 0; columnIndex < heights.length; columnIndex++) {
+        while (stack.length > 0 && heights[columnIndex] < heights[stack[stack.length - 1]]) {
+            let height = heights[stack.pop()];
+            let width = stack.length === 0 ? columnIndex : columnIndex - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, width * height);
+        }
+        stack.push(columnIndex);
+    }
+
+    while (stack.length > 0) {
+        let height = heights[stack.pop()];
+        let width = stack.length === 0 ? heights.length : heights.length - stack[stack.length - 1] - 1;
+        maxArea = Math.max(maxArea, width * height);
+    }
+
+    return maxArea;
+}
