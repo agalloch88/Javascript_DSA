6d22f9dec3d95bec1e0cc728d436c5e098c72080
Ryan Kirsch <agalloch88@gmail.com>
2023-08-04 09:24:37 -0400

    LongestSubarrayWithSum start

---
+++ Arrays/LongestSubarrayWithSum.js
@@ -0,0 +1,16 @@
+// Write a function which takes in a non-empty array of non-negative integers, and a non-negative integer representing a target sum. The function should find the longest subarray where the values collectively
+// sum up to equal the target sum. The function should return an array containing the starting and ending index of this subarray, both inclusive.
+
+// If there is no subarray which sums up to the target sum, the function should return an empty array. For the purposes of the problem, assume the given inputs will only ever have one answer.
+
+// Sample Input:
+
+// array = [1, 2, 3, 4, 3, 3, 1, 2, 1, 2]
+// targetSum = 10
+
+// Sample Output:
+
+// [4, 8]
+// The longest subarray which sums to 10 starts at index 4 (3) and ends at index 8 (1), meaning 3 + 3 + 1 + 2 + 1
+
+// Solution 1:
