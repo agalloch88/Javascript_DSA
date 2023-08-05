3135f4b71018a3a036b9ef648e2c0a2ee371cb7c
Ryan Kirsch <agalloch88@gmail.com>
2023-08-05 09:48:34 -0400

    LongestSubarrayWithSum solution 1 comments

--- Arrays/LongestSubarrayWithSum.js
+++ Arrays/LongestSubarrayWithSum.js
@@ -15,21 +15,33 @@
 
 // Solution 1:
 
+// iterative but sub-optimal solution checking all possible subarrays
+
+// O(n ^ 2) time due to nested for loops
+// O(1) space due to only storing indices and a few variables
+
 function longestSubarrayWithSum(array, targetSum) {
+    // initialize variable indices and set equal to an empty array
+    // if solution is never found, this will fulfill requirement of returning an empty array
     let indices = [];
-
+    // iterate over the input array starting at index 0 with variable startingIndex
     for (let startingIndex = 0; startingIndex < array.length; startingIndex++) {
+        // initialize variable currentSubarraySum to track the total of subarray
         let currentSubarraySum = 0;
-
+        // iterate over the input array starting at the startingIndex with variable endingIndex
         for (let endingIndex = startingIndex; endingIndex < array.length; endingIndex++) {
-            currentSubarraySum += array[startingIndex];
-            
+            // add the current value at endingIndex in input array to the current value stored in currentSubarraySum
+            currentSubarraySum += array[endingIndex];
+            // check whether the value stored in currentSubarraySum is equal to the targetSum value
             if (currentSubarraySum === targetSum) {
+                // if there are currently no indices, or if the length of subarray from current startingIndex and current endingIndex is greater than the length currently stored in indices
+                // then update indices with the current values of startingIndex and endingIndex
                 if (indices.length === 0 || indices[1] - indices[0] < endingIndex - startingIndex) {
                     indices = [startingIndex, endingIndex];
                 }
             }
         }
     }
+    // return the values stored in indices variable, if any
     return indices;
 }
