2d848431ffab05a7702e3790079dee12fb91ca9b
Ryan Kirsch <agalloch88@gmail.com>
2023-08-07 08:16:13 -0400

    BlackjackProbability solution 1

--- Recursion/BlackjackProbability.js
+++ Recursion/BlackjackProbability.js
@@ -28,3 +28,30 @@
 // the overall probability of busting is 0.4 + (0.1 * 0.5), read as the probability of busting on the first draw plus the possibility of busting on the second draw
 
 // Solution 1:
+
+function blackjackProbability(target, startingHand) {
+    let memo = {};
+    return parseFloat(calculateProbability(startingHand, target, memo).toFixed(3));
+}
+
+function calculateProbability(currentHand, target, memo) {
+    if (currentHand in memo) {
+        return memo[currentHand];
+    }
+
+    if (currentHand > target) {
+        return 1;
+    }
+
+    if (currentHand + 4 >= target) {
+        return 0;
+    }
+
+    let totalProbability = 0;
+    for (let drawnCard = 1; drawnCard <= 10; drawnCard++) {
+        totalProbability += 0.1 * calculateProbability(currentHand + drawnCard, target, memo);
+    }
+
+    memo[currentHand] = totalProbability;
+    return totalProbability;
+}
