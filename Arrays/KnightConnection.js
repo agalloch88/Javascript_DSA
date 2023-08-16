ad0a281c5aa0e6ec43c294e56f2e1b693276e854
Ryan Kirsch <agalloch88@gmail.com>
2023-08-16 06:19:30 -0400

    KnightConnection solution 1 comments

--- Arrays/KnightConnection.js
+++ Arrays/KnightConnection.js
@@ -24,7 +24,14 @@
 
 // Solution 1:
 
+// iterative solution finding path from one knight to another and converting to number of turns
+
+// O(n * m) time, where n is horizontal difference between knights and m is the vertical distance between knights, as since board is infinite these are the two main bounds on potential moves
+// O(n * m) space, as queue will grow in potential moves depending on these distances
+
+// main function taking in the positions of knightA and knightB
 function knightConnection(knightA, knightB) {
+  // set up array of possible move coordinates, meaning from current position a knight can have these possible increments or decrements to coordinates
   let possibleMoves = [
     [-2, 1],
     [-1, 2],
@@ -35,36 +42,48 @@ function knightConnection(knightA, knightB) {
     [-1, -2],
     [-2, -1],
   ];
-
+  // initialize variable queue, which will be an array of values about the position of knightA as possibilities are checked
+  // each entry in the queue will have three values: horizontal position, vertical position, and running count of number of moves
   let queue = [[knightA[0], knightA[1], 0]];
+  // initialize variable visited, and set equal to a new Set object, which starts out with a string-converted position of knightA
   let visited = new Set(positionToString(knightA));
-
+  // keep looping until there's a return
   while (true) {
+    // initialize variable currentPosition, and set equal to the first value in the queue, which is grabbed via shift()
     let currentPosition = queue.shift();
-
+    // if the current position coordinates match the knightB coordinates, the two knights overlap and have "connection", so execute below
     if (
       currentPosition[0] === knightB[0] &&
       currentPosition[1] === knightB[1]
     ) {
+      // return a rounded-up value for the count of moves at index 2 in currentPosition divided by 2, which will be how many total turns the process took
       return Math.ceil(currentPosition[2] / 2);
     }
-
+    // look at all the possibleMove options in possibleMoves
     for (let possibleMove of possibleMoves) {
+      // initialize variable position and calculate new coordinates based on the current possibleMove
       let position = [
         currentPosition[0] + possibleMove[0],
         currentPosition[1] + possibleMove[1],
       ];
+      // initialize variable positionString and set equal to the return of call to helper, passing in the position above
       let positionString = positionToString(position);
-
+      // if the position string is NOT in the visited Set, then execute the below
+      // if already visited these coordinates, no need to do so again
       if (!visited.has(positionString)) {
+        // update the running move count on currentPosition, incrementing by 1
         position.push(currentPosition[2] + 1);
+        // push the current position into the queue for checking
         queue.push(position);
+        // add the current positionString to the visited Set
         visited.add(positionString);
       }
     }
   }
 }
 
+// helper function which takes in position coordinates of a knight
 function positionToString(position) {
+  // return the joined values from position input
   return position.join(",");
 }
