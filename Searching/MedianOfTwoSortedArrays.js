b6616cd7c506a160c63366a58ac084a7b19a8954
Ryan Kirsch <agalloch88@gmail.com>
2023-08-18 08:31:47 -0400

    MedianOfTwoSortedArrays start

---
+++ Searching/MedianOfTwoSortedArrays.js
@@ -0,0 +1,15 @@
+// The problem presents two sorted arrays of integers, called arrayOne and arrayTwo. Write a function which returns the median of these arrays.
+
+// For the purposes of the problem, assume both arrays have at least one value. However, they could be of different lengths. If the median is a decimal value, it should not be rounded,
+// beyond the inevitable rounding of floating point math.
+
+// Sample Input:
+
+// arrayOne = [1, 3, 4, 5]
+// arrayTwo = [2, 3, 6, 7]
+
+// Sample Output:
+
+// 3.5, where the combined array is [1, 2, 3, 3, {median here} 4, 5, 6, 7]
+
+// Solution 1:
