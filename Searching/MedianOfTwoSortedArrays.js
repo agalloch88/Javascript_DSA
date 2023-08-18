7e74a52686189f6017e3da186adc59ac927e14c3
Ryan Kirsch <agalloch88@gmail.com>
2023-08-18 14:28:36 -0400

    MedianOfTwoSortedArrays solution 1

--- Searching/MedianOfTwoSortedArrays.js
+++ Searching/MedianOfTwoSortedArrays.js
@@ -13,3 +13,36 @@
 // 3.5, where the combined array is [1, 2, 3, 3, {median here} 4, 5, 6, 7]
 
 // Solution 1:
+
+function medianOfTwoSortedArrays(arrayOne, arrayTwo) {
+    let idxOne = 0;
+    let idxTwo = 0;
+    let middleIdx = Math.floor((arrayOne.length + arrayTwo.length - 1) / 2);
+
+    while (idxOne + idxTwo < middleIdx) {
+        if (idxOne >= arrayOne.length) {
+            idxTwo++;
+        } else if (idxTwo >= arrayTwo.length) {
+            idxOne++;
+        } else if (arrayOne[idxOne] < arrayTwo[idxTwo]) {
+            idxOne++;
+        } else {
+            idxTwo++;
+        }
+    }
+
+    if ((arrayOne.length + arrayTwo.length) % 2 === 0) {
+        let areBothValuesArrayOne = idxTwo >= arrayTwo.length || (idxOne + 1 < arrayOne.length && arrayTwo[idxTwo] > arrayOne[idxOne + 1]);
+        let areBothValuesArrayTwo = idxOne >= arrayOne.length || (idxTwo + 1 < arrayTwo.length && arrayOne[idxOne] > arrayTwo[idxTwo + 1]);
+
+        let valueOne = areBothValuesArrayOne ? arrayOne[idxOne + 1] : arrayTwo[idxTwo];
+        let valueTwo = areBothValuesArrayTwo ? arrayTwo[idxTwo + 1] : arrayOne[idxOne];
+        
+        return (valueOne + valueTwo) / 2;
+    }
+
+    let valueOne = idxOne < arrayOne.length ? arrayOne[idxOne] : Infinity;
+    let valueTwo = idxTwo < arrayTwo.length ? arrayTwo[idxTwo] : Infinity;
+    
+    return Math.min (valueOne, valueTwo);
+}
