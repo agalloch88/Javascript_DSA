c51942545b58ae837374b202e030614c5ec01897
Ryan Kirsch <agalloch88@gmail.com>
2023-09-22 10:10:24 -0400

    NumbersInPi solution 1

--- DynamicProgramming/NumbersInPi.js
+++ DynamicProgramming/NumbersInPi.js
@@ -17,3 +17,38 @@
 // "314159265 | 35897932384626433832 | 79"
 
 // Solution 1:
+
+function numbersInPi(pi, numbers) {
+    let numbersTable = {};
+
+    for (let number of numbers) {
+        numbersTable[number] = true;
+    }
+
+    let minSpaces = getMinSpaces(pi, numbersTable, {}, 0);
+    return minSpaces === Infinity ? -1 : minSpaces;
+}
+
+function getMinSpaces(pi, numbersTable, cache, idx) {
+    if (idx === pi.length) {
+        return -1;
+    }
+
+    if (idx in cache) {
+        return cache[idx];
+    }
+
+    let minSpaces = Infinity;
+
+    for (let i = idx; i < pi.length; i++) {
+        let prefix = pi.slice(idx, i + 1);
+        
+        if (prefix in numbersTable) {
+            let minSpacesInSuffix = getMinSpaces(pi, numbersTable, cache, i + 1);
+            minSpaces = Math.min(minSpaces, minSpacesInSuffix + 1);
+        }
+    }
+
+    cache[idx] = minSpaces;
+    return cache[idx];
+}
