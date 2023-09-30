1a8fab1227356e4d2a559d856388b378e656bc7d
Ryan Kirsch <agalloch88@gmail.com>
2023-09-30 12:09:38 -0400

    InterweavingStrings solution 1

--- Recursion/InterweavingStrings.js
+++ Recursion/InterweavingStrings.js
@@ -17,3 +17,31 @@
 // true
 
 // Solution 1:
+
+function interweavingStrings(one, two, three) {
+    if (three.length !== one.length + two.length) {
+        return false;
+    }
+
+    return areInterwoven(one, two, three, 0, 0);
+}
+
+function areInterwoven(one, two, three, i, j) {
+    let k = i + j;
+
+    if (k === three.length) {
+        return true;
+    }
+
+    if (i < one.length && one[i] === three[k]) {
+        if (areInterwoven(one, two, three, i + 1, j)) {
+            return true;
+        }
+    }
+
+    if (j < two.length && two[j] === three[k]) {
+        return areInterwoven(one, two, three, i, j + 1);
+    }
+
+    return false;
+}
