953bacbd6b0b5d4a729db73faf1fbf80c8e4d692
Ryan Kirsch <agalloch88@gmail.com>
2023-10-08 10:54:44 -0400

    MergeLinkedLists solution 1 comments

--- LinkedLists/MergeLinkedLists.js
+++ LinkedLists/MergeLinkedLists.js
@@ -20,6 +20,12 @@
 
 // Solution 1:
 
+// iterative solution using three pointers to track positions within lists and check values
+
+// O(nm) time due to checking all values between the two input lists
+// O(1) space due to only storing three pointer values and doing constant time operations/checks
+
+// singly LinkedList class, where every node has a value and potentially a next pointer
 class LinkedList {
     constructor(value) {
         this.value = value;
@@ -27,29 +33,44 @@ class LinkedList {
     }
 }
 
+// main function, which takes in the heads of the two lists to merge
 function mergeLinkedLists(headOne, headTwo) {
+    // initialize variable p1, and set equal to the head value passed in on headOne
     let p1 = headOne;
+    // also need to track the previous value of headOne, so initialize variable p1Prev and set equal to null at outset, since starting at head
     let p1Prev = null;
+    // initialize variable p2, and set equal to the head value passed in on headTwo
     let p2 = headTwo;
 
+    // keep looping so long as still within head and tail bounds of both lists
     while (p1 !== null && p2 !== null) {
+        // if the value of p1 is LESS than the value of p2, execute below as need to simply move along in first list
         if (p1.value < p2.value) {
+            // move p1Prev pointer by setting it equal to value of p1
             p1Prev = p1;
+            // move p1 pointer by setting equal to the p1 next value
             p1 = p1.next;
+        // if the value of p1 is GREATER than the value of p2, then execute below
         } else {
+            // handle edge case where p1Prev may be null, in which case p1 is still at the headOne value, so move p1Prev pointer by setting p1Prev next value equal to p2
             if (p1Prev !== null) {
                 p1Prev.next = p2;
             }
 
+            // move p1Prev pointer by setting it equal to p2 value
             p1Prev = p2;
+            // move p2 pointer by setting it equal to p2's nexxt value
             p2 = p2.next;
+            // link the p1Prev pointers next value by setting equal to p1
             p1Prev.next = p1;
         }
     }
 
+    // handle edge case where p1 goes out of bounds of list, so set the p1Prev pointer's next value equal to p2 to continue the remainder of list
     if (p1 === null) {
         p1Prev.next = p2;
     }
 
+    // return a ternary value of check to see which list had the smaller head value, and thus should be head for merged list
     return headOne.value < headTwo.value ? headOne : headTwo;
 }
