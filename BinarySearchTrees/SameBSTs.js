a4af81d9d71f1e8a8f2057c818fbef0752236b70
Ryan Kirsch <agalloch88@gmail.com>
2023-10-28 12:12:43 +0100

    SameBsts solution 1

--- BinarySearchTrees/SameBSTs.js
+++ BinarySearchTrees/SameBSTs.js
@@ -25,3 +25,46 @@
 //    2    11   81
 
 // Solution 1:
+
+function sameBsts(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) {
+        return false;
+    }
+
+    if (arrayOne.length === 0 && arrayTwo.length === 0) {
+        return true;
+    }
+
+    if (arrayOne[0] !== arrayTwo[0]) {
+        return false;
+    }
+
+    let leftOne = getSmaller(arrayOne);
+    let leftTwo = getSmaller(arrayTwo);
+    let rightOne = getBiggerOrEqual(arrayOne);
+    let rightTwo = getBiggerOrEqual(arrayTwo);
+
+    return sameBsts(leftOne, leftTwo) && sameBsts(rightOne, rightTwo);
+}
+
+function getSmaller(array) {
+    let smaller = [];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < array[0]) {
+            smaller.push(array[i]);
+        }
+    }
+    return smaller;
+}
+
+function getBiggerOrEqual(array) {
+    let biggerOrEqual = [];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] >= array[0]) {
+            biggerOrEqual.push(array[i]);
+        }
+    }
+    return biggerOrEqual;
+}
