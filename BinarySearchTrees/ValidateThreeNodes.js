033547a4982bebfdb7c0b79512e2e5a676265d71
Ryan Kirsch <agalloch88@gmail.com>
2023-11-05 12:24:06 -0500

    ValidateThreeNodes solution 1

--- BinarySearchTrees/ValidateThreeNodes.js
+++ BinarySearchTrees/ValidateThreeNodes.js
@@ -31,3 +31,37 @@
 // nodeOne is an ancestor of nodeTwo, and nodeThree is a descendant of nodeTwo
 
 // Solution 1:
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function validateThreeNodes(nodeOne, nodeTwo, nodeThree) {
+  if (isDescendant(nodeTwo, nodeOne)) {
+    return isDescendant(nodeThree, nodeTwo);
+  }
+
+  if (isDescendant(nodeTwo, nodeThree)) {
+    return isDescendant(nodeOne, nodeTwo);
+  }
+
+  return false;
+}
+
+function isDescendant(node, target) {
+  if (node === null) {
+    return false;
+  }
+
+  if (node === target) {
+    return true;
+  }
+
+  return target.value < node.value
+    ? isDescendant(node.left, target)
+    : isDescendant(node.right, target);
+}
