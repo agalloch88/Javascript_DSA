98a5c0015b122a54fefafdc600708d65b7023abd
Ryan Kirsch <agalloch88@gmail.com>
2023-11-14 22:36:07 -0500

    ValidateThreeNodes solution 2

--- BinarySearchTrees/ValidateThreeNodes.js
+++ BinarySearchTrees/ValidateThreeNodes.js
@@ -83,3 +83,34 @@ function isDescendant(node, target) {
     ? isDescendant(node.left, target)
     : isDescendant(node.right, target);
 }
+
+// Solution 2:
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function validateThreeNodes(nodeOne, nodeTwo, nodeThree) {
+  if (isDescendant(nodeTwo, nodeOne)) {
+    return isDescendant(nodeThree, nodeTwo);
+  }
+
+  if (isDescendant(nodeTwo, nodeThree)) {
+    return isDescendant(nodeOne, nodeTwo);
+  }
+
+  return false;
+
+}
+
+function isDescendant(node, target) {
+  while (node !== null && node !== target) {
+    node = target.value < node.value ? node.left : node.right;
+  }
+
+  return node === target;
+}
