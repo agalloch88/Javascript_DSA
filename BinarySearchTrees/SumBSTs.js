00e9341901f8798f38d285f88a5a7f40ad772c84
Ryan Kirsch <agalloch88@gmail.com>
2023-11-20 18:20:24 -0500

    SumBSTs solution 1

--- BinarySearchTrees/SumBSTs.js
+++ BinarySearchTrees/SumBSTs.js
@@ -43,3 +43,68 @@
 // The subtrees rooted at 8 and 2 are both BSTs, however they only contain 2 nodes each
 
 // Solution 1:
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function sumBsts(tree) {
+  return getTreeInfo(tree).totalSumBstNodes;
+}
+
+function getTreeInfo(tree) {
+  if (tree === null) {
+    return {
+      isBst: true,
+      maxValue: -Infinity,
+      minValue: Infinity,
+      bstSum: 0,
+      bstSize: 0,
+      totalSumBstNodes: 0,
+    };
+  }
+
+  let leftTreeInfo = getTreeInfo(tree.left);
+  let rightTreeInfo = getTreeInfo(tree.right);
+
+  let satisfiesBstProp =
+    tree.value > leftTreeInfo.maxValue && tree.value <= rightTreeInfo.minValue;
+  let isBst = satisfiesBstProp && leftTreeInfo.isBst && rightTreeInfo.isBst;
+
+  let maxValue = Math.max(
+    tree.value,
+    Math.max(leftTreeInfo.maxValue, rightTreeInfo.maxValue),
+  );
+  let minValue = Math.min(
+    tree.value,
+    Math.min(leftTreeInfo.minValue, rightTreeInfo.minValue),
+  );
+
+  let bstSum = 0;
+  let bstSize = 0;
+
+  let totalSumBstNodes =
+    leftTreeInfo.totalSumBstNodes + rightTreeInfo.totalSumBstNodes;
+
+  if (isBst) {
+    bstSum = tree.value + leftTreeInfo.bstSum + rightTreeInfo.bstSum;
+    bstSize = 1 + leftTreeInfo.bstSize + rightTreeInfo.bstSize;
+
+    if (bstSize >= 3) {
+      totalSumBstNodes = bstSum;
+    }
+  }
+
+  return {
+    isBst,
+    maxValue,
+    minValue,
+    bstSum,
+    bstSize,
+    totalSumBstNodes,
+  };
+}
