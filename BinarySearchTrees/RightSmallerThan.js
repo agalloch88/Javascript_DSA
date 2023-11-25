52a5b983d35da25c8865e1b28f968ab0742fc345
Ryan Kirsch <agalloch88@gmail.com>
2023-11-25 18:09:58 -0500

    RightSmallerThan solution 2

--- BinarySearchTrees/RightSmallerThan.js
+++ BinarySearchTrees/RightSmallerThan.js
@@ -44,3 +44,21 @@ function rightSmallerThan(array) {
       }, 0), // Initial count is set to 0
   );
 }
+
+// Solution 2:
+
+function rightSmallerThan(array) {
+  let rightSmallerCounts = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let rightSmallerCount = 0;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[i]) {
+        rightSmallerCount++;
+      }
+    }
+    rightSmallerCounts.push(rightSmallerCount);
+  }
+  return rightSmallerCounts;
+}
