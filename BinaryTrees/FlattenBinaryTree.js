cd7d113cc1f4891e67c0ea40b24eea83beca9cb6
Ryan Kirsch <agalloch88@gmail.com>
2023-12-04 12:12:43 +0100

    FlattenBinaryTreeStart

---
+++ BinaryTrees/FlattenBinaryTree.js
@@ -0,0 +1,15 @@
+// Write a function which takes in a Binary Tree, flattens it, and returns the Binary Tree's
+// leftmost node.
+
+// A flattened Binary Tree is a structure which is nearly identical to a Doubly-Linked List
+// (except that nodes have left and right pointers, rather than prev and next pointers),
+// where nodes follow the original tree's left-to-right order.
+
+// Note that if the input Binary Tree happens to be a valid Binary Search Tree, the nodes in
+// the flattened tree will be sorted.
+
+// The flattening of the Binary Tree should be done in place, meaning that the original data
+// structure should be mutated, and no new data structure should be created.
+
+// Each BinaryTree node has an integer value, a left child node, and a right child node. Children
+// nodes can either be BinaryTree nodes themselves, or None/null.
