8ead25e91c4137df37941f7c8b197ac516062983
Ryan Kirsch <agalloch88@gmail.com>
2023-12-13 12:12:43 +0100

    RightSiblingTree solution 1

--- BinaryTrees/RightSiblingTree.js
+++ BinaryTrees/RightSiblingTree.js
@@ -37,3 +37,31 @@
 // 1 remains the root node, and the node with value 10 no longer has a node pointing to it
 
 // Solution 1:
+
+function rightSiblingTree(root) {
+    mutate(root, null, null);
+    return root;
+}
+
+function mutate(node, parent, isLeftChild) {
+    if (node === null) {
+        return;
+    }
+
+    let {left, right} = node;
+    mutate(left, node, true);
+
+    if (parent === null) {
+        node.right = null;
+    } else if (isLeftChild) {
+        node.right = parent.right;
+    } else {
+        if (parent.right === null) {
+            node.right = null;
+        } else {
+            node.right = parent.right.left;
+        }
+    }
+
+    mutate(right, node, false);
+}
