2888a90a1c6ac7a7d9b1c3ec84d694ab7340099e
Ryan Kirsch <agalloch88@gmail.com>
2023-12-26 08:23:20 -0500

    AllKindsOfNodeDepths solution 3

--- BinaryTrees/AllKindsOfNodeDepths.js
+++ BinaryTrees/AllKindsOfNodeDepths.js
@@ -137,3 +137,55 @@ function nodeDepths(node, depth = 0) {
     depth + nodeDepths(node.left, depth + 1) + nodeDepths(node.right, depth + 1)
   );
 }
+
+// Solution 3:
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function allKindsOfNodeDepths(root) {
+  addNodeCounts(root);
+  addNodeDepths(root);
+  return sumAllNodeDepths(root);
+}
+
+function sumAllNodeDepths(node) {
+  if (node === null) {
+    return 0;
+  }
+
+  return sumAllNodeDepths(node.left) + sumAllNodeDepths(node.right) + node._sumOfDepths;
+}
+
+function addNodeDepths(node) {
+  node._sumOfDepths = 0;
+
+  if (node.left !== null) {
+    addNodeDepths(node.left);
+    node._sumOfDepths += node.left._sumOfDepths + node.left._numNodesInTree;
+  }
+
+  if (node.right !== null) {
+    addNodeDepths(node.right);
+    node._sumOfDepths += node.right._sumOfDepths + node.right._numNodesInTree
+  }
+}
+
+function addNodeCounts(node) {
+  node._numNodesInTree = 1;
+
+  if (node.left !== null) {
+    addNodeCounts(node.left);
+    node._numNodesInTree += node.left._numNodesInTree;
+  }
+
+  if (node.right !== null) {
+    addNodeCounts(node.right);
+    node._numNodesInTree += node.right._numNodesInTree;
+  }
+}
