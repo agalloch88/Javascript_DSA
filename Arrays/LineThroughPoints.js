650e3bf5fe3b5890ab71f8cd22256dcf2b93533d
Ryan Kirsch <agalloch88@gmail.com>
2024-01-28 12:12:43 +0100

    LineThroughPoints solution 1

---
+++ Arrays/LineThroughPoints.js
@@ -0,0 +1,23 @@
+// The problem presents an array of points plotted on a 2D graph (meaning the x/y plane). Write a function which returns the maximum number of points which a single line (or, potentially, multiple lines)
+// on the graph passes through.
+
+// The input array/list will contain  points represented by an array/list of two integers [x, y]. The input array/list will never contatin duplicate points, and will always contain at least one plotted point.
+
+// Sample Input:
+
+// points = [
+//  [1, 1],
+//  [2, 2],
+//  [3, 3],
+//  [0, 4],
+//  [-2, 6],
+//  [4, 0],
+//  [2, 1],
+// ]
+
+// Sample Output:
+
+// 4
+// A plotted line will pass through the following points: [-2, 6], [0, 4], [2, 2], [4, 0]
+
+// Solution 1:
