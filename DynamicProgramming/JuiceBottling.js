45f7120339b4d902e2622f8ab10674cd7400dab2
Ryan Kirsch <agalloch88@gmail.com>
2024-02-27 12:12:43 +0100

    JuiceBottling start

---
+++ DynamicProgramming/JuiceBottling.js
@@ -0,0 +1,19 @@
+// The problem presents an array/list of integers, prices, of length n, and displays the retail prices of various quantities of juice. Each index in the array/list corresponds
+// to the price of that amount of juice. For example, prices[2] would be the retail price of 2 units of juice.
+
+// To start, the user has n - 1 total units of juice. For example, if the length of prices is 5, then this would mean the user has 4 total units of juice. Write a function
+// to determine the optimal way to bottle the juice such that it maximizes revenue. This function should return an array/list of all the juice quantities required in ascending order.
+
+// Note that the first value in the prices input will always be 0, because there is no value when having 0 juice. All other values will be positive integer values.
+// Additionally, a larger quantity of juice will not always be more expensive than a smaller quantity. For simplicity, any test cases for this problem would only have one possible solution.
+
+// Sample Input:
+
+// prices = [0, 1, 3, 2]
+
+// Sample Output:
+
+// [1, 2]
+// There are 3 total units of juice, because the length of prices is 4. To maximize revenue, we split the juice into quantities of 1 and 2, giving a revenue of 1 + 3 = 4
+
+// Solution 1:
