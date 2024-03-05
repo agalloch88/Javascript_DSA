e77edf5eae66b6e8684fa176767328a4842c9fba
Ryan Kirsch <agalloch88@gmail.com>
2024-03-05 08:42:29 -0500

    run format script

--- DynamicProgramming/JuiceBottling.js
+++ DynamicProgramming/JuiceBottling.js
@@ -19,19 +19,22 @@
 // Solution 1:
 
 function juiceBottling(prices) {
-    let numSizes = prices.length;
-    let maxProfit = new Array(numSizes).fill(0);
-    let solutions = new Array(numSizes).fill(undefined).map(_ => []);
+  let numSizes = prices.length;
+  let maxProfit = new Array(numSizes).fill(0);
+  let solutions = new Array(numSizes).fill(undefined).map((_) => []);
 
-    for (let size = 0; size < numSizes; size++) {
-        for (let dividingPoint = 0; dividingPoint < size + 1; dividingPoint++) {
-            let possibleProfit = maxProfit[size - dividingPoint] + prices[dividingPoint];
+  for (let size = 0; size < numSizes; size++) {
+    for (let dividingPoint = 0; dividingPoint < size + 1; dividingPoint++) {
+      let possibleProfit =
+        maxProfit[size - dividingPoint] + prices[dividingPoint];
 
-            if (possibleProfit > maxProfit[size]) {
-                maxProfit[size] = possibleProfit;
-                solutions[size] = [dividingPoint].concat(solutions[size - dividingPoint]);
-            }
-        }
+      if (possibleProfit > maxProfit[size]) {
+        maxProfit[size] = possibleProfit;
+        solutions[size] = [dividingPoint].concat(
+          solutions[size - dividingPoint],
+        );
+      }
     }
-    return solutions[numSizes - 1];
+  }
+  return solutions[numSizes - 1];
 }
