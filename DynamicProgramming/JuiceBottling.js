13e7448bc9add38ae8b7faeb7e969233eceb8833
Ryan Kirsch <agalloch88@gmail.com>
2024-03-08 18:12:25 -0500

    JuiceBottling solution 1 comments

--- DynamicProgramming/JuiceBottling.js
+++ DynamicProgramming/JuiceBottling.js
@@ -18,23 +18,46 @@
 
 // Solution 1:
 
+// suboptimal solution looking at all combinations to determine optimal bottling
+
+// O(n^3) time due to nested for loops and additional n concat operation inside
+// O(n^2) space due to two additional arrays of n length
+ 
 function juiceBottling(prices) {
+  // initialize variable numSizes to set how many possible
+  // sizes there may be, and set equal to the length of the prices input
   let numSizes = prices.length;
+  // initialize variable maxProfit and set equal to a new array of numSizes length, filled with 0's
   let maxProfit = new Array(numSizes).fill(0);
+  // initialize variable solutions, and set equal to a new array of numSizes length,
+  // filled with undefined values, and mapping every _ value to an empty array which will
+  // hold the found solutions
   let solutions = new Array(numSizes).fill(undefined).map((_) => []);
 
+  // iterate over every size in the range of numSizes, starting at 0 bottles of juice
   for (let size = 0; size < numSizes; size++) {
+    // iterate over every dividing point so long as within the range of the current size
     for (let dividingPoint = 0; dividingPoint < size + 1; dividingPoint++) {
+      // initialize variable possibleProfit, and set equal to the value at the index of current size minus
+      // the current dividing point in the maxProfit array PLUS the value at the dividingPoint index
+      // in the prices input
       let possibleProfit =
         maxProfit[size - dividingPoint] + prices[dividingPoint];
 
+        // if the value at the current possibleProfit amount is GREATER than the value at the index of
+        // current size in maxProfit, then execute below
       if (possibleProfit > maxProfit[size]) {
+        // set the value at current size in maxProfit equal to the value of possibleProfit
         maxProfit[size] = possibleProfit;
+        // set the value at size in the solutions array equal to the value at the current 
+        // dividingPoint, concatenating the value at current size minus the dividingPoint in solutions array
         solutions[size] = [dividingPoint].concat(
           solutions[size - dividingPoint],
         );
       }
     }
   }
+  // once all possibilities are looked at, return the value at  numSizes minus 1 index in solutions array
+  // as this will be the most profitable option
   return solutions[numSizes - 1];
 }
