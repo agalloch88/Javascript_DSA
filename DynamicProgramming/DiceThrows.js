66e6d15948c36ded235b6028a294888179baf96d
Ryan Kirsch <agalloch88@gmail.com>
2024-03-23 21:03:36 -0400

    DiceThrows solution 1

--- DynamicProgramming/DiceThrows.js
+++ DynamicProgramming/DiceThrows.js
@@ -21,3 +21,27 @@
 // [1, 6], [2, 5], [4, 3], [3, 4], [5, 2], [6, 1]
 
 // Solution 1:
+
+function diceThrows(numDice, numSides, target) {
+    let storedResults = new Array(numDice + 1).fill(undefined).map(_ => new Array(target + 1).fill(-1));
+    return diceThrowsHelper(numDice, numSides, target, storedResults);
+}
+
+function diceThrowsHelper(numDice, numSides, target, storedResults) {
+    if (numDice === 0) {
+        return target === 0 ? 1 : 0;
+    }
+
+    if (storedResults[numDice][target] > -1) {
+        return storedResults[numDice][target];
+    }
+
+    let numWaysToReachTarget = 0;
+
+    for (let currentTarget = Math.max(0, target - numSides); currentTarget < target; currentTarget++) {
+        numWaysToReachTarget += diceThrowsHelper(numDice - 1, numSides, currentTarget, storedResults);
+    }
+
+    storedResults[numDice][target] = numWaysToReachTarget;
+    return numWaysToReachTarget;
+}
