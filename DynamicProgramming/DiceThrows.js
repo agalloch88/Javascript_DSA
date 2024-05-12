09781393d45351863d797f4efdf71a683a394227
Ryan Kirsch <agalloch88@gmail.com>
2024-05-12 12:12:43 +0100

    DiceThrows solution 3

--- DynamicProgramming/DiceThrows.js
+++ DynamicProgramming/DiceThrows.js
@@ -122,3 +122,28 @@ function diceThrows(numDice, numSides, target) {
   // Return the number of ways to achieve the target sum with the given number of dice
   return storedResults[numDice][target];
 }
+
+// Solution 3
+
+function diceThrows(numDice, numSides, target) {
+  let storedResults = [new Array(target + 1).fill(0), new Array(target + 1).fill(0)];
+  storedResults[0][0] = 1;
+
+  let previousNumDiceIndex = 0;
+  let newNumDiceIndex = 1;
+
+  for (let i = 0; i < numDice; i++) {
+    for (let currentTarget = 0; currentTarget < target + 1; currentTarget++) {
+      let numWaysToReachTarget = 0;
+
+      for (let currentNumSides = 1; currentNumSides < Math.min(currentTarget, numSides) + 1; currentNumSides++) {
+        numWaysToReachTarget += storedResults[previousNumDiceIndex][currentTarget - currentNumSides];
+      }
+      storedResults[newNumDiceIndex][currentTarget] = numWaysToReachTarget;
+    }
+    let temp = previousNumDiceIndex;
+    previousNumDiceIndex = newNumDiceIndex;
+    newNumDiceIndex = temp;
+  }
+  return storedResults[previousNumDiceIndex][target];
+}
