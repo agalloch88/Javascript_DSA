8e57e4069cbe550969116d2737d165420c549401
Ryan Kirsch <agalloch88@gmail.com>
2024-06-22 08:46:35 -0400

    run format script

--- LinkedLists/LinkedListPalindrome.js
+++ LinkedLists/LinkedListPalindrome.js
@@ -4,7 +4,7 @@
 
 // A palindrome is usually defined as a string which is written the same forward and backward.
 // For a Linked List's nodes to form a palindrome, their values must be the same when read from
-// left to right, and from right to left. Note that single-character strings are palindromes, 
+// left to right, and from right to left. Note that single-character strings are palindromes,
 // which means that single-node Linked Lists form palindromes.
 
 // Each LinkedList node has an integer `value` as well as a `next` node pointing to the next node
@@ -24,33 +24,34 @@
 // Solution 1:
 
 class LinkedList {
-    constructor(value) {
-        this.value = value;
-        this.next= null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedListInfo {
-    constructor(outerNodesAreEqual, leftNodeToCompare) {
-        this.outerNodesAreEqual = outerNodesAreEqual;
-        this.leftNodeToCompare = leftNodeToCompare;
-    }
+  constructor(outerNodesAreEqual, leftNodeToCompare) {
+    this.outerNodesAreEqual = outerNodesAreEqual;
+    this.leftNodeToCompare = leftNodeToCompare;
+  }
 }
 
 function linkedListPalindrome(head) {
-    let isPalindromeResults = isPalindrome(head, head);
-    return isPalindromeResults.outerNodesAreEqual;
+  let isPalindromeResults = isPalindrome(head, head);
+  return isPalindromeResults.outerNodesAreEqual;
 }
 
 function isPalindrome(leftNode, rightNode) {
-    if (rightNode === null) {
-        return new LinkedListInfo(true, leftNode);
-    }
+  if (rightNode === null) {
+    return new LinkedListInfo(true, leftNode);
+  }
 
-    let recursiveCallResults = isPalindrome(leftNode, rightNode.next);
-    let {leftNodeToCompare, outerNodesAreEqual} = recursiveCallResults;
-    let recursiveIsEqual = outerNodesAreEqual && leftNodeToCompare.value === rightNode.value;
-    let nextLeftNodeToCompare = leftNodeToCompare.next;
-    
-    return new LinkedListInfo(recursiveIsEqual, nextLeftNodeToCompare);
+  let recursiveCallResults = isPalindrome(leftNode, rightNode.next);
+  let { leftNodeToCompare, outerNodesAreEqual } = recursiveCallResults;
+  let recursiveIsEqual =
+    outerNodesAreEqual && leftNodeToCompare.value === rightNode.value;
+  let nextLeftNodeToCompare = leftNodeToCompare.next;
+
+  return new LinkedListInfo(recursiveIsEqual, nextLeftNodeToCompare);
 }
