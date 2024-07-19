1a350c7a08796cb8697e33ff19616a27fb303d47
Ryan Kirsch <agalloch88@gmail.com>
2024-07-19 12:12:43 +0100

    NodeSwap start

---
+++ LinkedLists/NodeSwap.js
@@ -0,0 +1,18 @@
+// Write a function which takes in the head of a Singly Linked List, swaps every pair of adjacent nodes in place (meaning, the function does not create a new Linked List)
+// and which returns the new head of the Singly Linked List.
+
+// If the input Linked List has an odd number of nodes, the final node should remain the same.
+
+// Each `LinkedList` has an integer `value`, as well as a `next` node pointing to the next node in the list, or to None/null if it is the tail of the list.
+
+// For the purposes of the problem, assume the input Linked List will always have at least one node. In other words, the head will never be None/null.
+
+// Sample Input:
+
+// head = 0 -> 1 -> 2 -> 3 -> 4 -> 5
+
+// Sample Output:
+
+// 1 -> 0 -> 3 -> 2 -> 5 -> 4
+
+// Solution 1:
