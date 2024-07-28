16ee1aa35cb430b0708a7319907badfa65168337
Ryan Kirsch <agalloch88@gmail.com>
2024-07-28 12:22:43 +0100

    SolveSudoku solution 1

--- Recursion/SolveSudoku.js
+++ Recursion/SolveSudoku.js
@@ -5,15 +5,15 @@
 
 // - - 3 | - 2 - | 6 - -
 // 9 - - | 3 - 5 | - - 1
-// - - 1 | 8 - 6 | 4 - - 
+// - - 1 | 8 - 6 | 4 - -
 // - - - - - - - - - - -
-// - - 8 | 1 - 2 | 9 - - 
+// - - 8 | 1 - 2 | 9 - -
 // 7 - - | - - - | - - 8
-// - - 6 | 7 - 8 | 2 - - 
+// - - 6 | 7 - 8 | 2 - -
 // - - - - - - - - - - -
-// - - 2 | 6 - 9 | 5 - - 
+// - - 2 | 6 - 9 | 5 - -
 // 8 - - | 2 - 3 | - - 9
-// - - 5 | - 1 - | 3 - - 
+// - - 5 | - 1 - | 3 - -
 
 // The objective is to fill the grid such that each row, column, and 3x3 subgrid contains the numbers 1 to 9 exactly once. In other words, no row may contain the same
 // digit more than once, no column may contain the same digit more than once, and none of the 9 individual 3x3 subgrids may contain the same digit more than once.
@@ -52,3 +52,67 @@
 // ]
 
 // Solution 1:
+
+function solveSudoku(board) {
+    solvePartialSudoku(0, 0, board);
+    return board;
+}
+
+function solvePartialSudoku(row, col, board) {
+    let currentRow = row;
+    let currentCol = col;
+
+    if (currentCol === board[currentRow].length) {
+        currentRow++;
+        currentCol = 0;
+
+        if (currentRow === board.length) {
+            return true;
+        }
+    }
+
+    if (board[currentRow][currentCol] === 0) {
+        return tryDigitsAtPosition(currentRow, currentCol, board);
+    }
+    return solvePartialSudoku(currentRow, currentCol + 1, board);
+}
+
+function tryDigitsAtPosition(row, col, board) {
+    for (let digit = 1; digit < 10; digit++) {
+        if (isValidAtPosition(digit, row, col, board)) {
+            board[row][col] = digit;
+
+            if (solvePartialSudoku(row, col + 1, board)) {
+                return true;
+            }
+        }
+    }
+
+    board[row][col] = 0;
+    return false;
+}
+
+function isValidAtPosition(value, row, col, board) {
+    let rowIsValid = !board[row].includes(value);
+    let colIsValid = !board.map(row => row[col]).includes(value);
+
+    if (!rowIsValid || !colIsValid) {
+        return false;
+    }
+
+    let subgridRowStart = Math.floor(row / 3) * 3;
+    let subgridColStart = Math.floor(col / 3) * 3;
+
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
+        for (let colIdx = 0; colIdx < 3; colIdx++) {
+            let rowToCheck = subgridRowStart + rowIdx;
+            let coldToCheck = subgridColStart + colIdx;
+            let existingValue = board[rowToCheck][coldToCheck];
+
+            if (existingValue === value) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
