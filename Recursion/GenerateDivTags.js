1ba3272468989c85ce3c8391c8afefb7c2bf324b
Ryan Kirsch <agalloch88@gmail.com>
2024-08-10 12:22:43 +0100

    GenerateDivTags solution 1 comments

--- Recursion/GenerateDivTags.js
+++ Recursion/GenerateDivTags.js
@@ -23,18 +23,31 @@
 
 // Solution 1:
 
+// recursive solution checking the number of opening and closing tags needed, using a passed-in prefix, to solve
+
+// O((2n)!/((n!((n + 1)!)))) time due to exponential possibilities based on increasing size
+// O((2n)!/((n!((n + 1)!)))) space due to exponential growth of result array
+
 function generateDivTags(numberOfTags) {
+  // Initialize an empty array to store the generated valid sequences of div tags.
   let matchedDivTags = [];
+
+  // Start the recursive process with the given number of opening and closing tags needed,
+  // an empty prefix, and the result array to store the sequences.
   generateDivTagsFromPrefix(numberOfTags, numberOfTags, '', matchedDivTags);
+
+  // Return the array containing all valid sequences of div tags.
   return matchedDivTags;
 }
 
+// helper function to generate tags based on a passed-in prefix
 function generateDivTagsFromPrefix(
   openingTagsNeeded,
   closingTagsNeeded,
   prefix,
   result,
 ) {
+  // If there are still opening tags needed, add an opening tag to the prefix and recurse.
   if (openingTagsNeeded > 0) {
     let newPrefix = prefix + '<div>';
     generateDivTagsFromPrefix(
@@ -45,6 +58,8 @@ function generateDivTagsFromPrefix(
     );
   }
 
+  // If the number of opening tags needed is less than the number of closing tags needed,
+  // it means we can add a closing tag to the prefix and recurse.
   if (openingTagsNeeded < closingTagsNeeded) {
     let newPrefix = prefix + '</div>';
     generateDivTagsFromPrefix(
@@ -55,6 +70,8 @@ function generateDivTagsFromPrefix(
     );
   }
 
+  // When no more closing tags are needed, we have a complete valid sequence.
+  // Add the current prefix to the result array.
   if (closingTagsNeeded === 0) {
     result.push(prefix);
   }
