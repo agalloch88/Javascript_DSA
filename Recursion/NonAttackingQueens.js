e2f6f3f02eb19aa15dd6db823ce47160aac87f8a
Ryan Kirsch <agalloch88@gmail.com>
2024-09-21 15:24:35 -0400

    NonAttackingQueens solution 2

--- Recursion/NonAttackingQueens.js
+++ Recursion/NonAttackingQueens.js
@@ -88,3 +88,57 @@ function isNonAttackingPlacement(row, col, columnPlacements) {
   // If no queen attacks the new one, return true.
   return true;
 }
+
+// Solution 2:
+
+function nonAttackingQueens(n) {
+  let blockedColumns = new Set();
+  let blockedUpDiagonals = new Set();
+  let blockedDownDiagonals = new Set();
+  return getNumberOfNonAttackingQueenPlacements(0, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, n);
+}
+
+function getNumberOfNonAttackingQueenPlacements(row, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, boardSize) {
+  if (row === boardSize) {
+    return 1;
+  }
+
+  let validPlacements = 0;
+  for (let col = 0; col < boardSize; col++) {
+    if (isNonAttackingPlacement(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals)) {
+      placeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals);
+      validPlacements += getNumberOfNonAttackingQueenPlacements(row + 1, blockedColumns, blockedUpDiagonals, blockedDownDiagonals, boardSize);
+      removeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals);
+    }
+  }
+
+  return validPlacements;
+}
+
+function isNonAttackingPlacement(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals) {
+  if (blockedColumns.has(col)) {
+    return false;
+  }
+
+  if (blockedUpDiagonals.has(row + col)) {
+    return false;
+  }
+
+  if (blockedDownDiagonals.has(row - col)) {
+    return false;
+  }
+
+  return true;
+}
+
+function placeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals) {
+  blockedColumns.add(col);
+  blockedUpDiagonals.add(row + col);
+  blockedDownDiagonals.add(row - col);
+}
+
+function removeQueen(row, col, blockedColumns, blockedUpDiagonals, blockedDownDiagonals) {
+  blockedColumns.delete(col);
+  blockedUpDiagonals.delete(row + col);
+  blockedDownDiagonals.delete(row - col);
+}
