2c39a18c4a081352ba18d7616324b6daab542a5a
Ryan Kirsch <agalloch88@gmail.com>
2024-09-27 12:24:12 +0100

    NumberOfBinaryTreeTopologies start

---
+++ Recursion/NumberOfBinaryTreeToplogies.js
@@ -0,0 +1,18 @@
+// Write a function which takes in a non-negative integer, `n`, and returns the number of possible
+// Binary Tree topologies which may be created with exactly `n` nodes.
+
+// A Binary Tree topoloigy is defined as any Binary Tree configuration, irrespective of 
+// node values. For instance, there exist only two Binary Tree topologies where `n` is equal
+// to 2: a root node with a left node, and a root node with a right node.
+
+// Note that when `n` is equal to 0, there is one topoloigy which may be created: the None/Null node.
+
+// Sample Input:
+
+// n = 3
+
+// Sample Output:
+
+// 5
+
+// Solution 1:
