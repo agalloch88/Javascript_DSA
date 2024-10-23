526d0f1045bbc365ab586f6d417e8484fec40422
Ryan Kirsch <agalloch88@gmail.com>
2024-10-23 12:24:12 +0100

    MaxProfitWithKTransactions solution 1

--- DynamicProgramming/MaxProfitWithKTransactions.js
+++ DynamicProgramming/MaxProfitWithKTransactions.js
@@ -22,3 +22,25 @@
 // buy at 5, sell at 11, buy at 3, sell at 90.
 
 // Solution 1:
+
+function maxProfitWithKTransactions(prices, k) {
+    if (!prices.length) {
+        return 0;
+    }
+
+    let profits = [];
+
+    for (let t = 0; t < k +1; t++) {
+        let row = new Array(prices.length).fill(0);
+        profits.push(row);
+    }
+
+    for (let t = 1; t < k + 1; t++) {
+        let maxThusFar = -Infinity;
+        for (let d = 1; d < prices.length; d++) {
+            maxThusFar = Math.max(maxThusFar, profits[t - 1][d - 1] - prices[d - 1]);
+            profits[t][d] = Math.max(profits[t][d - 1], maxThusFar + prices[d])
+        }
+    }
+    return profits[k][prices.length - 1];
+}
