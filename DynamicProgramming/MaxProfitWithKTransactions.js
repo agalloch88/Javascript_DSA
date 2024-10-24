e7ffd3f4d1067033e2ee6a3e805fc1e8164408ed
Ryan Kirsch <agalloch88@gmail.com>
2024-10-24 12:24:12 +0100

    run format script

--- DynamicProgramming/MaxProfitWithKTransactions.js
+++ DynamicProgramming/MaxProfitWithKTransactions.js
@@ -24,23 +24,23 @@
 // Solution 1:
 
 function maxProfitWithKTransactions(prices, k) {
-    if (!prices.length) {
-        return 0;
-    }
+  if (!prices.length) {
+    return 0;
+  }
 
-    let profits = [];
+  let profits = [];
 
-    for (let t = 0; t < k +1; t++) {
-        let row = new Array(prices.length).fill(0);
-        profits.push(row);
-    }
+  for (let t = 0; t < k + 1; t++) {
+    let row = new Array(prices.length).fill(0);
+    profits.push(row);
+  }
 
-    for (let t = 1; t < k + 1; t++) {
-        let maxThusFar = -Infinity;
-        for (let d = 1; d < prices.length; d++) {
-            maxThusFar = Math.max(maxThusFar, profits[t - 1][d - 1] - prices[d - 1]);
-            profits[t][d] = Math.max(profits[t][d - 1], maxThusFar + prices[d])
-        }
+  for (let t = 1; t < k + 1; t++) {
+    let maxThusFar = -Infinity;
+    for (let d = 1; d < prices.length; d++) {
+      maxThusFar = Math.max(maxThusFar, profits[t - 1][d - 1] - prices[d - 1]);
+      profits[t][d] = Math.max(profits[t][d - 1], maxThusFar + prices[d]);
     }
-    return profits[k][prices.length - 1];
+  }
+  return profits[k][prices.length - 1];
 }
