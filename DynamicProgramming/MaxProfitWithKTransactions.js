14e5c044d5dea2df7f07d5bd0bf8a91f6e491627
Ryan Kirsch <agalloch88@gmail.com>
2024-10-26 08:25:16 -0500

    MaxProfitWithKTransactions solution 2

--- DynamicProgramming/MaxProfitWithKTransactions.js
+++ DynamicProgramming/MaxProfitWithKTransactions.js
@@ -67,3 +67,35 @@ function maxProfitWithKTransactions(prices, k) {
   // The maximum profit achievable with at most `k` transactions on the last day.
   return profits[k][prices.length - 1];
 }
+
+// Solution 2:
+
+function maxProfitWithKTransactions(prices, k) {
+    if (!prices.length) {
+        return 0;
+    }
+
+    let evenProfits = new Array(prices.length).fill(0);
+    let oddProfits = new Array(prices.length).fill(0);
+
+    for (let t = 1; t < k + 1; t++) {
+        let maxThusFar = -Infinity;
+        let currentProfits, previousProfits;
+
+        if (t % 2 === 1) {
+            currentProfits = oddProfits;
+            previousProfits = evenProfits;
+        } else {
+            currentProfits = evenProfits;
+            previousProfits = oddProfits;
+        }
+
+        for (let d = 1; d < prices.length; d++) {
+            maxThusFar = Math.max(maxThusFar, previousProfits[d - 1] - prices[d - 1]);
+            currentProfits[d] = Math.max(currentProfits[d - 1], maxThusFar + prices[d]);
+            
+        }
+    }
+
+    return k % 2 === 0 ? evenProfits[prices.length - 1] : oddProfits[prices.length - 1];
+}
