ea44512dc877a2d1486f950d1b87794162478800
Ryan Kirsch <agalloch88@gmail.com>
2024-10-29 12:24:12 +0100

    PalindromePartitioningMinCuts start

---
+++ DynamicProgramming/PalindromePartitioningMinCuts.js
@@ -0,0 +1,16 @@
+// Given a non-empty string, write a function which returns the minimum number of cuts needed to
+// perform on a string such that each remaining substring is a palindrome.
+
+// A palindrome is defined as a string which is written the same forward as backward. Note that
+// for the purposes of the problem, single-character strings are palindromes.
+
+// Sample Input:
+
+// string = "noonabbad"
+
+// Sample Output:
+
+// 2
+// noon | abba | d
+
+// Solution 1:
