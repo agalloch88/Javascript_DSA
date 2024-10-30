3d6c2d5ad54ef44db4a71731669a8bead4f96a7d
Ryan Kirsch <agalloch88@gmail.com>
2024-10-30 06:10:36 -0500

    PalindromePartitioningMinCuts solution 1

--- DynamicProgramming/PalindromePartitioningMinCuts.js
+++ DynamicProgramming/PalindromePartitioningMinCuts.js
@@ -14,3 +14,44 @@
 // noon | abba | d
 
 // Solution 1:
+
+function plaindromPartitioningMinCuts(string) {
+    let palindromes = new Array(string.length).fill(1).map(row => []);
+
+    for (let i = 0; i < string.length; i++) {
+        for (let j = 0; j < string.length; j++) {
+            palindromes[i][j] = isPalindrome(string.slice(i, j + 1));
+        }
+    }
+    let cuts = new Array(string.length);
+    cuts.fill(Infinity);
+
+    for (let i = 0; i < string.length; i++) {
+        if (palindromes[0][i]) {
+            cuts[i] = 0;
+        } else {
+            cuts[i] = cuts[i - 1] + 1;
+            
+            for (let j = 1; j < i; j++) {
+                if (palindromes[j][i] && cuts[j - 1] + 1 < cuts[i]) {
+                    cuts[i] = cuts[j - 1] + 1;
+                }               
+            }
+        }
+    }
+    return cuts[cuts.length - 1];
+}
+
+function isPalindrome(string) {
+    let leftIdx = 0;
+    let rightIdx = string.length - 1;
+
+    while (leftIdx < rightIdx) {
+        if (string[leftIdx] !== string[rightIdx]) {
+            return false;
+        }
+        leftIdx++;
+        rightIdx--;
+    }
+    return true;
+}
