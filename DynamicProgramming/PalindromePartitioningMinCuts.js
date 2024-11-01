d95662cacb267d6c13ab797e126239f43c43ff92
Ryan Kirsch <agalloch88@gmail.com>
2024-11-01 05:39:07 -0500

    PalindromePartitioningMinCuts solution 2

--- DynamicProgramming/PalindromePartitioningMinCuts.js
+++ DynamicProgramming/PalindromePartitioningMinCuts.js
@@ -73,3 +73,54 @@ function isPalindrome(string) {
   }
   return true;
 }
+
+// Solution 2:
+
+function palindromePartitioningMinCuts(string) {
+    let palindromes = [];
+
+    for (let i = 0; i < string.length; i++) {
+        let row = [];
+
+        for (let j = 0; j < string.length; j++) {
+            if (i === j) {
+                row.push(true);
+            } else {
+                row.push(false);
+            }
+        }
+        palindromes.push(row);
+    }
+
+    for (let length = 2; length < string.length; length++) {
+        for (let i = 0; i < string.length - length + 1; i++) {
+            let j = i + length - 1;
+
+            if (length === 2) {
+                palindromes[i][j] = string[i] === string[j];
+            } else {
+                palindromes[i][j] = string[i] === string[j] && palindromes[i + 1][j - 1];
+            }
+        }
+    }
+
+    let cuts = new Array(string.length).fill(Infinity);
+
+    for (let i = 0; i < string.length; i++) {
+        if (palindromes[0][i]) {
+            cuts[i] = 0;
+        } else {
+            cuts[i] = cuts[i - 1] + 1;
+
+            for (let j = 1; j < i; j++) {
+                if (palinderomes[j][i] && cuts[j - 1] + 1 < cuts[i]) {
+                    cuts[i] = cuts[j - 1] + 1 
+                }
+                
+            }
+        }
+        
+    }
+
+    return cuts.[cuts.length - 1];
+}
