b7a0a46ca6413263a1473266a0160e667973060a
Ryan Kirsch <agalloch88@gmail.com>
2024-11-05 12:24:12 +0100

    LongestIncreasinSubsequence start

---
+++ DynamicProgramming/LongestIncreasingSubsequence.js
@@ -0,0 +1,19 @@
+// Given a non-empty array of integers, write a function which returns the longest strictly-increasing
+// subsequence in the array.
+
+// A subsequence of an array is a set of numbers which are not necessarily adjacent in the array,
+// but are in the same order as they appear in the array. For example, the numbers [1, 3, 4] form
+// a subsequence of the array [1, 2, 3, 4], and so do the numbers [2, 4]. Note that a single
+// numbwer in an array, and the array itself, are both valid subsequences of the array.
+
+// For the purposes of the problem, assume there will only be one longest increasin subsequence.
+
+// Sample Input:
+
+// array = [5, 7, -24, 12, 10, 2, 3, 12, 5, 6, 35]
+
+// Sample Output:
+
+// [-24, 2, 3, 5, 6, 35]
+
+// Solution 1:
