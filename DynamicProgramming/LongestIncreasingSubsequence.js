fe82b90550780fb1e55962656b158db89b1fc708
Ryan Kirsch <agalloch88@gmail.com>
2024-11-06 12:24:12 +0100

    LongestIncreasinSubsequence solution 1

--- DynamicProgramming/LongestIncreasingSubsequence.js
+++ DynamicProgramming/LongestIncreasingSubsequence.js
@@ -17,3 +17,36 @@
 // [-24, 2, 3, 5, 6, 35]
 
 // Solution 1:
+
+function longestIncreasingSubsequence(array) {
+    let sequences = new Array(array.length);
+    let lengths = array.map(num => 1);
+    let maxLengthIdx = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        let currentNum = array[i];
+
+        for (let j = 0; j < i; j++) {
+            let otherNum = array[j];
+            
+            if (otherNum < currentNum && lengths[j] + 1 >= lengths[i]) {
+                lengths[i] = lengths[j] + 1;
+                sequences[i] = j;
+            }
+        }
+        if (lengths[i] >= lengths[maxLengthIdx]) {
+            maxLengthIdx = i;
+        }
+    }
+    return buildSequence(array, sequences, maxLengthIdx);
+}
+
+function buildSequence(array, sequences, currentIdx) {
+    let sequence = [];
+
+    while (currentIdx != undefined) {
+        sequence.unshift(array[currentIdx]);
+        currentIdx = sequences[currentIdx];
+    }
+    return sequence;
+}
