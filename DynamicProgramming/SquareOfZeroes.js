98179ff1eb41542e3b3077be1055b5761819b0ac
Ryan Kirsch <agalloch88@gmail.com>
2024-11-26 12:24:12 +0100

    SquareOfZeroes solution 2

--- DynamicProgramming/SquareOfZeroes.js
+++ DynamicProgramming/SquareOfZeroes.js
@@ -117,3 +117,19 @@ function squareOfZeroes(matrix) {
   }
   return false;
 }
+
+function isSquareOfZeroes(matrix, r1, c1, r2, c2) {
+    for (let row = r1; row < r2 + 1; row++) {
+        if (matrix[row][c1] !== 0 || matrix[row][c2] !== 0) {
+            return false;
+        }
+    }
+
+    for (let col = 0; col < c2 + 1; col++) {
+        if (matrix[r1][col] !== 0 || matrix[r2][col] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
