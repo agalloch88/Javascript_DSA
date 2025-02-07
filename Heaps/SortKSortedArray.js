524d3c5b8039b10f6b87b3d2ebf07f4f1acfe7e0
Ryan Kirsch <agalloch88@gmail.com>
2025-02-07 12:24:12 +0100

    SortKSortedArray solution 1 MinHeap class in progress

--- Heaps/SortKSortedArray.js
+++ Heaps/SortKSortedArray.js
@@ -19,3 +19,48 @@
 // [1, 2, 3, 4, 5, 5, 6, 7]
 
 // Solution 1:
+
+class MinHeap {
+    constructor(array) {
+        this.heap = this.buildHeap(array);
+    }
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+
+    buildHeap(array) {
+        let firstParentIdx = Math.floor((array.length - 2) / 2);
+        for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+            this.siftDown(currentIdx, array.length - 1, array);
+        }
+        return array;
+    }
+
+    siftDown(currentIdx, endIdx, heap) {
+        let childOneIdx = currentIdx * 2 + 1;
+        while (childOneIdx <= endIdx) {
+            let childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+            let idxToSwap;
+
+            if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+                idxToSwap = childTwoIdx;
+            } else {
+                idxToSwap = childOneIdx;
+            }
+
+            if (heap[idxToSwap] < heap[currentIdx]) {
+                this.swap(currentIdx, idxToSwap, heap);
+                currentIdx = idxToSwap;
+                childOneIdx = currentIdx * 2 + 1;
+            } else {
+                return;
+            }
+        }
+    }
+
+    swap(i, j, heap) {
+        let temp = heap[j];
+        heap[j] = heap[i];
+        heap[i] = temp;
+    }
+}
