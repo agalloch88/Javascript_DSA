031ec4b94c572ec4ab2ae29becc1e846a8ca969b
Ryan Kirsch <agalloch88@gmail.com>
2025-02-08 12:24:12 +0100

    SortKSortedArray solution 1 MinHeap class complete

--- Heaps/SortKSortedArray.js
+++ Heaps/SortKSortedArray.js
@@ -58,6 +58,31 @@ class MinHeap {
         }
     }
 
+    siftUp(currentIdx, heap) {
+        let parentIdx = Math.floor((currentIdx - 1) / 2);
+        while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+            this.swap(currentIdx, parentIdx, heap);
+            currentIdx = parentIdx;
+            parentIdx = Math.floor((currentIdx - 1) / 2);
+        }
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    remove() {
+        this.swap(0, this.heap.length - 1, this.heap);
+        let valueToRemove = this.heap.pop();
+        this.siftDown(0, this.heap.length - 1, this.heap);
+        return valueToRemove;
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.siftUp(this.heap.length - 1, this.heap);
+    }
+
     swap(i, j, heap) {
         let temp = heap[j];
         heap[j] = heap[i];
