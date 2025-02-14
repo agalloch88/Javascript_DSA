a0d2e10d6e145efa0c108f8c184bd48a3274d615
Ryan Kirsch <agalloch88@gmail.com>
2025-02-14 12:24:12 +0100

    run format script

--- Heaps/SortKSortedArray.js
+++ Heaps/SortKSortedArray.js
@@ -21,92 +21,94 @@
 // Solution 1:
 
 class MinHeap {
-    constructor(array) {
-        this.heap = this.buildHeap(array);
-    }
-    isEmpty() {
-        return this.heap.length === 0;
-    }
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+  isEmpty() {
+    return this.heap.length === 0;
+  }
 
-    buildHeap(array) {
-        let firstParentIdx = Math.floor((array.length - 2) / 2);
-        for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
-            this.siftDown(currentIdx, array.length - 1, array);
-        }
-        return array;
+  buildHeap(array) {
+    let firstParentIdx = Math.floor((array.length - 2) / 2);
+    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, array.length - 1, array);
     }
+    return array;
+  }
 
-    siftDown(currentIdx, endIdx, heap) {
-        let childOneIdx = currentIdx * 2 + 1;
-        while (childOneIdx <= endIdx) {
-            let childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
-            let idxToSwap;
+  siftDown(currentIdx, endIdx, heap) {
+    let childOneIdx = currentIdx * 2 + 1;
+    while (childOneIdx <= endIdx) {
+      let childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+      let idxToSwap;
 
-            if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
-                idxToSwap = childTwoIdx;
-            } else {
-                idxToSwap = childOneIdx;
-            }
+      if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+        idxToSwap = childTwoIdx;
+      } else {
+        idxToSwap = childOneIdx;
+      }
 
-            if (heap[idxToSwap] < heap[currentIdx]) {
-                this.swap(currentIdx, idxToSwap, heap);
-                currentIdx = idxToSwap;
-                childOneIdx = currentIdx * 2 + 1;
-            } else {
-                return;
-            }
-        }
+      if (heap[idxToSwap] < heap[currentIdx]) {
+        this.swap(currentIdx, idxToSwap, heap);
+        currentIdx = idxToSwap;
+        childOneIdx = currentIdx * 2 + 1;
+      } else {
+        return;
+      }
     }
+  }
 
-    siftUp(currentIdx, heap) {
-        let parentIdx = Math.floor((currentIdx - 1) / 2);
-        while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
-            this.swap(currentIdx, parentIdx, heap);
-            currentIdx = parentIdx;
-            parentIdx = Math.floor((currentIdx - 1) / 2);
-        }
+  siftUp(currentIdx, heap) {
+    let parentIdx = Math.floor((currentIdx - 1) / 2);
+    while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+      this.swap(currentIdx, parentIdx, heap);
+      currentIdx = parentIdx;
+      parentIdx = Math.floor((currentIdx - 1) / 2);
     }
+  }
 
-    peek() {
-        return this.heap[0];
-    }
+  peek() {
+    return this.heap[0];
+  }
 
-    remove() {
-        this.swap(0, this.heap.length - 1, this.heap);
-        let valueToRemove = this.heap.pop();
-        this.siftDown(0, this.heap.length - 1, this.heap);
-        return valueToRemove;
-    }
+  remove() {
+    this.swap(0, this.heap.length - 1, this.heap);
+    let valueToRemove = this.heap.pop();
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return valueToRemove;
+  }
 
-    insert(value) {
-        this.heap.push(value);
-        this.siftUp(this.heap.length - 1, this.heap);
-    }
+  insert(value) {
+    this.heap.push(value);
+    this.siftUp(this.heap.length - 1, this.heap);
+  }
 
-    swap(i, j, heap) {
-        let temp = heap[j];
-        heap[j] = heap[i];
-        heap[i] = temp;
-    }
+  swap(i, j, heap) {
+    let temp = heap[j];
+    heap[j] = heap[i];
+    heap[i] = temp;
+  }
 }
 
 function sortKSortedArray(array, k) {
-    let minHeapWithKElements = new MinHeap(array.slice(0, Math.min(k + 1, array.length)));
+  let minHeapWithKElements = new MinHeap(
+    array.slice(0, Math.min(k + 1, array.length)),
+  );
 
-    let nextIndexToInsertElement = 0;
-    for (let idx = k + 1; idx < array.length; idx++) {
-        let minElement = minHeapWithKElements.remove();
-        array[nextIndexToInsertElement] = minElement;
-        nextIndexToInsertElement++;
-        let currentElement = array[idx];
-        minHeapWithKElements.insert(currentElement);
-    }
+  let nextIndexToInsertElement = 0;
+  for (let idx = k + 1; idx < array.length; idx++) {
+    let minElement = minHeapWithKElements.remove();
+    array[nextIndexToInsertElement] = minElement;
+    nextIndexToInsertElement++;
+    let currentElement = array[idx];
+    minHeapWithKElements.insert(currentElement);
+  }
 
-    while (!minHeapWithKElements.isEmpty()) {
-        let minElement = minHeapWithKElements.remove();
-        array[nextIndexToInsertElement] = minElement;
-        nextIndexToInsertElement++;
-    }
+  while (!minHeapWithKElements.isEmpty()) {
+    let minElement = minHeapWithKElements.remove();
+    array[nextIndexToInsertElement] = minElement;
+    nextIndexToInsertElement++;
+  }
 
-    return array;
+  return array;
 }
