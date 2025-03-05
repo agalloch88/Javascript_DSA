2333aab4fd086450294768738bfde26ae3dd16d3
Ryan Kirsch <agalloch88@gmail.com>
2025-03-05 12:24:12 +0100

    HeapSort solution 1

--- Sorting/HeapSort.js
+++ Sorting/HeapSort.js
@@ -10,3 +10,45 @@
 // [2, 3, 5, 5, 6, 8, 9]
 
 // Solution 1:
+
+function heapSort(array) {
+    buildMaxHeap(array);
+    for (let endIdx = array.length - 1; endIdx > 0; endIdx--) {
+        swap(0, endIdx, array);
+        siftDown(0, endIdx - 1, array);
+    }
+    return array;
+}
+
+function buildMaxHeap(array) {
+    let firstParentIdx = Math.floor((array.length - 2) / 2);
+    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+        siftDown(currentIdx, array.length - 1, array);
+    }
+}
+
+function siftDown(currentIdx, endIdx, heap) {
+    let childOneIdx = currentIdx * 2 + 1;
+    while (childOneIdx <= endIdx) {
+        let childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+        let idxToSwap;
+        if (childTwoIdx !== -1 && heap[childTwoIdx] > heap[childOneIdx]) {
+            idxToSwap = childTwoIdx;
+        } else {
+            idxToSwap = childOneIdx;
+        }
+        if (heap[idxToSwap] > heap[currentIdx]) {
+            swap(currentIdx, idxToSwap, heap);
+            currentIdx = idxToSwap;
+            childOneIdx = currentIdx * 2 + 1;
+        } else {
+            return;
+        }
+    }
+}
+
+function swap(i, j, array) {
+    let temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
