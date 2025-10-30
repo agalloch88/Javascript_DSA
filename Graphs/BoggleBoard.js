59c92940a784a87c8ee1ecc85c40ef1f2f37d322
Ryan Kirsch <agalloch88@gmail.com>
2025-10-30 12:36:12 +0100

    BoggleBoard solution 1

--- Graphs/BoggleBoard.js
+++ Graphs/BoggleBoard.js
@@ -30,3 +30,97 @@
 // note that words may be ordered differently
 
 // Solution 1:
+
+function boggleBoard(board, words) {
+  let trie = new Trie();
+  for (let word of words) {
+    trie.add(word);
+  }
+
+  let finalWords = {};
+  let visited = board.map((row) => row.map((letter) => false));
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      explore(i, j, board, trie.root, visited, finalWords);
+    }
+  }
+  return Object.keys(finalWords);
+}
+
+function explore(i, j, board, trieNode, visited, finalWords) {
+  if (visited[i][j]) {
+    return;
+  }
+
+  let letter = board[i][j];
+  if (!(letter in trieNode)) {
+    return;
+  }
+  visited[i][j] = true;
+  trieNode = trieNode[letter];
+  if ('*' in trieNode) {
+    finalWords[trieNode['*']] = true;
+  }
+
+  let neighbors = getNeighbors(i, j, board);
+  for (let neighbor of neighbors) {
+    explore(neighbor[0], neighbor[1], board, trieNode, visited, finalWords);
+  }
+  visited[i][j] = false;
+}
+
+function getNeighbors(i, j, board) {
+  let neighbors = [];
+
+  if (i > 0 && j > 0) {
+    neighbors.push([i - 1, j - 1]);
+  }
+
+  if (i > 0 && j < board[0].length - 1) {
+    neighbors.push([i - 1, j + 1]);
+  }
+
+  if (i < board.length - 1 && j < board[0].length - 1) {
+    neighbors.push([i + 1, j + 1]);
+  }
+
+  if (i < board.length - 1 && j > 0) {
+    neighbors.push([i + 1, j - 1]);
+  }
+
+  if (i > 0) {
+    neighbors.push([i - 1, j]);
+  }
+
+  if (i < board.length - 1) {
+    neighbors.push([i + 1, j]);
+  }
+
+  if (j > 0) {
+    neighbors.push([i, j - 1]);
+  }
+
+  if (j < board[0].length - 1) {
+    neighbors.push([i, j + 1]);
+  }
+
+  return neighbors;
+}
+
+class Trie {
+  constructor() {
+    this.root = {};
+    this.endSymbol = '*';
+  }
+
+  add(word) {
+    let current = this.root;
+    for (let letter of word) {
+      if (!(letter in current)) {
+        current[letter] = {};
+      }
+      current = current[letter];
+      current[this.endSymbol] = word;
+    }
+  }
+}
